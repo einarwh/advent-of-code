b3dcd078586cd2086f1dd3abe176584c90ec2437
Einar W. Høst <einarwh@gmail.com>
2022-12-02 17:01:49 +0100

    2019-01

---
+++ 2019/01/aoc01.fsx
@@ -0,0 +1,25 @@
+// Advent of Code 2019. Day 1.
+// dotnet fsi aoc01.fsx
+
+open System
+open System.IO
+
+let fuel mass = mass / 3 - 2
+
+let zenoFuel mass = 
+    let gen m = if m > 0 then Some (m, fuel m) else None
+    mass |> fuel |> Seq.unfold gen |> Seq.sum 
+
+let parse (s: string) =
+    match Int32.TryParse(s) with 
+    | (true, n) -> Some n
+    | _ -> None
+
+let calculate fuelFn = 
+    Seq.choose parse >> Seq.map fuelFn >> Seq.sum
+
+let run lines = 
+    lines |> calculate fuel |> printfn "%d"
+    lines |> calculate zenoFuel |> printfn "%d"
+
+"input" |> File.ReadAllLines |> run
