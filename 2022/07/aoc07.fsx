88ec5da3311c463737877f25881e2d8a216a3437
Einar W. Høst <einarwh@gmail.com>
2022-12-07 19:10:11 +0100

    Update aoc07.fsx

--- 2022/07/aoc07.fsx
+++ 2022/07/aoc07.fsx
@@ -1,5 +1,6 @@
 
 open System.IO
+open System.Text.RegularExpressions
 
 type Command = 
     | ChangeDirectory of string 
@@ -9,9 +10,60 @@ type FileSystemEntry =
     | File of (string * int) 
     | Directory of (string * FileSystemEntry list)
 
-let run lines = 
-    lines |> printfn "%A"
+let parseChangeDirectoryCommand (line : string) : Command option = 
+    let pattern = "^\$ cd (.+)$"
+    let m = Regex.Match(line, pattern)
+    if m.Success then
+        m.Groups.[1].Value |> ChangeDirectory |> Some
+    else 
+        None 
+
+let parseListCommand (line : string) : Command option = 
+    let pattern = "^\$ ls"
+    let m = Regex.Match(line, pattern)
+    if m.Success then
+        ListDirectory |> Some
+    else 
+        None
+
+let consumeChangeDirectoryCommand (lines : string list) : string list = 
+    match lines with 
+    | [] -> failwith "expected cd command"
+    | h :: t -> 
+        parseChangeDirectoryCommand h |> ignore 
+        t 
+
+let consumeListCommand (lines : string list) : string list = 
+    match lines with 
+    | [] -> failwith "expected ls command"
+    | h :: t -> 
+        parseListCommand h |> ignore 
+        t 
+
+let consumeListContent (lines : string list) = 
+    let rec consume acc lines = 
+        match lines with 
+        | [] -> (acc, lines) 
+        | h :: rest -> 
+            match parseChangeDirectoryCommand h with 
+            | Some cd -> (acc, lines)
+            | None -> 
+                printfn "Consumed %s" h 
+                consume (h :: acc) rest 
+    let (consumed, remaining) = consume [] lines 
+    remaining
+
+let consumeDirectory (lines : string list) = 
+    lines 
+    |> consumeChangeDirectoryCommand 
+    |> consumeListCommand
+    |> consumeListContent
+
+let run (lines : string list) = 
+    consumeDirectory lines
 
 "sample"
 |> File.ReadAllLines 
+|> Array.toList
 |> run 
+|> printfn "%A"
