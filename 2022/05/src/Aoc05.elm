b722200fc37161ae18b1fcc8c0ad16920940d374
Einar W. Høst <einarwh@gmail.com>
2022-12-07 09:46:24 +0100

    05

--- 2022/05/src/Aoc05.elm
+++ 2022/05/src/Aoc05.elm
@@ -11,7 +11,10 @@ import Svg.Attributes exposing (..)
 import Time
 
 delay : Float
-delay = 500
+delay = 1000
+
+crateSize : Int 
+crateSize = 20
 
 -- MAIN
 
@@ -65,8 +68,8 @@ init _ =
         , { amount = 1, source = 1, target = 2 } ]
     
     model = { stacks = stacks
-            , commands = []
-            , keepOrder = false
+            , commands = commands
+            , keepOrder = False
             , counter = 0
             , debug = "..."}
   in 
@@ -74,7 +77,7 @@ init _ =
 
 -- UPDATE
 
-type Msg = Tick
+type Msg = Tick | Step 
 
 takeAmount : Int -> Crates -> Stack -> (Crates, Stack) 
 takeAmount amount taken stack =
@@ -98,33 +101,105 @@ runCommand : Bool -> Command -> Stacks -> Stacks
 runCommand keepOrder command stacks = 
     moveAmount keepOrder command.amount command.source command.target stacks
 
+toCommandText : Command -> String 
+toCommandText cmd = 
+  "move " ++ String.fromInt cmd.amount ++ " from " ++ String.fromInt cmd.source ++ " to " ++ String.fromInt cmd.target
+
 updateModel : Model -> Model
 updateModel model =
-  let
-    counter = model.counter + 1
-  in
-    { model | counter = counter }
+  case model.commands of 
+    [] -> model 
+    cmd :: restCmds -> 
+      let 
+        updatedStacks = runCommand model.keepOrder cmd model.stacks
+        updatedCounter = model.counter + 1 
+        debugText = toCommandText cmd
+      in 
+        { model | counter = updatedCounter, stacks = updatedStacks, commands = restCmds, debug = debugText }
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Tick ->
       (updateModel model, Cmd.none)
+    Step ->
+      (updateModel model, Cmd.none)
 
 -- SUBSCRIPTIONS
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Time.every delay (\_ -> Tick)
+  if List.isEmpty model.commands then Sub.none 
+  else Time.every delay (\_ -> Tick)
+
+  -- Time.every delay (\_ -> Tick)
 
 -- VIEW
-  
+
+toCrateRect : Int -> Int -> Int -> Html Msg 
+toCrateRect yMax stackId crateNo = 
+  let 
+    w = crateSize
+    h = crateSize
+    xVal = (1 + stackId) * 2 * w
+    yVal = (1 + crateNo) * h
+  in
+    rect
+      [ x (String.fromInt xVal)
+      , y (String.fromInt (yMax - yVal))
+      , width (String.fromInt w) 
+      , height (String.fromInt h)
+      , stroke "black"
+      , fill "none" ]
+      []
+
+toCrateText : Int -> Int -> Int -> Crate -> Html Msg 
+toCrateText yMax stackId crateNo crate = 
+  let 
+    w = crateSize
+    h = crateSize
+    xOffset = 4
+    yOffset = 18
+    xVal = xOffset + (1 + stackId) * 2 * w
+    yVal = (1 + crateNo) * h - yOffset
+  in
+    text_
+      [ x (String.fromInt xVal)
+      , y (String.fromInt (yMax - yVal))
+      , fontFamily "monospace" ]
+      [ text (String.fromChar crate) ]
+
+toCrateSvgElements : Int -> Int -> Int -> Crate -> List (Html Msg)
+toCrateSvgElements yMax stackId crateNo crate = 
+  [ toCrateRect yMax stackId crateNo
+  , toCrateText yMax stackId crateNo crate ]
+
+toStackSvgElements : Int -> (Int, Stack) -> List (Html Msg)
+toStackSvgElements yMax (stackId, stack) =
+  let 
+    crateElements = 
+      stack |> List.indexedMap (toCrateSvgElements yMax stackId)
+            |> List.concat
+  in 
+    crateElements
+
+toSvg : Stacks -> Html Msg 
+toSvg stacks = 
+  let 
+    lst = stacks |> Dict.toList |> List.concatMap (toStackSvgElements 400)
+  in 
+    svg
+      [ viewBox "0 0 400 400"
+      , width "400"
+      , height "400"
+      , Svg.Attributes.style "background-color:lightgrey" ]
+      lst
 
 view : Model -> Html Msg
 view model =
   let
-    s = text "foo"
-    tickStr = "Steps: " ++ String.fromInt model.counter
+    s = toSvg model.stacks
+    tickStr = "commands: " ++ String.fromInt model.counter
   in 
     Html.table 
       []
@@ -148,5 +223,9 @@ view model =
               [ Html.div [ Html.Attributes.align "center" ] [ s ] 
               , Html.div [] [ Html.text tickStr ]
               , Html.div [] [ Html.text model.debug ]
-              ] ] ]
-
+              ] ] 
+      , Html.tr 
+          []
+          [ Html.td 
+              []
+              [ Html.button [ onClick Step ] [ text "Step" ] ] ] ]
