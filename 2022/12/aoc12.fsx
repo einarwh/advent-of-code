42c6b4dd1352ccd6c54fdb1d44e38c74056dbb66
Einar W. Høst <einarwh@gmail.com>
2022-12-13 22:34:20 +0100

    2022-12a

--- 2022/12/aoc12.fsx
+++ 2022/12/aoc12.fsx
@@ -3,7 +3,6 @@
 // dotnet fsi aoc12.fsx
 
 open System.IO
-open System.Collections.Generic
 
 type Pos = (int * int)
 
@@ -21,67 +20,66 @@ let findStartPos = findCharPos 'S'
 
 let findEndPos = findCharPos 'E'
 
-let listPotentialNeighbours (pos : Pos) : Pos list = 
-    match pos with 
-    | (x, y) -> 
-        [ (x - 1, y - 1)
-          (x, y - 1)
-          (x + 1, y - 1)
-          (x - 1, y)
-          (x + 1, y)
-          (x - 1, y + 1)
-          (x, y + 1)
-          (x + 1, y + 1) ]
+let toElevation (ch : char) : char = 
+    match ch with 
+    | 'S' -> 'a'
+    | 'E' -> 'z'
+    | _ -> ch
 
-let listNeighbours (matrix : char [,]) ((x, y) : Pos) : Pos list = 
+let listLegalNeighbours (matrix : char [,]) ((x, y) : Pos) : Pos list = 
     let rowCount = Array2D.length1 matrix // ys
     let colCount = Array2D.length2 matrix // xs
-    let currentHeight = Array2D.get matrix y x 
+    let currentHeight = Array2D.get matrix y x |> toElevation
     let check (x, y) = 
         if x >= 0 && x < colCount && y >= 0 && y < rowCount then 
-            let targetHeight = Array2D.get matrix y x 
+            let targetHeight = Array2D.get matrix y x |> toElevation
             let diffHeight = (int targetHeight) - (int currentHeight) 
-            diffHeight <= 1
+            let result = diffHeight <= 1
+            result
         else 
             false 
-    [ (x - 1, y - 1)
-      (x, y - 1)
-      (x + 1, y - 1)
-      (x - 1, y)
-      (x + 1, y)
-      (x - 1, y + 1)
-      (x, y + 1)
-      (x + 1, y + 1) ]
-    |> List.filter check
+    let result = 
+        [ (x, y - 1)
+          (x - 1, y)
+          (x + 1, y)
+          (x, y + 1) ]
+        |> List.filter check
+    result 
 
 let solve (startPos : Pos) (endPos : Pos) (lists : char list list) = 
     let matrix = array2D lists
-    let rowCount = Array2D.length1 matrix // ys
-    let colCount = Array2D.length2 matrix // xs
-    let rec fn (steps : int) (current : Pos) (visited : Set<Pos>) (unvisited : PriorityQueue<Pos, int>) = 
-        let neighbours = listNeighbours matrix current 
-        let unvisitedNeighbours = 
-            neighbours |> List.filter (fun n -> not <| Set.contains n visited)
-        
+    let rec fn (distance : int) (positions : Set<Pos>) (visited : Set<Pos>) = 
+        if Set.contains endPos visited then 
+            distance - 1
+        else if Set.count positions = 0 then 
+            failwith "Didn't find end position!"
+        else 
+            let nextPositions = 
+                positions
+                |> Seq.collect (fun pos -> listLegalNeighbours matrix pos)
+                |> Set.ofSeq 
+                |> Set.filter (fun pos -> not <| Set.contains pos visited)
+            fn (distance + 1) nextPositions (Set.union visited positions)
+    let initSet = Set.empty |> Set.add startPos 
+    fn 0 initSet Set.empty
 
-
-    let posx =
-        [ for y in 0 .. rowCount - 1 do
-            for x in 0 .. colCount - 1 -> (x, y) ]
-    posx |> printfn "%A"
-
-    rowCount |> printfn "rows: %d"
-    colCount |> printfn "cols: %d"
-    startPos |> printfn "S: %A"
-    endPos |> printfn "E: %A"
-    matrix |> printfn "%A"
-
-let run lists =
+let part1 lists = 
     let startPos = lists |> findStartPos
     let endPos = lists |> findEndPos
-    solve startPos endPos lists
+    let distance = solve startPos endPos lists
+    distance |> printfn "Distance: %A"
 
-"sample"
+let part2 lists = 
+    let startPos = lists |> findStartPos
+    let endPos = lists |> findEndPos
+    let distance = solve startPos endPos lists
+    distance |> printfn "Distance: %A"
+
+let run lists =
+    lists |> part1 
+    lists |> part2
+
+"input"
 |> File.ReadAllLines 
 |> Array.toList 
 |> List.map (Seq.toList)
