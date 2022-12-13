4d2124510cf3596e3d5d0fa3fe6a8ddbd155c1e3
Einar W. Høst <einarwh@gmail.com>
2022-12-13 21:29:00 +0100

    2022-13

--- 2022/13/aoc13.fsx
+++ 2022/13/aoc13.fsx
@@ -85,14 +85,51 @@ and compareLists (lst1 : Item list, lst2 : Item list) =
         | LT -> LT 
         | GT -> GT 
         | EQ -> compareLists (t1, t2)
- 
-let run (lines : string list) =
-    let pairs = lines |> List.map parsePair 
-    pairs 
+
+let part1 lines = 
+    lines 
+    |> List.map parsePair 
     |> List.mapi (fun ix pair -> if LT = compareItems pair then Some (ix+1) else None) 
     |> List.choose id
     |> List.sum
     |> printfn "Sum: %d"
+
+let sort (lines : Item list) : Item list = 
+    let rec insert (seen : Item list) (sorted : Item list) (item : Item) = 
+        match sorted with 
+        | [] -> (item :: seen) |> List.rev 
+        | h :: t -> 
+            match compareItems (item, h) with 
+            | LT -> (List.rev seen) @ (item :: sorted)
+            | _ -> insert (h :: seen) t item 
+    let rec fn (sorted : Item list) (lines : Item list) = 
+        match lines with 
+        | [] -> sorted 
+        | h :: t -> 
+            fn (insert [] sorted h) t
+    fn [] lines
+
+let findPacketIndex (items : Item list) (packet : Item) = 
+    1 + List.findIndex (fun it -> it = packet) items
+
+let findPacketIndexes (packets : Item list) (items : Item list) = 
+    packets |> List.map (findPacketIndex items)
+
+let part2 lines = 
+    let (p1, _) = "[[2]]" |> parseItem 0
+    let (p2, _) = "[[6]]" |> parseItem 0
+    lines 
+    |> List.collect (split "\n" >> Array.toList)
+    |> List.map (parseItem 0 >> fst)
+    |> (@) [p1;p2]
+    |> sort
+    |> findPacketIndexes [p1;p2]
+    |> List.fold (*) 1
+    |> printfn "Decoder key: %d" 
+
+let run (lines : string list) =
+    part1 lines
+    part2 lines
 
 "input"
 |> File.ReadAllText
