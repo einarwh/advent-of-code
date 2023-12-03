77781377da3f53e7f1dd68d719ca1af6e1da0c73
Einar W. Høst <einarwh@gmail.com>
2023-12-04 00:14:44 +0100

    Update aoc03.fsx

--- 2023/03/aoc03.fsx
+++ 2023/03/aoc03.fsx
@@ -24,63 +24,52 @@ let toNumberBox ((x, y), s : string) =
       xMax = x+s.Length-1
       yMax = y }
 
-let parseNumbers (line : string) = 
-    Regex.Matches(line, "\d+") 
+let parse pattern line = 
+    Regex.Matches(line, pattern) 
     |> Seq.map (fun m -> (m.Index, m.Value))
     |> Seq.toList
 
-let parseSymbols (line : string) = 
-    Regex.Matches(line, "[^\d\.]") 
-    |> Seq.map (fun m -> (m.Index, m.Value))
-    |> Seq.toList
+let parseNumbers = parse "\d+"
 
-let parseSymbolsAtLine (i : int) (line : string) = 
+let parseSymbols = parse "[^\d\.]"
+
+let parseSymbolsAtLine (y : int) (line : string) = 
     line
     |> parseSymbols
-    |> List.map (fun (x, s) -> (toSymbolBox (x, i), s))
+    |> List.map (fun (x, s) -> (toSymbolBox (x, y), s))
 
-let parseNumbersAtLine (i : int) (line : string) = 
+let parseNumbersAtLine (y : int) (line : string) = 
     line
     |> parseNumbers
-    |> List.map (fun (x, s) -> (toNumberBox ((x, i), s), s))
+    |> List.map (fun (x, s) -> (toNumberBox ((x, y), s), s))
 
 let overlapping (box1 : Box) (box2 : Box) : bool = 
     box1.xMax >= box2.xMin && box2.xMax >= box1.xMin &&  
     box1.yMax >= box2.yMin && box2.yMax >= box1.yMin 
 
+let checkNumber (symbolBoxes : Box list) (numberBox, _) : bool = 
+    symbolBoxes |> List.exists (overlapping numberBox)
+
+let gearRatio (numBoxList : (Box * string) list) (symbolBox, _) : int = 
+    let parts = 
+        numBoxList |> List.filter (fun (box, _) -> overlapping box symbolBox)
+    match parts with 
+    | [(_, str1); (_, str2)] -> (int str1) * (int str2) 
+    | _ -> 0 
+
 let readLines = 
     File.ReadAllLines
     >> Array.filter (fun line -> line <> String.Empty)
 
-let checkNumber (symbolBoxes : Box list) (numberBox, _) : bool = 
-    symbolBoxes |> List.exists (overlapping numberBox)
-
-let gearRatio (numBoxList : (Box * string) list) (symbolBox, _) : int64 = 
-    let parts = 
-        numBoxList |> List.filter (fun (box, num) -> overlapping box symbolBox)
-    match parts with 
-    | [(_, str1); (_, str2)] -> (int64 str1) * (int64 str2) 
-    | _ -> 0 
-
 let run fileName = 
-    let lines = readLines fileName
-    let symbolsAndBoxes = 
-        lines 
-        |> Array.toList
-        |> List.mapi parseSymbolsAtLine
-        |> List.collect id
-    let symbolBoxes = 
-        symbolsAndBoxes |> List.map fst
-    let numbersAndBoxes = 
-        lines 
-        |> Array.toList
-        |> List.mapi parseNumbersAtLine 
-        |> List.collect id
-    let partNumbers = 
-        numbersAndBoxes
-        |> List.filter (checkNumber symbolBoxes)
-        |> List.map (snd >> int)    
-    partNumbers 
+    let lines = readLines fileName |> Array.toList
+    let parseWith parser = List.mapi parser >> List.collect id 
+    let symbolsAndBoxes = lines |> parseWith parseSymbolsAtLine
+    let symbolBoxes = symbolsAndBoxes |> List.map fst
+    let numbersAndBoxes = lines |> parseWith parseNumbersAtLine
+    numbersAndBoxes
+    |> List.filter (checkNumber symbolBoxes)
+    |> List.map (snd >> int)
     |> List.sum 
     |> printfn "%d" 
     symbolsAndBoxes 
