3cc3a2d893f7cd15378b639d1ddb622a1ae7866c
Einar W. Høst <einarwh@gmail.com>
2023-12-03 17:06:19 +0100

    fix

--- 2023/03/src/Aoc03.elm
+++ 2023/03/src/Aoc03.elm
@@ -9,9 +9,17 @@ import Array exposing (Array)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Time
+import Regex
 
 crateSize : Int 
 crateSize = 10
+
+unitWidth : Int 
+unitWidth = 10
+
+unitHeight : Int 
+unitHeight = 10
+
 
 -- MAIN
 
@@ -24,7 +32,7 @@ main =
 
 -- MODEL
 
-type alias SymbolBox = 
+type alias Box = 
     { xMin : Int 
     , xMax : Int 
     , yMin : Int 
@@ -34,14 +42,13 @@ type alias Pos =
     { x : Int
     , y : Int }
 
-type alias SymbolPos = 
-    { x : Int
-    , y : Int }
+type alias BoxedString = 
+    { box : Box 
+    , val : String }
 
-type alias NumberBox = 
-    { xStart : Int
-    , xEnd : Int 
-    , y : Int }
+type alias PosedString = 
+    { pos : Pos 
+    , val : String }
 
 type alias Crate = String 
 
@@ -55,6 +62,9 @@ type alias Stacks = Dict StackId Stack
 
 type alias Model = 
   { stacks : Stacks
+  , data : String
+  , numberBoxes : List Box 
+  , symbolBoxes : List Box 
   , counter : Int 
   , debug : String }
 
@@ -101,7 +111,45 @@ parseStacks s =
           stacks = stackIds |> List.map (initStack crateLines)
         in         
           Dict.fromList stacks
-       
+
+parseNumbers : String -> List (Int, String) 
+parseNumbers line = 
+  let 
+    numberRegex = Regex.fromString "[0-9]+" |> Maybe.withDefault Regex.never
+    matches = line |> Regex.find numberRegex
+  in 
+    matches |> List.map (\m -> (m.index, m.match))
+
+toNumberBox : Pos -> String -> Box 
+toNumberBox { x, y } s = 
+  { xMin = x
+  , xMax = x + String.length s - 1
+  , yMin = y
+  , yMax = y }
+
+parseNumbersWithBox : Int -> String -> List BoxedString 
+parseNumbersWithBox y line = 
+  line |> parseNumbers |> List.map (\(x, s) -> { box = toNumberBox { x = x, y = y } s, val = s })
+
+parseSymbols : String -> List (Int, String) 
+parseSymbols line = 
+  let 
+    symbolRegex = Regex.fromString "[^\\d\\.]" |> Maybe.withDefault Regex.never
+    matches = line |> Regex.find symbolRegex
+  in 
+    matches |> List.map (\m -> (m.index, m.match))
+
+toSymbolBox : Pos -> Box 
+toSymbolBox { x, y } = 
+  { xMin = x - 1
+  , xMax = x + 1
+  , yMin = y - 1
+  , yMax = y + 1 }
+
+parseSymbolsWithBox : Int -> String -> List BoxedString 
+parseSymbolsWithBox y line = 
+  line |> parseSymbols |> List.map (\(x, s) -> { box = toSymbolBox { x = x, y = y }, val = s })
+
 init : () -> (Model, Cmd Msg)
 init _ =
   let 
@@ -117,7 +165,25 @@ init _ =
 .664.598.."""
 
     lines = sample |> String.split "\n"
-    debugStr = lines |> List.length |> String.fromInt
+    --debugStr = lines |> List.length |> String.fromInt
+
+    boxedNumbers = 
+        lines |> List.indexedMap parseNumbersWithBox |> List.concat
+
+    numberBoxes = boxedNumbers |> List.map (\bt -> bt.box)
+
+    boxedSymbols = 
+        lines |> List.indexedMap parseSymbolsWithBox |> List.concat
+
+    symbolBoxes = boxedSymbols |> List.map (\bt -> bt.box)
+
+    --debugStr = ".....+.58." |> parseNumbers |> List.length |> String.fromInt
+
+    debugStr = 
+        "617*......" 
+        |> parseSymbols 
+        |> List.map (\(a, b) -> b)
+        |> String.concat 
 
     cratesample = """    [D]    
 [N] [C]    
@@ -133,6 +199,9 @@ move 1 from 1 to 2"""
     parsedStacks = parseStacks stacksStr
 
     model = { stacks = parsedStacks
+            , data = sample
+            , symbolBoxes = symbolBoxes
+            , numberBoxes = numberBoxes
             , counter = 0
             , debug = debugStr }
   in 
@@ -240,6 +309,55 @@ toStackSvgElements yMax (stackId, stack) =
   in 
     labelElement :: crateElements
 
+toLineText : Int -> Int -> String -> Html Msg 
+toLineText yMax i line = 
+  let 
+    xVal = 40
+    yVal = 20 * (i + 1) 
+  in
+    text_
+      [ x (String.fromInt 40)
+      , y (String.fromInt 40)
+      , fontSize "9px"
+      , fontFamily "monospace" ]
+      [ text line ]
+
+toColoredBox : Int -> String -> Box -> Html Msg 
+toColoredBox yMax fillColor box = 
+  let 
+    xDiff = box.xMax - box.xMin 
+    yDiff = box.yMax - box.yMin 
+    w = unitWidth * xDiff 
+    h = unitHeight * (1 + yDiff)
+    xVal = unitWidth * box.xMin
+    yVal = unitHeight * box.yMin
+  in
+    rect
+      [ x (String.fromInt xVal)
+      , y (String.fromInt (yMax - yVal))
+      , width (String.fromInt w) 
+      , height (String.fromInt h)
+      , stroke "black"
+      , opacity "0.6"
+      , fill fillColor ]
+      []
+
+toSvgX : List Box -> List Box -> Html Msg 
+toSvgX numberBoxes symbolBoxes = 
+  let 
+    yMax = 500
+    numberRects = numberBoxes |> List.map (toColoredBox yMax "pink")
+    symbolRects = symbolBoxes |> List.map (toColoredBox yMax "lightgreen")
+    texts = [ "467..114..", "...*......", "..35..633." ] |> List.indexedMap (toLineText yMax)
+    lst = numberRects ++ symbolRects ++ texts
+  in 
+    svg
+      [ viewBox "0 0 210 500"
+      , width "210"
+      , height "500" 
+      , Svg.Attributes.style "background-color:lightblue" ]
+      lst
+
 toSvg : Stacks -> Html Msg 
 toSvg stacks = 
   let 
@@ -256,7 +374,7 @@ toSvg stacks =
 view : Model -> Html Msg
 view model =
   let
-    s = toSvg model.stacks
+    s = toSvgX model.numberBoxes model.symbolBoxes
   in 
     Html.table 
       []
