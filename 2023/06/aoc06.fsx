ed718f6deef30a57e6c6d535f968c2c2417027f2
Einar W. Høst <einarwh@gmail.com>
2023-12-06 07:44:11 +0100

    06a

--- 2023/06/aoc06.fsx
+++ 2023/06/aoc06.fsx
@@ -1,13 +1,20 @@
-// Advent of Code 2023. 
+// Advent of Code 2023. Day 6: Wait For It
 // dotnet fsi aoc06.fsx
 
 open System
 open System.IO
 open System.Text.RegularExpressions
 
-let readChunks fileName = 
-    let text = File.ReadAllText fileName 
-    text.TrimEnd().Split("\n\n") |> Array.toList 
+let parseNumbers (s : string) : int list =
+    Regex.Matches(s, "\d+") 
+    |> Seq.map (fun m -> int m.Value)
+    |> Seq.toList
+
+let strategies (duration : int) : int list = 
+    [0 .. duration] |> List.map (fun i -> i * (duration - i))
+
+let winners (record, attempts) = 
+    attempts |> List.filter (fun a -> a > record) |> List.length
 
 let readLines = 
     File.ReadAllLines
@@ -15,6 +22,14 @@ let readLines =
 
 let run fileName = 
     let lines = readLines fileName |> Array.toList
-    printfn "%A" lines 
+    match lines |> List.map parseNumbers with 
+    | [durations; distances] -> 
+        durations 
+        |> List.map strategies 
+        |> List.zip distances
+        |> List.map winners
+        |> List.reduce (*)
+        |> printfn "%d"
+    | _ -> failwith "Wrong"
 
-"sample" |> run 
+"input" |> run 
