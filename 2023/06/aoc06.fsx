edaa950bebfb6d3a13ec75a1fb9ca0cdf094c09b
Einar W. Høst <einarwh@gmail.com>
2023-12-06 08:51:02 +0100

    Update aoc06.fsx

--- 2023/06/aoc06.fsx
+++ 2023/06/aoc06.fsx
@@ -10,26 +10,22 @@ let parse (s : string) : string list =
     |> Seq.map (fun m -> m.Value)
     |> Seq.toList
 
-let parseNumbers (s : string) = 
-    s |> parse |> List.map int
+let parseDoubles (s : string) = 
+    s |> parse |> List.map double
 
 let parseDouble (s : string) = 
     s |> parse |> String.concat "" |> double
 
-let strategies (duration : int) : int list = 
-    [0 .. duration] |> List.map (fun i -> i * (duration - i))
-
-let winners (record, attempts) = 
-    attempts |> List.filter (fun a -> a > record) |> List.length
-
-let solve (duration : double) (record : double) = 
+let solve (duration : double, record : double) = 
     let b = duration 
     let c = record 
     let v = Math.Sqrt(b * b - 4. * c)
     let r1 = (b - v) / 2.
     let r2 = (b + v) / 2.
-    let first = r1 |> Math.Ceiling |> int
-    let last = r2 |> Math.Floor |> int
+    let firstRounded = r1 |> Math.Ceiling
+    let first = if r1 = firstRounded then int r1 + 1 else int firstRounded
+    let lastRounded = r2 |> Math.Floor 
+    let last = if r2 = lastRounded then int r2 - 1 else int lastRounded
     last - first + 1
 
 let readLines = 
@@ -38,18 +34,17 @@ let readLines =
 
 let run fileName = 
     let lines = readLines fileName |> Array.toList
-    match lines |> List.map parseNumbers with 
+    match lines |> List.map parseDoubles with 
     | [times; distances] -> 
-        times 
-        |> List.map strategies 
-        |> List.zip distances
-        |> List.map winners
+        distances 
+        |> List.zip times
+        |> List.map solve
         |> List.reduce (*)
         |> printfn "%d"
     | _ -> failwith "Wrong"
     match lines |> List.map parseDouble with 
     | [time; distance] -> 
-        solve time distance |> printfn "%A"
+        solve (time, distance) |> printfn "%A"
     | _ -> failwith "Wrong"
 
 "input" |> run 
