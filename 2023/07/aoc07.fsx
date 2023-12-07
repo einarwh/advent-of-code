005d3e5fdcf779c1e9c3bd71cb66eb33545d2f3f
Einar W. Høst <einarwh@gmail.com>
2023-12-07 08:58:17 +0100

    Update aoc07.fsx

--- 2023/07/aoc07.fsx
+++ 2023/07/aoc07.fsx
@@ -10,7 +10,7 @@ let parseLine (line : string) : (string * int64) =
         (card, int64 bid)
     | _ -> failwith "Wrong"
 
-let rateHandType (hand : string) = 
+let rateHand1 (hand : string) = 
     let lst = 
         hand 
         |> Seq.toList
@@ -18,6 +18,28 @@ let rateHandType (hand : string) =
         |> List.map (snd >> List.length)
         |> List.sortDescending
     match lst with 
+    | [5] -> 6
+    | [4;1] -> 5
+    | [3;2] -> 4
+    | [3;1;1] -> 3
+    | [2;2;1] -> 2
+    | [2;1;1;1] -> 1
+    | _ -> 0
+
+let rateHand2 (hand : string) = 
+    let (jokers, rest) = hand |> Seq.toList |> List.partition (fun ch -> ch = 'J')
+    let jokerCount = jokers |> List.length
+    let lst = 
+        rest 
+        |> Seq.toList
+        |> List.groupBy id
+        |> List.map (snd >> List.length)
+        |> List.sortDescending
+    let improved = 
+        match lst with 
+        | h :: t -> (h + jokerCount) :: t
+        | _ -> lst
+    match improved with 
     | [5] -> 6
     | [4;1] -> 5
     | [3;2] -> 4
@@ -43,35 +65,51 @@ let rateCard1 (card : char) : int =
     | 'A' -> 14
     | _ -> failwith "Wrong"
 
-let compareByHandType (hand1 : string) (hand2 : string) = 
-    rateHandType hand1 - rateHandType hand2 
+let rateCard2 (card : char) : int =
+    match card with 
+    | 'J' -> 1
+    | '2' -> 2
+    | '3' -> 3
+    | '4' -> 4
+    | '5' -> 5
+    | '6' -> 6
+    | '7' -> 7
+    | '8' -> 8
+    | '9' -> 9
+    | 'T' -> 10
+    | 'Q' -> 12
+    | 'K' -> 13
+    | 'A' -> 14
+    | _ -> failwith "Wrong"
 
-let compareByStrongestCard (hand1 : string) (hand2 : string) = 
-    Seq.zip (Seq.map rateCard1 hand1) (Seq.map rateCard1 hand2) 
+let compareByHandType handTypeRater (hand1 : string) (hand2 : string) = 
+    handTypeRater hand1 - handTypeRater hand2 
+
+let compareByStrongestCard cardRater (hand1 : string) (hand2 : string) = 
+    Seq.zip (Seq.map cardRater hand1) (Seq.map cardRater hand2) 
     |> Seq.map (fun (a, b) -> (a - b)) 
     |> Seq.tryFind (fun n -> n <> 0)
     |> Option.defaultValue 0
 
-let compareHands (hand1 : string) (hand2 : string) =
-    match compareByHandType hand1 hand2 with 
-    | 0 -> compareByStrongestCard hand1 hand2 
+let compareHands handRater cardRater (hand1 : string) (hand2 : string) =
+    match compareByHandType handRater hand1 hand2 with 
+    | 0 -> compareByStrongestCard cardRater hand1 hand2 
     | n -> n
 
 let readLines = 
     File.ReadAllLines
     >> Array.filter (fun line -> line <> String.Empty)
 
+let runWith comparer parsedLines = 
+    parsedLines 
+    |> List.sortWith (fun (h1,_) (h2,_) -> comparer h1 h2)
+    |> List.map snd
+    |> List.mapi (fun i bid -> (int64 i + 1L) * bid)
+    |> List.sum
+
 let run fileName = 
-    let lines = readLines fileName |> Array.toList
-    let parsed = lines |> List.map parseLine 
-    let sorted = 
-        parsed 
-        |> List.sortWith (fun (h1,b1) (h2, b2) -> compareHands h1 h2)
-    let winnings =
-        sorted
-        |> List.map snd
-        |> List.mapi (fun i bid -> (int64 i + 1L) * bid)
-        |> List.sum
-    printfn "%d" winnings
+    let parsed = readLines fileName |> Array.toList |> List.map parseLine
+    parsed |> runWith (compareHands rateHand1 rateCard1) |> printfn "%d"
+    parsed |> runWith (compareHands rateHand2 rateCard2) |> printfn "%d"
 
 "input" |> run 
