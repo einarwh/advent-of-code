27340f336bb2303eb125233767bdd36bdd6d72fb
Einar W. Høst <einarwh@gmail.com>
2023-12-08 07:55:58 +0100

    08 init

--- 2023/08/aoc08.fsx
+++ 2023/08/aoc08.fsx
@@ -3,18 +3,41 @@
 
 open System
 open System.IO
-open System.Text.RegularExpressions
 
-let readChunks fileName = 
-    let text = File.ReadAllText fileName 
-    text.TrimEnd().Split("\n\n") |> Array.toList 
+module Seq = 
+  let repeat items = 
+    seq { while true do yield! items }
+
+let parse (s : string) = 
+    let a = s.Substring(0, 3)
+    let b = s.Substring(7, 3)
+    let c = s.Substring(12, 3)
+    (a, (b, c))
 
 let readLines = 
     File.ReadAllLines
-    >> Array.filter (fun line -> line <> String.Empty)
+    >> Array.filter ((<>) String.Empty)
+
+let rec solve steps start stop instructions (map : Map<string, string*string>) = 
+    if start = stop then steps
+    else 
+        let ins = instructions |> Seq.head 
+        let tail = instructions |> Seq.tail
+        let (left, right) = map[start]
+        let next = 
+            match ins with 
+            | 'R' -> right 
+            | 'L' -> left 
+            | _ -> failwith <| sprintf "No instruction %c" ins
+        solve (steps + 1) next stop tail map
 
 let run fileName = 
     let lines = readLines fileName |> Array.toList
-    printfn "%A" lines 
+    match lines with 
+    | h :: t -> 
+        let ins = h |> Seq.repeat 
+        let map = t |> List.map parse |> Map.ofList
+        solve 0 "AAA" "ZZZ" ins map |> printfn "%d"
+    | _ -> failwith "Wrong"
 
-"sample" |> run 
+"input" |> run 
