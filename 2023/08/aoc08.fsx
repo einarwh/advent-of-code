89a35ba4539ea8684e02e4575d480f4bbcaf0cfa
Einar W. Høst <einarwh@gmail.com>
2023-12-08 09:31:01 +0100

    Update aoc08.fsx

--- 2023/08/aoc08.fsx
+++ 2023/08/aoc08.fsx
@@ -4,10 +4,6 @@
 open System
 open System.IO
 
-module Seq = 
-  let repeat items = 
-    seq { while true do yield! items }
-
 let parse (s : string) = 
     let a = s.Substring(0, 3)
     let b = s.Substring(7, 3)
@@ -15,29 +11,28 @@ let parse (s : string) =
     (a, (b, c))
 
 let readLines = 
-    File.ReadAllLines
-    >> Array.filter ((<>) String.Empty)
+    File.ReadAllLines >> Array.filter ((<>) String.Empty)
 
-let rec solve steps start stop instructions (map : Map<string, string*string>) = 
-    if start = stop then steps
-    else 
-        let ins = instructions |> Seq.head 
-        let tail = instructions |> Seq.tail
-        let (left, right) = map[start]
-        let next = 
-            match ins with 
-            | 'R' -> right 
-            | 'L' -> left 
-            | _ -> failwith <| sprintf "No instruction %c" ins
-        solve (steps + 1) next stop tail map
+let solve (instructions : string) (map : Map<string, string*string>) = 
+    let rec loop steps pos = 
+        if pos = "ZZZ" then steps
+        else 
+            let (left, right) = map[pos]
+            let next = 
+                let insNo = steps % String.length instructions
+                match instructions[insNo] with 
+                | 'R' -> right 
+                | 'L' -> left 
+                | _ -> failwith "Wrong"
+            loop (steps + 1) next 
+    loop 0 "AAA"
 
 let run fileName = 
     let lines = readLines fileName |> Array.toList
     match lines with 
     | h :: t -> 
-        let ins = h |> Seq.repeat 
         let map = t |> List.map parse |> Map.ofList
-        solve 0 "AAA" "ZZZ" ins map |> printfn "%d"
+        solve h map |> printfn "%d"
     | _ -> failwith "Wrong"
 
 "input" |> run 
