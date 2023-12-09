0505cf77b118858f6519720c2286342ea05b6a17
Einar W. Høst <einarwh@gmail.com>
2023-12-09 10:02:29 +0100

    Update aoc09.fsx

--- 2023/09/aoc09.fsx
+++ 2023/09/aoc09.fsx
@@ -7,35 +7,15 @@ open System.IO
 let parseLine (s : string) = 
     s.Split(" ") |> Array.toList |> List.map int64
 
-let differences numbers = 
-    numbers |> List.pairwise |> List.map (fun (a, b) -> (b - a)) 
+let differences = 
+    List.pairwise >> List.map (fun (a, b) -> (b - a)) 
 
-let historyValue numbers = 
-    let rec loop numbers lasts = 
-        if List.forall (fun n -> n = 0L) numbers then 
-            List.sum lasts
-        else 
-            let diffs = differences numbers 
-            let last = List.last numbers
-            loop diffs (last :: lasts)
-    loop numbers []
-
-let rec calculate p numbers : int64 list = 
+let rec calculate p numbers = 
     match numbers with 
     | [] -> []
     | n :: t -> 
         let d = n - p
         d :: calculate d t
-
-let backwardsHistory numbers = 
-    let rec loop numbers (firsts : int64 list) = 
-        if List.forall (fun n -> n = 0L) numbers then 
-            firsts |> (calculate 0L) |> List.last
-        else 
-            let diffs = differences numbers 
-            let first = List.head numbers
-            loop diffs (first :: firsts)
-    loop numbers []
 
 let histories numbers = 
     let rec loop numbers firsts lasts = 
@@ -44,10 +24,10 @@ let histories numbers =
             let backwards = firsts |> calculate 0L |> List.last
             (forwards, backwards)
         else 
-            let diffs = differences numbers 
+            let next = differences numbers 
             let first = List.head numbers
             let last = List.last numbers
-            loop diffs (first :: firsts) (last :: lasts)
+            loop next (first :: firsts) (last :: lasts)
     loop numbers [] []
 
 let readLines = 
@@ -55,23 +35,10 @@ let readLines =
 
 let run fileName = 
     let parsed = readLines fileName |> Array.toList |> List.map parseLine
-    parsed 
-    |> List.sumBy historyValue
-    |> printfn "%d"
-
-    parsed 
-    |> List.sumBy backwardsHistory
-    |> printfn "%A"
-
     let historiesList = parsed |> List.map histories
     let forwards = historiesList |> List.map fst 
     let backwards = historiesList |> List.map snd
     forwards |> List.sum |> printfn "%d"
     backwards |> List.sum |> printfn "%d"
-
-    // parsed 
-    // |> List.map backwardsHistory
-    // |> printfn "%A"
-
 
 "input" |> run 
