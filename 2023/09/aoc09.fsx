8b20e7965439f1bf861147d0d6f898c855ed27fd
Einar W. Høst <einarwh@gmail.com>
2023-12-09 09:31:07 +0100

    09a

--- 2023/09/aoc09.fsx
+++ 2023/09/aoc09.fsx
@@ -1,20 +1,32 @@
-// Advent of Code 2023. 
+// Advent of Code 2023. Day 9: Mirage Maintenance
 // dotnet fsi aoc09.fsx
 
 open System
 open System.IO
-open System.Text.RegularExpressions
 
-let readChunks fileName = 
-    let text = File.ReadAllText fileName 
-    text.TrimEnd().Split("\n\n") |> Array.toList 
+let parseLine (s : string) = 
+    s.Split(" ") |> Array.toList |> List.map int64
+
+let differences numbers = 
+    numbers |> List.pairwise |> List.map (fun (a, b) -> (b - a)) 
+
+let historyValue numbers = 
+    let rec loop numbers acc = 
+        if List.forall (fun n -> n = 0L) numbers then 
+            List.sum acc
+        else 
+            let diffs = differences numbers 
+            let last = List.last numbers
+            loop diffs (last :: acc)
+    loop numbers []
 
 let readLines = 
-    File.ReadAllLines
-    >> Array.filter (fun line -> line <> String.Empty)
+    File.ReadAllLines >> Array.filter ((<>) String.Empty)
 
 let run fileName = 
     let lines = readLines fileName |> Array.toList
-    printfn "%A" lines 
+    lines 
+    |> List.sumBy (parseLine >> historyValue)
+    |> printfn "%d"
 
-"sample" |> run 
+"input" |> run 
