02c2a89771811015496e01074db90417f8af7434
Einar W. Høst <einarwh@gmail.com>
2023-12-11 14:01:28 +0100

    delete

--- 2023/10/src/Aoc10Bendish.elm
+++ 2023/10/src/Aoc10Bendish.elm
@@ -578,26 +578,8 @@ toColoredBox fillColor (xStart, yStart) =
       , y (String.fromInt yVal)
       , width (String.fromInt unitSize) 
       , height (String.fromInt unitSize)
-    --   , stroke "black"
       , opacity "0.8"
       , fill fillColor ]
-      []
-
-toOutlineBox : (Int, Int) -> Html Msg 
-toOutlineBox (xStart, yStart) = 
-  let 
-    xVal = unitSize * xStart
-    yVal = unitSize * yStart
-  in
-    rect
-      [ x (String.fromInt xVal)
-      , y (String.fromInt yVal)
-      , width (String.fromInt unitSize) 
-      , height (String.fromInt unitSize)
-      , stroke "red"
-      , strokeWidth pipeStrokeWidth
-      , fill "None"
-      , opacity "0.8" ]
       []
 
 toStartOutline : (Int, Int) -> Html Msg 
@@ -637,7 +619,6 @@ toSvg model =
       [ viewBox ("0 0 " ++ svgWidth ++ svgHeight)
       , width svgWidth
       , height svgHeight ]
-      -- , Svg.Attributes.style "background-color:lightblue" ]
       elements
 
 view : Model -> Html Msg
