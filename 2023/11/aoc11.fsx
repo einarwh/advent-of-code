bd2cbeb745f568d448318b98feafe005b7503d59
Einar W. Høst <einarwh@gmail.com>
2023-12-11 10:09:02 +0100

    Update aoc11.fsx

--- 2023/11/aoc11.fsx
+++ 2023/11/aoc11.fsx
@@ -14,20 +14,18 @@ let findEmptyRows rows =
 
 let findEmptyColumns rows  =
     let getColumn rows i = rows |> List.map (Seq.item i)
-    let isEmptyColumn = Seq.forall ((=) '.')
     let columnCount = rows |> List.head |> String.length
-    let indexes = [0 .. columnCount - 1]
-    indexes 
+    [0 .. columnCount - 1]
     |> List.map (getColumn rows)
-    |> List.mapi (fun i col -> if isEmptyColumn col then Some (int64 i) else None)
+    |> List.mapi (fun i col -> if Seq.forall ((=) '.') col then Some (int64 i) else None)
     |> List.choose id 
 
-let findGalaxies (lines : string list) =
-    let rowCount = lines |> List.length 
-    let colCount = lines |> List.head |> String.length 
+let findGalaxies rows =
+    let rowCount = rows |> List.length 
+    let colCount = rows |> List.head |> String.length 
     [ for y in 0 .. rowCount - 1 do 
         for x in 0 .. colCount - 1 do 
-            if lines[y][x] = '#' then yield (int64 x, int64 y) ]
+            if rows[y][x] = '#' then yield (int64 x, int64 y) ]
 
 let rec findPairs galaxies = 
     match galaxies with 
@@ -36,16 +34,18 @@ let rec findPairs galaxies =
         let pairs = t |> List.map (fun g -> (h, g)) 
         pairs @ findPairs t 
 
-let findDistance (expansion : int64) emptyRows emptyColumns ((x1, y1), (x2, y2)) = 
+let findDistance expansion emptyRows emptyColumns ((x1, y1), (x2, y2)) = 
     let xStart = min x1 x2 
     let xStop = max x1 x2
     let yStart = min y1 y2 
     let yStop = max y1 y2 
     let isBetween start stop n = start <= n && n <= stop 
-    let xExpansions = emptyColumns |> List.filter (isBetween xStart xStop) |> List.length |> int64
-    let yExpansions = emptyRows |> List.filter (isBetween yStart yStop) |> List.length |> int64
-    let xExpansion = (expansion - 1L) * xExpansions
-    let yExpansion = (expansion - 1L) * yExpansions
+    let countExpansions start stop = 
+        List.filter (isBetween start stop) >> List.length >> int64
+    let getExpansion start stop indexes =
+        (expansion - 1L) * (countExpansions start stop indexes)
+    let xExpansion = emptyColumns |> getExpansion xStart xStop 
+    let yExpansion = emptyRows |> getExpansion yStart yStop 
     xStop - xStart + xExpansion + yStop - yStart + yExpansion
     
 let run fileName = 
