639ee40aa41b00f7458aca8cc5a205dfb9df449e
Einar W. Høst <einarwh@gmail.com>
2023-12-12 09:57:51 +0100

    Update aoc12.fsx

--- 2023/12/aoc12.fsx
+++ 2023/12/aoc12.fsx
@@ -16,73 +16,47 @@ let parseLine (s : string) =
 let readLines =
     File.ReadAllLines >> Array.filter ((<>) String.Empty)
 
-let rec arrangements (springs : char list) : char list list=
-    match springs with
-    | [] -> [[]]
-    | s :: t ->
-        let rest = arrangements t
-        match s with
-        | '?' ->
-            let broken = rest |> List.map (fun r -> '#' :: r)
-            let working = rest |> List.map (fun r -> '.' :: r)
-            broken @ working
-        | _ ->
-            rest |> List.map (fun r -> s :: r)
-
-let countDamaged (s : string) =
-    Regex.Matches(s, "\#+")
-    |> Seq.map (fun m -> m.Value |> String.length)
-    |> Seq.toList
-
-let check (pattern : int list) (springs : string) =
-    let damaged = countDamaged springs
-    if List.length damaged = List.length pattern then
-        List.zip damaged pattern
-        |> List.forall (fun (a, b) -> a = b)
-    else false
-
 let rec tryConsume (springs : char list) (damaged : int) =
     if damaged = 0 then
-        printfn "done consuming: %s" (toStr springs)
+        // printfn "done consuming: %s" (toStr springs)
         Some springs
     else
         match springs with
         | [] -> None
         | '.' :: _ -> None
         | '#' :: rest ->
-            printfn "consuming #"
+            // printfn "consuming #"
             tryConsume rest (damaged - 1)
         | '?' :: rest ->
-            printfn "consuming ? as #"
+            // printfn "consuming ? as #"
             tryConsume rest (damaged - 1)
         | _ -> failwith "no"
 
-
 let consume (springs : char list) (damaged : int) =
-    printfn "consume %d damaged from %s" damaged (toStr springs)
+    // printfn "consume %d damaged from %s" damaged (toStr springs)
     match tryConsume springs damaged with
     | Some s ->
         match s with
         | [] ->
-            printfn "consumed %d: %s -> %s" damaged (toStr springs) (toStr s)
+            // printfn "consumed %d: %s -> %s" damaged (toStr springs) (toStr s)
             [ s ]
         | '#' :: _ -> []
         | _ :: r ->
-            printfn "consumed %d + 1: %s -> %s" damaged (toStr springs) (toStr r)
+            // printfn "consumed %d + 1: %s -> %s" damaged (toStr springs) (toStr r)
             [ r ]
     | None -> []
 
 let rec findPossible (springs : char list) (damaged : int) =
-    printfn "find possible %s" (toStr springs)
+    // printfn "find possible %s" (toStr springs)
     if damaged = 0 then [ springs ]
     else
         match springs with
         | [] -> []
         | '.' :: rest ->
-            printfn "skip dot"
+            // printfn ". -> skip"
             findPossible rest damaged
         | '#' :: _ ->
-            printfn "# -> try consume"
+            // printfn "# -> try consume"
             match tryConsume springs damaged with
             | Some s ->
                 match s with
@@ -91,23 +65,15 @@ let rec findPossible (springs : char list) (damaged : int) =
                 | _ :: r -> [ r ]
             | None -> []
         | '?' :: rest ->
-            printfn "skip ? as ."
+            // printfn "skip ? as ."
             let p1 = findPossible rest damaged // ? as '.'
-            printfn "..."
-            printfn "consume ? as #"
+            // printfn "..."
+            // printfn "consume ? as #"
             let p2 = consume springs damaged // ? as '#'
             let combined = p1 @ p2
-            printfn "got %A" combined
+            // printfn "got %A" combined
             combined
         | _ -> failwith "oof"
-
-let count (springs : char list, pattern : int list) =
-    let damaged = pattern |> List.head
-    printfn "damaged = %d" damaged
-    printfn "springs = %s" (toStr springs)
-    let possibleSprings = findPossible springs damaged
-    printfn "possible = %A" possibleSprings
-    []
 
 let rec loop pattern springs =
     match pattern with
@@ -116,42 +82,35 @@ let rec loop pattern springs =
         let possibles = findPossible springs dmg
         possibles |> List.collect (fun (possible : char list) -> loop remaining possible)
 
-let solve (springs : char list, pattern : int list) =
-    let foo = loop pattern springs
-    let bar = 
-        foo 
+let solve i (springs : char list, pattern : int list) =
+    printfn "solve %d" i 
+    let startTime = DateTime.Now 
+    let result = 
+        loop pattern springs
         |> List.filter (fun leftover -> List.contains '#' leftover |> not) 
-    printfn "%A" bar
-    bar |> List.length
+        |> List.length
+    let elapsedSeconds = (DateTime.Now - startTime).TotalSeconds |> int 
+    printfn "...%d seconds" elapsedSeconds
+    result 
 
-let countArrangements (line : string) =
-    let springs, damaged = line |> parseLine
-    springs
-    |> Seq.toList
-    |> arrangements
-    |> List.map (List.toArray >> String)
-    |> List.filter (check damaged)
-    |> List.length
+let unfold n (springs, pattern) = 
+    let unfoldedSprings = 
+        [0 .. n - 1] |> List.map (fun _ -> springs) |> List.reduce (fun a b -> a @ ['?'] @ b) 
+    let unfoldedPattern = 
+        [0 .. n - 1] |> List.collect (fun _ -> pattern)
+    // printfn "%s" (toStr unfoldedSprings)
+    // printfn "%A" unfoldedPattern
+    (unfoldedSprings, unfoldedPattern)
 
 let run fileName =
     let lines = readLines fileName |> Array.toList
-    lines
-    |> List.map countArrangements
-    |> List.sum
-    |> printfn "%d"
     let s1 = "????.######..#####. 1,6,5"
     let s2 = "?###???????? 3,2,1"
-    // let foo = count (parseLine s2)
-    let bar = solve (parseLine s2)
     lines
     |> List.map parseLine
-    |> List.map solve
+    |> List.map (unfold 5)
+    |> List.mapi solve
     |> List.sum
     |> printfn "%A"
 
-    printfn "."
-
-
-
-
-"sample" |> run
+"input" |> run
