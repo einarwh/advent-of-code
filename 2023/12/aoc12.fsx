135f2d19eb30da86dea859a8d6c7f6148d823b3f
Einar W. Høst <einarwh@gmail.com>
2023-12-12 10:07:23 +0100

    too slow

--- 2023/12/aoc12.fsx
+++ 2023/12/aoc12.fsx
@@ -3,7 +3,6 @@
 
 open System
 open System.IO
-open System.Text.RegularExpressions
 
 let toStr = List.toArray >> String
 
@@ -18,45 +17,34 @@ let readLines =
 
 let rec tryConsume (springs : char list) (damaged : int) =
     if damaged = 0 then
-        // printfn "done consuming: %s" (toStr springs)
         Some springs
     else
         match springs with
         | [] -> None
         | '.' :: _ -> None
         | '#' :: rest ->
-            // printfn "consuming #"
             tryConsume rest (damaged - 1)
         | '?' :: rest ->
-            // printfn "consuming ? as #"
             tryConsume rest (damaged - 1)
         | _ -> failwith "no"
 
 let consume (springs : char list) (damaged : int) =
-    // printfn "consume %d damaged from %s" damaged (toStr springs)
     match tryConsume springs damaged with
     | Some s ->
         match s with
-        | [] ->
-            // printfn "consumed %d: %s -> %s" damaged (toStr springs) (toStr s)
-            [ s ]
+        | [] -> [ s ]
         | '#' :: _ -> []
-        | _ :: r ->
-            // printfn "consumed %d + 1: %s -> %s" damaged (toStr springs) (toStr r)
-            [ r ]
+        | _ :: r -> [ r ]
     | None -> []
 
 let rec findPossible (springs : char list) (damaged : int) =
-    // printfn "find possible %s" (toStr springs)
     if damaged = 0 then [ springs ]
     else
         match springs with
         | [] -> []
         | '.' :: rest ->
-            // printfn ". -> skip"
             findPossible rest damaged
         | '#' :: _ ->
-            // printfn "# -> try consume"
             match tryConsume springs damaged with
             | Some s ->
                 match s with
@@ -65,14 +53,9 @@ let rec findPossible (springs : char list) (damaged : int) =
                 | _ :: r -> [ r ]
             | None -> []
         | '?' :: rest ->
-            // printfn "skip ? as ."
             let p1 = findPossible rest damaged // ? as '.'
-            // printfn "..."
-            // printfn "consume ? as #"
             let p2 = consume springs damaged // ? as '#'
-            let combined = p1 @ p2
-            // printfn "got %A" combined
-            combined
+            p1 @ p2
         | _ -> failwith "oof"
 
 let rec loop pattern springs =
@@ -82,35 +65,40 @@ let rec loop pattern springs =
         let possibles = findPossible springs dmg
         possibles |> List.collect (fun (possible : char list) -> loop remaining possible)
 
-let solve i (springs : char list, pattern : int list) =
-    printfn "solve %d" i 
+let debugSolve i (springs : char list, pattern : int list) =
     let startTime = DateTime.Now 
     let result = 
         loop pattern springs
         |> List.filter (fun leftover -> List.contains '#' leftover |> not) 
         |> List.length
     let elapsedSeconds = (DateTime.Now - startTime).TotalSeconds |> int 
-    printfn "...%d seconds" elapsedSeconds
+    printfn "Line %d has %d permutations (%d seconds)" (i + 1) result elapsedSeconds
     result 
+
+let solve (springs : char list, pattern : int list) =
+    loop pattern springs
+    |> List.filter (fun leftover -> List.contains '#' leftover |> not) 
+    |> List.length
 
 let unfold n (springs, pattern) = 
     let unfoldedSprings = 
         [0 .. n - 1] |> List.map (fun _ -> springs) |> List.reduce (fun a b -> a @ ['?'] @ b) 
     let unfoldedPattern = 
         [0 .. n - 1] |> List.collect (fun _ -> pattern)
-    // printfn "%s" (toStr unfoldedSprings)
-    // printfn "%A" unfoldedPattern
     (unfoldedSprings, unfoldedPattern)
 
 let run fileName =
     let lines = readLines fileName |> Array.toList
-    let s1 = "????.######..#####. 1,6,5"
-    let s2 = "?###???????? 3,2,1"
     lines
     |> List.map parseLine
-    |> List.map (unfold 5)
-    |> List.mapi solve
+    |> List.map solve
     |> List.sum
     |> printfn "%A"
+    // lines
+    // |> List.map parseLine
+    // |> List.map (unfold 5)
+    // |> List.mapi debugSolve
+    // |> List.sum
+    // |> printfn "%A"
 
 "input" |> run
