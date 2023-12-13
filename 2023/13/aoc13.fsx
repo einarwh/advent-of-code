8b19816b6b12e0965b338a97ad71f31ba830f000
Einar W. Høst <einarwh@gmail.com>
2023-12-13 09:17:22 +0100

    Update aoc13.fsx

--- 2023/13/aoc13.fsx
+++ 2023/13/aoc13.fsx
@@ -30,15 +30,64 @@ let toColumns rows =
     [0 .. len - 1]
     |> List.map (fun i -> rows |> List.map (fun r -> r[i]) |> List.toArray |> String)
 
-let countSymmetries (chunk : string) = 
-    let rows = chunk.Split("\n") |> Array.toList
+let countSymmetries (pattern : string) = 
+    let rows = pattern.Split("\n") |> Array.toList
     let cols = rows |> toColumns
     let horizontal = rows |> findSymmetry |> List.sum |> (*) 100
     let vertical = cols |> findSymmetry |> List.sum 
     horizontal + vertical 
 
+let rec toggleChar (chars : char list) index = 
+    match chars with
+    | [] -> []
+    | old :: t -> 
+        let ch = 
+            if index = 0 then 
+                match old with 
+                | '#' -> '.'
+                | '.' -> '#'
+                | _ -> failwith "oof"
+            else old 
+        ch :: toggleChar t (index - 1)
+        
+let createLineVariations (line : string) = 
+    let chars = Seq.toList line 
+    [0 .. String.length line - 1] 
+    |> List.map (toggleChar chars >> List.toArray >> String)
+
+let rec toVariations lines : string list list = 
+    match lines with 
+    | [] -> []
+    | line :: rest -> 
+        let vars = createLineVariations line 
+        let these = vars |> List.map (fun v -> v :: rest)
+        let more = rest |> toVariations |> List.map (fun v -> line :: v) 
+        these @ more 
+
+let tryFindAlternativeSymmetry (lines : string list) = 
+    let originalSymmetries = lines |> findSymmetry 
+    let alternativeSymmetries = 
+        lines 
+        |> toVariations 
+        |> List.collect (findSymmetry) 
+        |> List.except originalSymmetries
+    match alternativeSymmetries with 
+    | h :: _ -> Some h 
+    | _ -> None 
+
+let countSymmetriesWithSmudge (pattern : string) = 
+    let rows = pattern.Split("\n") |> Array.toList
+    match tryFindAlternativeSymmetry rows with 
+    | Some rowIndex -> 100 * rowIndex 
+    | None -> 
+        let cols = toColumns rows 
+        match tryFindAlternativeSymmetry cols with 
+        | Some colIndex -> colIndex
+        | None -> 0 
+
 let run fileName =
     let chunks = readChunks fileName
     chunks |> List.map countSymmetries |> List.sum |> printfn "%d"
+    chunks |> List.map countSymmetriesWithSmudge |> List.sum |> printfn "%d"
 
 "input" |> run
