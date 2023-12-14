93d035e7d35832db7bcc6f92dad418ae1640181b
Einar W. Høst <einarwh@gmail.com>
2023-12-14 11:19:45 +0100

    south

--- 2023/14/aoc14.fsx
+++ 2023/14/aoc14.fsx
@@ -23,9 +23,31 @@ let rec rollNorth (lines : string list) : string list =
         a' :: rollNorth (b' :: rest)
     | _ -> lines 
 
-let rec tiltNorth (current : string list) : string list = 
-    let tilted = rollNorth current 
-    if tilted = current then current else tiltNorth tilted 
+let rollSouthStep (s1, s2) = 
+    let line1 = s1 |> Seq.toList
+    let line2 = s2 |> Seq.toList
+    let zipped = List.zip line1 line2 
+    let rolled = zipped |> List.map (fun (a, b) -> if a = 'O' && b = '.' then (b, a) else (a, b))
+    let s1' = rolled |> List.map fst |> List.toArray |> String
+    let s2' = rolled |> List.map snd |> List.toArray |> String
+    (s1', s2')
+
+let rec rollSouth (lines : string list) : string list = 
+    let folder upper acc = 
+        match acc with 
+        | [] -> [ upper ]
+        | lower :: rest -> 
+            let (upper', lower') = rollSouthStep (upper, lower)
+            upper' :: lower' :: rest
+    List.foldBack folder lines []
+
+let rec tilt roll current = 
+    let tilted = roll current 
+    if tilted = current then current else tilt roll tilted 
+
+let rec tiltNorth = tilt rollNorth 
+
+let rec tiltSouth = tilt rollSouth
 
 let countRocks (line : string) : int = 
     line |> Seq.filter ((=) 'O') |> Seq.length 
@@ -36,7 +58,12 @@ let calculateLoad (lines : string list) =
     
 let run fileName =
     let lines = readLines fileName |> Array.toList
-    let tilted = lines |> tiltNorth
-    tilted |> calculateLoad |> printfn "%d"
+    let tiltedNorth = lines |> tiltNorth
+    printfn "tilted north"
+    tiltedNorth |> calculateLoad |> printfn "%d"
+    printfn "tilted south"
+    let tiltedSouth = lines |> tiltSouth
+    tiltedSouth |> List.iter (printfn "%A")
+    tiltedSouth |> calculateLoad |> printfn "%d"
 
-"input" |> run
+"sample" |> run
