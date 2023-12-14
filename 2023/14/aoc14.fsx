58027aa1d49a0694cfc00286ea9b4bedec91fdc9
Einar W. Høst <einarwh@gmail.com>
2023-12-14 11:57:57 +0100

    foo

--- 2023/14/aoc14.fsx
+++ 2023/14/aoc14.fsx
@@ -41,13 +41,63 @@ let rec rollSouth (lines : string list) : string list =
             upper' :: lower' :: rest
     List.foldBack folder lines []
 
+let rec rollEastStep (chars : char list) : char list = 
+    match chars with 
+    | 'O' :: '.' :: rest -> '.' :: rollEastStep ('O' :: rest)
+    | ch :: rest -> ch :: rollEastStep rest 
+    | [] -> []
+
+let rollLineEast (s : string) : string = 
+    let cs = s |> Seq.toList 
+    rollEastStep cs |> List.toArray |> String
+
+let rollEast (lines : string list) : string list = 
+    lines |> List.map rollLineEast
+
+let rec rollWestStep (chars : char list) : char list = 
+    let folder (left : char) (acc : char list) : char list = 
+        match acc with 
+        | [] -> [ left ]
+        | right :: rest -> 
+            match (left, right) with 
+            | ('.', 'O') -> 'O' :: '.' :: rest 
+            | _ -> left :: acc 
+    List.foldBack folder chars []
+
+let rollLineWest (s : string) : string = 
+    let cs = s |> Seq.toList 
+    rollWestStep cs |> List.toArray |> String
+
+let rollWest (lines : string list) : string list = 
+    lines |> List.map rollLineWest
+
 let rec tilt roll current = 
     let tilted = roll current 
     if tilted = current then current else tilt roll tilted 
 
-let rec tiltNorth = tilt rollNorth 
+let tiltNorth = tilt rollNorth 
 
-let rec tiltSouth = tilt rollSouth
+let tiltSouth = tilt rollSouth
+
+let tiltEast = tilt rollEast
+
+let tiltWest = tilt rollWest 
+
+let cycle = tiltNorth >> tiltWest >> tiltSouth >> tiltEast
+
+let solve limit lines = 
+    let rec loop n seen current = 
+        if n >= limit then 
+            (n, -1, seen) 
+        else 
+            if List.contains current seen then 
+                let ix = List.findIndex ((=) current) seen 
+                (n, ix, current :: seen) 
+            else 
+                let seen' = current :: seen
+                let next = cycle current 
+                loop (n + 1) seen' next 
+    loop 0 [] lines 
 
 let countRocks (line : string) : int = 
     line |> Seq.filter ((=) 'O') |> Seq.length 
@@ -56,14 +106,43 @@ let calculateLoad (lines : string list) =
     let len = lines |> List.length 
     lines |> List.mapi (fun i line -> (len - i) * countRocks line) |> List.sum
     
+let printPattern (i : int) (pattern : string list) = 
+    printfn "%d" i
+    pattern |> List.iter (printfn "%A")
+
 let run fileName =
     let lines = readLines fileName |> Array.toList
-    let tiltedNorth = lines |> tiltNorth
+
     printfn "tilted north"
-    tiltedNorth |> calculateLoad |> printfn "%d"
+    let tiltedNorth = lines |> tiltNorth
+    // tiltedNorth |> calculateLoad |> printfn "%d"
+    // tiltedNorth |> List.iter (printfn "%A")
+
     printfn "tilted south"
     let tiltedSouth = lines |> tiltSouth
-    tiltedSouth |> List.iter (printfn "%A")
-    tiltedSouth |> calculateLoad |> printfn "%d"
+    // tiltedSouth |> List.iter (printfn "%A")
+    // tiltedSouth |> calculateLoad |> printfn "%d"
+
+    printfn "tilted east"
+    let tiltedEast = lines |> tiltEast
+    // tiltedEast |> List.iter (printfn "%A")
+    // tiltedEast |> calculateLoad |> printfn "%d"
+    
+    printfn "tilted west"
+    let tiltedWest = lines |> tiltWest
+    // tiltedWest |> List.iter (printfn "%A")
+    // tiltedWest |> calculateLoad |> printfn "%d"
+
+    printfn "cycle 1"
+    let cycle1 = lines |> cycle 
+    // cycle1 |> List.iter (printfn "%A")
+
+    let foo = solve 1000 lines 
+    foo |> printfn "%A"
+    let (lastIndex, firstIndex, seen) = foo 
+    printfn "last index: %d" lastIndex 
+    printfn "first index: %d" firstIndex 
+    seen |> List.iteri (printPattern)
+
 
 "sample" |> run
