eb382272b93e9549cbc314c10d4f4ff5ed279fc8
Einar W. Høst <einarwh@gmail.com>
2023-12-14 07:55:08 +0100

    14 start

---
+++ 2023/14/aoc14.fsx
@@ -0,0 +1,39 @@
+// Advent of Code 2023. Day 13: Point of Incidence
+// dotnet fsi aoc13.fsx
+
+open System
+open System.IO
+
+let readLines =
+    File.ReadAllLines >> Array.filter ((<>) String.Empty)
+
+let rollNorthStep (s1, s2) = 
+    let line1 = s1 |> Seq.toList
+    let line2 = s2 |> Seq.toList
+    let zipped = List.zip line1 line2 
+    let rolled = zipped |> List.map (fun (a, b) -> if a = '.' && b = 'O' then (b, a) else (a, b))
+    let s1' = rolled |> List.map fst |> List.toArray |> String
+    let s2' = rolled |> List.map snd |> List.toArray |> String
+    (s1', s2')
+
+let rec tiltNorth (lines : string list) : string list = 
+    let folder (upper : string) (acc : string list) : string list = 
+        match acc with 
+        | [] -> [ upper ]
+        | lower :: rest -> 
+            let (upper', lower') = rollNorthStep (upper, lower)
+            upper' :: lower' :: rest 
+    List.foldBack folder lines []
+
+let run fileName =
+    let upper = "O....#...."
+    let lower = "O.OO#....#"
+    rollNorthStep (upper, lower) |> printfn "%A"
+    let lines = readLines fileName |> Array.toList
+    printfn "original"
+    lines |> List.iter (printfn "%A")
+    let tilted = lines |> tiltNorth
+    printfn "tilted"
+    tilted |> List.iter (printfn "%A")
+
+"sample" |> run
