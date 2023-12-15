406b3ae8c0d92aace4a75c4789d7942323cbae71
Einar W. Høst <einarwh@gmail.com>
2023-12-15 08:40:48 +0100

    Update aoc15.fsx

--- 2023/15/aoc15.fsx
+++ 2023/15/aoc15.fsx
@@ -4,16 +4,76 @@
 open System
 open System.IO
 
-let readLines =
-    File.ReadAllLines >> Array.filter ((<>) String.Empty)
+type Op = 
+    | Insert of (string * int)
+    | Remove of string
 
-let hash (s : string) : int = 
+let readText fileName = 
+    let text = File.ReadAllText fileName 
+    text.TrimEnd()
+
+let parseOp (s : string) = 
+    if s.EndsWith('-') then 
+        let label = s.Substring(0, s.Length - 1)
+        Remove label
+    else 
+        let ss = s.Split("=")
+        let label = ss[0] 
+        let lens = int ss[1]
+        Insert (label, lens)
+
+let parseAllOps (s : string) = 
+    s.Split(",") |> Array.toList |> List.map parseOp
+
+let getHash (s : string) : int = 
     let folder current ch  = 
         ((current + int ch) * 17) % 256
     s |> Seq.toList |> List.fold folder 0
 
-let run fileName =
-    let lines = readLines fileName |> Array.toList
-    "cm-" |> hash |> printfn "%d"
+let part1 (input : string) = 
+    input.Split(",") |> Array.sumBy getHash
 
-"sample" |> run
+let executeOp (hashmap : Map<int, (string * int) list>) (op : Op) = 
+    match op with 
+    | Remove label -> 
+        let remove (maybe : (string * int) list option) = 
+            maybe |> Option.map (fun lenses -> lenses |> List.filter (fun (l, _) -> l <> label))
+        let hash = getHash label 
+        hashmap |> Map.change hash remove
+    | Insert (label, focal) -> 
+        let insert maybe = 
+            match maybe with 
+            | Some lenses ->
+                if List.exists (fun (l, _) -> l = label) lenses then 
+                    lenses 
+                    |> List.map (fun (l, f) -> if l = label then (l, focal) else (l, f))
+                    |> Some 
+                else 
+                    Some (lenses @ [label, focal])
+            | None -> Some [label, focal]
+        let hash = getHash label 
+        hashmap |> Map.change hash insert 
+
+let rec execute (ops : Op list) (hashmap : Map<int, (string * int) list>) : Map<int, (string * int) list> = 
+    match ops with 
+    | [] -> hashmap 
+    | op :: rest -> 
+        (executeOp hashmap op) |> execute rest 
+
+let part2 (input : string) = 
+    let calculate (boxNo : int, lenses : (string * int) list) = 
+        lenses |> List.map snd |> List.mapi (fun i focal -> (boxNo + 1) * (i + 1) * focal)
+    let ops = input |> parseAllOps 
+    Map.empty 
+    |> execute ops 
+    |> Map.toList 
+    |> List.collect calculate
+    |> List.sum 
+
+let run fileName =
+    let input = readText fileName
+    input |> part1 |> printfn "%d"
+    input |> part2 |> printfn "%d"
+    "."
+    
+"input" |> run
