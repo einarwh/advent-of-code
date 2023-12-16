21a0ddb7b92584f109c61fb6d3b79fbb809aaee3
Einar W. Høst <einarwh@gmail.com>
2023-12-16 10:33:22 +0100

    Update aoc16.fsx

--- 2023/16/aoc16.fsx
+++ 2023/16/aoc16.fsx
@@ -22,45 +22,34 @@ let continueInDir dir (x, y) =
     | S -> (x, y + 1)
     | E -> (x + 1, y)
 
-let rec reflect (pos : int*int) (dir : Dir) (grid : char[,]) energized seen =
+let rec reflect (pos : int*int) (dir : Dir) (grid : char[,]) seen =
     let (x, y) = pos
     let xLimit = Array2D.getColumnCount grid
     let yLimit = Array2D.getRowCount grid
     if Set.contains (pos, dir) seen then
-        (energized, seen)
+        seen
     elif x >= 0 && x < xLimit && y >= 0 && y < yLimit then
-        let energized = energized |> Set.add pos
         let seen = seen |> Set.add (pos, dir)
         let ch = Array2D.get grid y x
         match ch with
         | '.' ->
-            // Continue in same direction.
             let nextPos = continueInDir dir pos
-            reflect nextPos dir grid energized seen
+            reflect nextPos dir grid seen
         | '|' when dir = N || dir = S ->
-            // Continue in same direction.
             let nextPos = continueInDir dir pos
-            reflect nextPos dir grid energized seen
+            reflect nextPos dir grid seen
         | '|' ->
-            // Split in directions N and S.
             let northPos = continueInDir N pos
             let southPos = continueInDir S pos
-            let (energized, seen) = reflect northPos N grid energized seen
-            let (energized, seen) = reflect southPos S grid energized seen
-            (energized, seen)
+            seen |> reflect northPos N grid |> reflect southPos S grid 
         | '-' when dir = W || dir = E ->
-            // Continue in same direction.
             let nextPos = continueInDir dir pos
-            reflect nextPos dir grid energized seen
+            reflect nextPos dir grid seen
         | '-' ->
-            // Split in directions W and E.
             let westPos = continueInDir W pos
             let eastPos = continueInDir E pos
-            let (energized, seen) = reflect westPos W grid energized seen
-            let (energized, seen) = reflect eastPos E grid energized seen
-            (energized, seen)
+            seen |> reflect westPos W grid |> reflect eastPos E grid
         | '/' ->
-            // Continue in reflected direction.
             let newDir =
                 match dir with
                 | N -> E
@@ -68,9 +57,8 @@ let rec reflect (pos : int*int) (dir : Dir) (grid : char[,]) energized seen =
                 | S -> W
                 | E -> N
             let nextPos = continueInDir newDir pos
-            reflect nextPos newDir grid energized seen
+            reflect nextPos newDir grid seen
         | '\\' ->
-            // Continue in reflected direction.
             let newDir =
                 match dir with
                 | N -> W
@@ -78,15 +66,15 @@ let rec reflect (pos : int*int) (dir : Dir) (grid : char[,]) energized seen =
                 | S -> E
                 | E -> S
             let nextPos = continueInDir newDir pos
-            reflect nextPos newDir grid energized seen
+            reflect nextPos newDir grid seen
         | _ -> failwith <| sprintf "%c?" ch
     else
-        (energized, seen)
+        seen
 
 let findBest grid dir startPositions = 
     startPositions 
-    |> List.map (fun pos -> reflect pos dir grid Set.empty Set.empty)
-    |> List.map fst 
+    |> List.map (fun pos -> reflect pos dir grid Set.empty)
+    |> List.map (Set.map fst)
     |> List.map Set.count
     |> List.sortDescending
     |> List.head 
@@ -108,8 +96,8 @@ let solve grid =
 let run fileName =
     let lines = readLines fileName
     let grid = array2D lines
-    let (energized, _) = reflect (0, 0) E grid Set.empty Set.empty
-    energized |> Set.count |> printfn "%d"
+    let seen = reflect (0, 0) E grid Set.empty
+    seen |> Set.map fst |> Set.count |> printfn "%d"
     solve grid |> printfn "%d"
 
 "input" |> run
