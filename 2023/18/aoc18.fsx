c8a897610b17c509f5cdfb4d907f63ef4d4e54a1
Einar W. Høst <einarwh@gmail.com>
2023-12-18 16:43:10 +0100

    Update aoc18.fsx

--- 2023/18/aoc18.fsx
+++ 2023/18/aoc18.fsx
@@ -8,8 +8,6 @@ type Direction = U | D | L | R
 
 type Pos = { x : int64; y : int64 }
 
-type Vector = { start : Pos; stop : Pos }
-
 type Instruction = {
     dir : Direction
     meters : int64
@@ -31,31 +29,28 @@ let directionFromNumber (n : int) =
     | 3 -> U
     | _ -> failwith <| sprintf "%d?" n
 
-let parseHexCode (s : string) =
-    let code = s.Substring(2, 5)
-    let meters = Convert.ToInt32(code, 16)
-    let dir = s.Substring(7, 1) |> int |> directionFromNumber
-    { dir = dir
-      meters = meters }
-
 let parseInstruction (s : string) =
     let parts = s.Split(" ")
     { dir = directionFromString parts[0]
       meters = int parts[1] }
 
-let parseLine (s : string) =
+let parseHexCode (s : string) =
+    let parse (s : string) =
+        let code = s.Substring(2, 5)
+        let meters = Convert.ToInt32(code, 16)
+        let dir = s.Substring(7, 1) |> int |> directionFromNumber
+        { dir = dir
+          meters = meters }
     let parts = s.Split(" ")
-    parseHexCode parts[2]
-
-let toStep dir = 
-    match dir with 
-    | U -> (0L, -1L)
-    | D -> (0L, 1L)
-    | L -> (-1L, 0L)
-    | R -> (1L, 0L)
+    parse parts[2]
 
 let getNextPos pos dir meters = 
-    let (xStep, yStep) = toStep dir 
+    let (xStep, yStep) = 
+        match dir with 
+        | U -> (0L, -1L)
+        | D -> (0L, 1L)
+        | L -> (-1L, 0L)
+        | R -> (1L, 0L)
     { x = pos.x + xStep * meters
       y = pos.y + yStep * meters }
 
@@ -69,30 +64,24 @@ let toPositions instructions =
     let startPos = { x = 0; y = 0 }
     loop startPos instructions [ startPos ] 
 
-let readLines =
-    File.ReadAllLines >> Array.filter ((<>) String.Empty)
-
-let determinant (p1 : Pos, p2 : Pos) =
+let determinant (p1, p2) =
     p1.x * p2.y - p2.x * p1.y
 
 let shoelaceArea positions = 
     positions |> List.pairwise |> List.map determinant |> List.sum |> (fun s -> s / 2L)
 
-let solve instructions = 
-    let positions = instructions |> toPositions
-    let area = shoelaceArea positions
+let solve parser lines  = 
+    let instructions = lines |> List.map parser
+    let area = instructions |> toPositions |> shoelaceArea
     let perimeter = instructions |> List.sumBy (fun x -> x.meters) 
     area + perimeter / 2L + 1L 
 
-let part1 lines = 
-    lines |> List.map parseInstruction |> solve 
-
-let part2 lines =
-    lines |> List.map parseLine |> solve
+let readLines =
+    File.ReadAllLines >> Array.filter ((<>) String.Empty)
 
 let run fileName =
     let lines = readLines fileName |> Array.toList
-    lines |> part1 |> printfn "%d"
-    lines |> part2 |> printfn "%d"
+    lines |> solve parseInstruction |> printfn "%d"
+    lines |> solve parseHexCode |> printfn "%d"
 
 "input" |> run
