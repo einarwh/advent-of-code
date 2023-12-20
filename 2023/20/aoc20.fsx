eb51c24255b03c441d33edcbec177f7cbcb87e76
Einar W. Høst <einarwh@gmail.com>
2023-12-20 11:14:07 +0100

    just started

--- 2023/20/aoc20.fsx
+++ 2023/20/aoc20.fsx
@@ -4,6 +4,48 @@
 open System
 open System.IO
 
+type Pulse = High | Low 
+
+type Label = string 
+
+[<AutoOpen>]
+module ModuleTypes =
+
+    type FlipFlopState = On | Off 
+
+    type FlipFlop = {
+        label : Label 
+        state : FlipFlopState
+    }
+
+    type ConjunctionState = Map<Label, Pulse>
+
+    type Conjunction = {
+        label : Label 
+        state : ConjunctionState 
+        inputs : Label list 
+        outputs : Label list 
+    }
+
+    type Broadcaster = Broadcaster 
+
+
+module FlipFlop = 
+
+    let receive (flipFlop : FlipFlop) (pulse : Pulse) = 
+        match pulse with 
+        | High -> (flipFlop, [])
+        | Low -> 
+            match flipFlop.state with 
+            | On -> ({ flipFlop with state = Off }, Low)
+            | Off -> ({ flipFlop with state = On }, High)
+
+module Conjunction = 
+
+    let receive (conjunction : Conjunction) (sender : Label) (pulse : Pulse) = 
+        let state = conjunction.state |> Map.add 
+
+
 let readLines =
     File.ReadAllLines >> Array.filter ((<>) String.Empty)
 
