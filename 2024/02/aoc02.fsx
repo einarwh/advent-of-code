3e0935128bf8a04d3a20449420a7cd100d84f3b8
Einar W. Høst <einarwh@gmail.com>
2024-12-02 08:38:51 +0100

    Update aoc02.fsx

--- 2024/02/aoc02.fsx
+++ 2024/02/aoc02.fsx
@@ -4,14 +4,26 @@
 open System
 open System.IO
 
-let parseReport (s : string) : int array = 
-    s.Split() |> Array.map int  
+let parseReport (s : string) : int list = 
+    s.Split() |> Array.toList |> List.map int  
 
-let isSafe (report : int array) : bool = 
-    let diffs = report |> Array.pairwise |> Array.map (fun (a, b) -> a - b)
-    let safeIncreasing = diffs |> Array.forall (fun d -> d >= 1 && d <= 3)
-    let safeDecreasing = diffs |> Array.forall (fun d -> d >= -3 && d <= -1)
+let isSafe (report : int list) : bool = 
+    let diffs = report |> List.pairwise |> List.map (fun (a, b) -> a - b)
+    let safeIncreasing = diffs |> List.forall (fun d -> d >= 1 && d <= 3)
+    let safeDecreasing = diffs |> List.forall (fun d -> d >= -3 && d <= -1)
     safeIncreasing || safeDecreasing
+
+let permute (report : int list) : int list list = 
+    let len = report |> List.length 
+    let indexes = [0 .. len - 1]
+    let indexedReport = report |> List.indexed
+    indexes |> List.map (fun i -> indexedReport |> List.choose (fun (ix, n) -> if i = ix then None else Some n))
+
+let isSafeWithDampener (report : int list) : bool = 
+    report |> permute |> List.exists isSafe
+
+let isSafeLenient (report : int list) : bool = 
+    isSafe report || isSafeWithDampener report 
 
 let readLines = 
     File.ReadAllLines
@@ -22,5 +34,6 @@ let run fileName =
     let lines = readLines fileName
     let reports = lines |> List.map parseReport 
     reports |> List.filter isSafe |> List.length |> printfn "%d" 
+    reports |> List.filter isSafeLenient |> List.length |> printfn "%d" 
 
 run "input"
