62fa4f0059c28acc13f18b6eca44ae8e75d45816
Einar W. Høst <einarwh@gmail.com>
2024-12-03 07:39:55 +0100

    Update aoc03.fsx

--- 2024/03/aoc03.fsx
+++ 2024/03/aoc03.fsx
@@ -5,21 +5,26 @@ open System
 open System.IO
 open System.Text.RegularExpressions
 
-let mul (m : Match) : int = 
-    let read (ix : int) = m.Groups.[ix].Value |> int
-    read 1 * read 2
+type op = 
+    | Mul of (int * int)
+    // | Do 
+    // | Dont 
 
-let parse (line : string) = 
+let mul (m : Match) : op = 
+    let read (ix : int) = m.Groups.[ix].Value |> int
+    Mul (read 1, read 2)
+
+let parse (line : string) : op list = 
     let pattern = "mul\((\d+),(\d+)\)"
     let matches = Regex.Matches(line, pattern)
-    matches |> Seq.map mul |> Seq.sum 
+    matches |> Seq.toList |> List.map mul  
 
 let readLines = 
     File.ReadAllText
 
 let run fileName = 
     let text = File.ReadAllText fileName
-    let foo = parse text 
-    foo |> printfn "%A"
+    let ops = parse text 
+    ops |> List.map (fun (Mul (a, b)) -> a * b) |> List.sum |> printfn "%d"
 
 run "input"
