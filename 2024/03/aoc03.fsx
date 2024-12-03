7102a7ae1c1aeb03db3ce209cf5a370200cc5529
Einar W. Høst <einarwh@gmail.com>
2024-12-03 09:10:58 +0100

    pattern

--- 2024/03/aoc03.fsx
+++ 2024/03/aoc03.fsx
@@ -7,17 +7,22 @@ open System.Text.RegularExpressions
 
 type op = 
     | Mul of (int * int)
-    // | Do 
-    // | Dont 
+    | Do 
+    | Dont 
 
 let mul (m : Match) : op = 
     let read (ix : int) = m.Groups.[ix].Value |> int
     Mul (read 1, read 2)
 
+let toOp (m : Match) : op = 
+    let name = m.Groups.[1].Value
+    printfn "%A" name
+    Do 
+
 let parse (line : string) : op list = 
-    let pattern = "mul\((\d+),(\d+)\)"
+    let pattern = "((mul)\((\d+)\,(\d+)\)|(do)\(\)|(don\'t)\(\))"
     let matches = Regex.Matches(line, pattern)
-    matches |> Seq.toList |> List.map mul  
+    matches |> Seq.toList |> List.map toOp  
 
 let readLines = 
     File.ReadAllText
@@ -25,6 +30,8 @@ let readLines =
 let run fileName = 
     let text = File.ReadAllText fileName
     let ops = parse text 
-    ops |> List.map (fun (Mul (a, b)) -> a * b) |> List.sum |> printfn "%d"
+    printfn "%A" ops
 
-run "input"
+    // ops |> List.map (fun (Mul (a, b)) -> a * b) |> List.sum |> printfn "%d"
+
+run "sample2"
