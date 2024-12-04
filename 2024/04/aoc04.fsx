9a1cbc48e29095da26268a1f3a30e94cac42cdef
Einar W. Høst <einarwh@gmail.com>
2024-12-04 08:16:02 +0100

    04 part 1

--- 2024/04/aoc04.fsx
+++ 2024/04/aoc04.fsx
@@ -5,13 +5,38 @@ open System
 open System.IO
 
 module Array2D = 
-    let tryGet (a : 'a[,]) index1 index2 = 
-        let first = index1 >= 0 && index1 < a.GetLength(0)
-        let second = index2 >= 0 && index2 < a.GetLength(1)
+    let tryGet (a : 'a[,]) (x, y) = 
+        let first = y >= 0 && y < a.GetLength(0)
+        let second = x >= 0 && x < a.GetLength(1)
         if first && second then 
-            Some <| Array2D.get a index1 index2
+            Some <| Array2D.get a y x
         else 
             None 
+
+type Pos = int * int 
+
+let findXmas (board : char [,]) (xpos : Pos) (move : Pos -> Pos) = 
+    let mpos = move xpos
+    let apos = move mpos 
+    let spos = move apos 
+    let word = 
+        [ Array2D.tryGet board xpos  
+          Array2D.tryGet board mpos 
+          Array2D.tryGet board apos 
+          Array2D.tryGet board spos ]
+        |> List.choose id
+    if word = ['X'; 'M'; 'A'; 'S'] then 1 else 0
+
+let findAllDirections (board : char [,]) (x : int, y : int) = 
+    let nn = findXmas board (x, y) (fun (x, y) -> (x, y-1))
+    let nw = findXmas board (x, y) (fun (x, y) -> (x-1, y-1))
+    let ww = findXmas board (x, y) (fun (x, y) -> (x-1, y))
+    let sw = findXmas board (x, y) (fun (x, y) -> (x-1, y+1))
+    let ss = findXmas board (x, y) (fun (x, y) -> (x, y+1))
+    let se = findXmas board (x, y) (fun (x, y) -> (x+1, y+1))
+    let ee = findXmas board (x, y) (fun (x, y) -> (x+1, y))
+    let ne = findXmas board (x, y) (fun (x, y) -> (x+1, y-1))
+    [nn; nw; ww; sw; ss; se; ee; ne] |> List.sum
 
 let readLines = 
     File.ReadAllLines
@@ -22,7 +47,11 @@ let run fileName =
     let lines = readLines fileName
     let width = lines |> List.head |> Seq.length 
     let height = lines |> List.length 
-    let board = Array2D.init height width (fun i j -> lines.[i].[j])
-    board |> printfn "%A"
+    let board = Array2D.init height width (fun y x -> lines.[y].[x])
+    let positions = [for x in [0..width-1] do for y in [0..height-1] -> (x, y)]
+    positions 
+    |> List.map (findAllDirections board)
+    |> List.sum 
+    |> printfn "%d"
 
-run "sample"
+run "input"
