f3582f3bf7ec8efc5a1bccb6796bffb108c7edbb
Einar W. Høst <einarwh@gmail.com>
2024-12-07 19:00:49 +0100

    Slow.

--- 2024/06/alt.fsx
+++ 2024/06/alt.fsx
@@ -83,8 +83,9 @@ let generateBoards visited board =
     visited |> Set.toList |> List.map (addObstruction board)
 
 let hasLoop (intervalsByRow : (Boundary*Boundary) list array) (intervalsByCol : (Boundary*Boundary) list array) startPos board = 
-    printfn "---------------------"
-    printfn "Check board for loop!"
+    // printf "."
+    // printfn "---------------------"
+    // printfn "Check board for loop!"
     let between (n : int) (startBoundary : Boundary, stopBoundary : Boundary) : bool =
         match (startBoundary, stopBoundary) with 
         | Outside, Obstacle upper -> n <= upper 
@@ -92,114 +93,114 @@ let hasLoop (intervalsByRow : (Boundary*Boundary) list array) (intervalsByCol : 
         | Obstacle lower, Outside -> lower <= n 
         | Outside, Outside -> true 
     let tryFindLargest (n : int) (intervals : (Boundary*Boundary) list) : int option = 
-        printfn "tryFindLargest: %d - %A" n intervals
+        // printfn "tryFindLargest: %d - %A" n intervals
         match intervals |> List.find (between n) with 
         | Outside, Obstacle upper -> Some (upper - 1)  
         | Obstacle lower, Obstacle upper -> Some (upper - 1) 
         | Obstacle lower, Outside -> None  
         | _ -> None 
     let tryFindSmallest (n : int) (intervals : (Boundary*Boundary) list) : int option = 
-        printfn "tryFindSmallest: %d - %A" n intervals
+        // printfn "tryFindSmallest: %d - %A" n intervals
         match intervals |> List.find (between n) with 
         | Outside, Obstacle upper -> None   
         | Obstacle lower, Obstacle upper -> Some (lower + 1) 
         | Obstacle lower, Outside -> Some (lower + 1)  
         | _ -> None 
     let rec walk route visited dir (pos : int*int) =
-        printfn ""
-        printfn "At %A facing %A having visited %A" pos dir visited
+        // printfn ""
+        // printfn "At %A facing %A having visited %A" pos dir visited
         let (x, y) = pos  
         if Set.contains (pos, dir) visited then 
-            printfn "Found loop!"
+            // printfn "Found loop!"
             true 
         else 
             // Find next obstruction in the given direction.
             // There might not be one -> escape, and there is no loop.
             match dir with 
             | (0, -1) -> 
-                printfn "Moving N"
+                // printfn "Moving N"
                 match tryFindSmallest y intervalsByCol.[x] with 
                 | None -> 
-                    printfn "Found no obstacle - exiting. No loop."
+                    // printfn "Found no obstacle - exiting. No loop."
                     false 
                 | Some smallest -> 
-                    printfn "Found obstacle at %A" (x, smallest - 1)
+                    // printfn "Found obstacle at %A" (x, smallest - 1)
                     let nextP = (x, smallest)
-                    printfn "Move to %A" nextP
+                    // printfn "Move to %A" nextP
                     let movesList = [smallest .. y] |> List.map (fun i -> ((x, i), dir)) |> List.rev
                     let moves = movesList |> Set.ofList
-                    printfn "Add moves %A" moves
+                    // printfn "Add moves %A" moves
                     let overlap = Set.intersect moves visited 
                     if Set.isEmpty overlap then 
-                        printfn "No overlap!"
+                        // printfn "No overlap!"
                         let joined = Set.union moves visited 
                         walk (route @ movesList) joined (turnRight dir) nextP
                     else 
-                        printfn "Overlap found -> there was a loop!"
+                        // printfn "Overlap found -> there was a loop!"
                         true 
             | (-1, 0) -> 
-                printfn "Moving W"
+                // printfn "Moving W"
                 match tryFindSmallest x intervalsByRow.[y] with 
                 | None ->
-                    printfn "Found no obstacle - exiting. No loop."
+                    // printfn "Found no obstacle - exiting. No loop."
                     false
                 | Some xSmallest -> 
-                    printfn "Found obstacle at %A" (xSmallest - 1, y)
+                    // printfn "Found obstacle at %A" (xSmallest - 1, y)
                     let nextP = (xSmallest, y)
-                    printfn "Move to %A" nextP
+                    // printfn "Move to %A" nextP
                     let movesList = [xSmallest .. x] |> List.map (fun i -> ((i, y), dir)) |> List.rev
                     let moves = movesList |> Set.ofList
-                    printfn "Add moves %A" moves
+                    // printfn "Add moves %A" moves
                     let overlap = Set.intersect moves visited 
                     if Set.isEmpty overlap then 
-                        printfn "No overlap!"
+                        // printfn "No overlap!"
                         let joined = Set.union moves visited 
                         walk (route @ movesList) joined (turnRight dir) nextP
                     else 
-                        printfn "Overlap found -> there was a loop!"
+                        // printfn "Overlap found -> there was a loop!"
                         true 
             | (0, 1) -> 
-                printfn "Moving S"
+                // printfn "Moving S"
                 match tryFindLargest y intervalsByCol.[x] with 
                 | None -> 
-                    printfn "Found no obstacle - exiting. No loop."
-                    route |> List.rev |> List.iter (printfn "%A") 
+                    // printfn "Found no obstacle - exiting. No loop."
+                    // route |> List.rev |> List.iter (printfn "%A") 
                     false 
                 | Some yLargest -> 
-                    printfn "Found obstacle at %A" (x, yLargest + 1)
+                    // printfn "Found obstacle at %A" (x, yLargest + 1)
                     let nextP = (x, yLargest)
-                    printfn "Move to %A" nextP
+                    // printfn "Move to %A" nextP
                     let movesList = [y .. yLargest] |> List.map (fun i -> ((x, i), dir)) 
                     let moves = movesList |> Set.ofList
-                    printfn "Add moves %A" moves
+                    // printfn "Add moves %A" moves
                     let overlap = Set.intersect moves visited 
                     if Set.isEmpty overlap then 
-                        printfn "No overlap!"
+                        // printfn "No overlap!"
                         let joined = Set.union moves visited 
                         walk (route @ movesList) joined (turnRight dir) nextP
                     else 
-                        printfn "Overlap found -> there was a loop!"
+                        // printfn "Overlap found -> there was a loop!"
                         true 
             | (1, 0) -> 
-                printfn "Moving E"
+                // printfn "Moving E"
                 match tryFindLargest x intervalsByRow.[y] with 
                 | None -> 
-                    printfn "Found no obstacle - exiting. No loop."
+                    // printfn "Found no obstacle - exiting. No loop."
                     false 
                 | Some xLargest -> 
-                    printfn "Found obstacle at %A" (xLargest + 1, y)
+                    // printfn "Found obstacle at %A" (xLargest + 1, y)
                     let nextP = (xLargest, y)
-                    printfn "Move to %A" nextP
+                    // printfn "Move to %A" nextP
                     let movesList = [x .. xLargest] |> List.map (fun i -> ((i, y), dir))
                     let moves = movesList |> Set.ofList
-                    printfn "Add moves %A" moves
+                    // printfn "Add moves %A" moves
                     let overlap = Set.intersect moves visited 
                     if Set.isEmpty overlap then 
-                        printfn "No overlap!"
+                        // printfn "No overlap!"
                         let joined = Set.union moves visited 
                         walk (route @ movesList) joined (turnRight dir) nextP
                     else 
-                        printfn "Overlap found -> there was a loop!"
+                        // printfn "Overlap found -> there was a loop!"
                         true 
     walk [] Set.empty (0, -1) startPos
 
@@ -219,7 +220,7 @@ let createIntervals (obstructions : int list) =
         [ Outside, Obstacle first ] @ inner @ [ Obstacle last, Outside ]
 
 let hasLoopWithAddedObstruction (obsByRow : int list array) (obsByCol : int list array) startPos (basicBoard : char[,]) (x, y) = 
-    printfn "================"
+    // printfn "================"
     let add index value obstructions = 
         let result = Array.copy obstructions
         Array.set result index (insertSorted value result.[index]) 
@@ -229,7 +230,7 @@ let hasLoopWithAddedObstruction (obsByRow : int list array) (obsByCol : int list
     let intervalsByRow = byRow |> Array.map createIntervals
     let intervalsByCol = byCol |> Array.map createIntervals
     let board = addObstruction basicBoard (x, y)
-    printfn "Adding obstruction at %A" (x, y)  
+    // printfn "Adding obstruction at %A" (x, y)  
     hasLoop intervalsByRow intervalsByCol startPos board 
 
 let run fileName = 
@@ -254,10 +255,10 @@ let run fileName =
     // Part 2
     let sw = Stopwatch.StartNew()
     let route = board |> traceRoute startPos
-    printfn "EXISTING %A" obstructions
-    printfn "ROUTE %A" route
+    // printfn "EXISTING %A" obstructions
+    // printfn "ROUTE %A" route
     let allCandidates = visited |> Set.toList 
-    printfn "ALL CANDIDATES %A" allCandidates    
+    // printfn "ALL CANDIDATES %A" allCandidates    
 
     let extraObs = (3, 6)
     // hasLoopWithAddedObstruction obsByRow obsByCol startPos board extraObs
@@ -268,4 +269,4 @@ let run fileName =
     |> printfn "%d"
     printfn "%A" sw.Elapsed
 
-run "sample"
+run "input"
