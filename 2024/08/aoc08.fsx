8aa87d6378b7a9bb093ab70dfd674db3f5d75dfb
Einar W. Høst <einarwh@gmail.com>
2024-12-08 09:57:46 +0100

    works?

--- 2024/08/aoc08.fsx
+++ 2024/08/aoc08.fsx
@@ -5,10 +5,12 @@ open System
 open System.IO
 
 module Array2D = 
-    let tryGet (a : 'a[,]) (x, y) = 
+    let inBounds (a : 'a[,]) (x, y) = 
         let first = y >= 0 && y < a.GetLength(0)
         let second = x >= 0 && x < a.GetLength(1)
-        if first && second then Some (Array2D.get a y x) else None
+        first && second
+    let tryGet (a : 'a[,]) (x, y) = 
+        if inBounds a (x, y) then Some (Array2D.get a y x) else None
     let positions (a : 'a[,]) = 
         let rowCount = a.GetLength(0)
         let colCount = a.GetLength(1)
@@ -23,10 +25,20 @@ let readLines =
     >> Array.filter (fun line -> line <> String.Empty)
     >> Array.toList
 
-let findAntinodes ((x1, y1), (x2, y2)) = 
+let findAntinodes board ((x1, y1), (x2, y2)) = 
     let xd = x2 - x1 
     let yd = y2 - y1
-    [ (x1 - xd, y1 - yd); (x2 + xd, y2 + yd) ] 
+    [ (x1 - xd, y1 - yd); (x2 + xd, y2 + yd) ] |> List.filter (Array2D.inBounds board)
+
+let findAntinodesWithHarmonics board ((x1, y1), (x2, y2)) = 
+    let xd = x2 - x1 
+    let yd = y2 - y1
+    let sub (x, y) = (x - xd, y - yd) 
+    let add (x, y) = (x + xd, y + yd) 
+    let unfold fn = List.unfold (fun pos -> if Array2D.inBounds board pos then Some (pos, fn pos) else None)
+    let subList = (x1, y1) |> unfold sub
+    let addList = (x2, y2) |> unfold add
+    subList @ addList 
 
 let rec pairs lst = 
     match lst with 
@@ -34,7 +46,7 @@ let rec pairs lst =
     | h :: t -> 
         List.map (fun it -> (h, it)) t @ pairs t 
 
-let countAntinodes board = 
+let countAntinodes antinodeFinder board = 
     let check board (x, y) = 
         match Array2D.get board y x with 
         | '.' -> None 
@@ -45,19 +57,16 @@ let countAntinodes board =
         |> List.choose (check board) 
         |> List.groupBy (fun (a, p)-> a)
         |> List.map (fun (a, lst) -> (a, List.map snd lst))
-    let foo = 
-        antennae
-        |> List.map (fun (a, positions) -> (positions |> pairs |> List.collect findAntinodes))
-        |> List.concat
-        |> Set.ofList 
-        |> Set.toList 
-        |> List.choose (Array2D.tryGet board)
-        |> List.length 
-    foo
+    antennae
+    |> List.map (fun (a, positions) -> (positions |> pairs |> List.collect (antinodeFinder board)))
+    |> List.concat
+    |> Set.ofList 
+    |> Set.count 
 
 let run fileName = 
     let lines = readLines fileName |> List.map Seq.toList
     let board = Array2D.fromList lines
-    board |> countAntinodes |> printfn "%d"
+    board |> countAntinodes findAntinodes |> printfn "%d"
+    board |> countAntinodes findAntinodesWithHarmonics |> printfn "%d"
 
-run "input"
+run "sample"
