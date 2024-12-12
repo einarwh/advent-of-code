8bb9594ab0e04b66af3d840b385a128223b02e23
Einar W. Høst <einarwh@gmail.com>
2024-12-12 09:11:49 +0100

    Update aoc12.fsx

--- 2024/12/aoc12.fsx
+++ 2024/12/aoc12.fsx
@@ -4,14 +4,14 @@
 open System
 open System.IO
 
-module Array2D = 
+module Garden = 
     let inBounds (a : 'a[,]) (x, y) = 
         let first = y >= 0 && y < a.GetLength(0)
         let second = x >= 0 && x < a.GetLength(1)
         first && second
-    let tryGetPos (a : 'a[,]) (x, y) = 
+    let tryGet (a : 'a[,]) (x, y) = 
         if inBounds a (x, y) then Some (Array2D.get a y x) else None
-    let getPos (a : 'a[,]) (x, y) = 
+    let get (a : 'a[,]) (x, y) = 
         Array2D.get a y x
     let positions (a : 'a[,]) = 
         let rowCount = a.GetLength(0)
@@ -28,10 +28,10 @@ let readLines =
     >> Array.toList
 
 let rec fill (garden : char[,]) (ch : char) (x : int, y : int) (plot : Set<int * int>) = 
-    if (x, y) |> Array2D.inBounds garden then 
+    if (x, y) |> Garden.inBounds garden then 
         if Set.contains (x, y) plot then plot 
         else 
-            if ch = Array2D.get garden y x then 
+            if ch = Garden.get garden (x, y) then 
                 plot 
                 |> Set.add (x, y)
                 |> fill garden ch ((x - 1), y) 
@@ -42,7 +42,7 @@ let rec fill (garden : char[,]) (ch : char) (x : int, y : int) (plot : Set<int *
     else plot 
 
 let fillPlot (garden : char[,]) (x, y)  = 
-    let ch = Array2D.get garden y x
+    let ch = Garden.get garden (x, y)
     fill garden ch (x, y) Set.empty
 
 let findPlots (garden : char[,]) =
@@ -55,17 +55,17 @@ let findPlots (garden : char[,]) =
             else 
                 let plot = fillPlot garden pos 
                 loop remaining (plot :: plots) (Set.union visited plot)
-    let startPositions = Array2D.positions garden 
+    let startPositions = Garden.positions garden 
     loop startPositions [] Set.empty
 
 let calculateArea plot = Set.count plot 
 
 let borders (garden : char[,]) (x, y) : int = 
     let isBorder plotPlant pos = 
-        match Array2D.tryGetPos garden pos with 
+        match Garden.tryGet garden pos with 
         | None -> true 
         | Some plant -> plant <> plotPlant
-    let plotPlant = Array2D.get garden y x 
+    let plotPlant = Garden.get garden (x, y)
     [ (x, y-1); (x-1, y); (x, y+1); (x+1, y) ]
     |> List.filter (isBorder plotPlant)
     |> List.length 
@@ -82,7 +82,7 @@ let fenceCost (garden : char[,]) (plot : Set<int*int>) =
 
 let run fileName = 
     let lines = readLines fileName |> List.map Seq.toList
-    let garden = Array2D.fromList lines 
+    let garden = Garden.fromList lines 
     let foo = fillPlot garden (0, 0)
     let plots = findPlots garden 
     // plots |> List.length |> printfn "%d"
