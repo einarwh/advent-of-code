cf63371916ccc7a74d23d1ea70ab2124d60b6026
Einar W. Høst <einarwh@gmail.com>
2024-12-12 09:09:01 +0100

    a

--- 2024/12/aoc12.fsx
+++ 2024/12/aoc12.fsx
@@ -1,4 +1,4 @@
-// Advent of Code 2024. Day 12
+// Advent of Code 2024. Day 12: Garden Groups.
 // dotnet fsi aoc12.fsx
 
 open System
@@ -9,8 +9,10 @@ module Array2D =
         let first = y >= 0 && y < a.GetLength(0)
         let second = x >= 0 && x < a.GetLength(1)
         first && second
-    let tryGet (a : 'a[,]) (x, y) = 
+    let tryGetPos (a : 'a[,]) (x, y) = 
         if inBounds a (x, y) then Some (Array2D.get a y x) else None
+    let getPos (a : 'a[,]) (x, y) = 
+        Array2D.get a y x
     let positions (a : 'a[,]) = 
         let rowCount = a.GetLength(0)
         let colCount = a.GetLength(1)
@@ -39,16 +41,52 @@ let rec fill (garden : char[,]) (ch : char) (x : int, y : int) (plot : Set<int *
             else plot 
     else plot 
 
-let fillArea (garden : char[,]) (x, y)  = 
+let fillPlot (garden : char[,]) (x, y)  = 
     let ch = Array2D.get garden y x
     fill garden ch (x, y) Set.empty
+
+let findPlots (garden : char[,]) =
+    let rec loop (startPositions : (int*int) list) (plots : Set<int*int> list) (visited : Set<int*int>) = 
+        match startPositions with 
+        | [] -> plots 
+        | pos :: remaining -> 
+            if Set.contains pos visited then 
+                loop remaining plots visited 
+            else 
+                let plot = fillPlot garden pos 
+                loop remaining (plot :: plots) (Set.union visited plot)
+    let startPositions = Array2D.positions garden 
+    loop startPositions [] Set.empty
+
+let calculateArea plot = Set.count plot 
+
+let borders (garden : char[,]) (x, y) : int = 
+    let isBorder plotPlant pos = 
+        match Array2D.tryGetPos garden pos with 
+        | None -> true 
+        | Some plant -> plant <> plotPlant
+    let plotPlant = Array2D.get garden y x 
+    [ (x, y-1); (x-1, y); (x, y+1); (x+1, y) ]
+    |> List.filter (isBorder plotPlant)
+    |> List.length 
+
+let calculatePerimeter (garden : char[,]) (plot : Set<int*int>) = 
+    plot 
+    |> Set.toList 
+    |> List.sumBy (borders garden) 
+
+let fenceCost (garden : char[,]) (plot : Set<int*int>) = 
+    let a = calculateArea plot
+    let p = calculatePerimeter garden plot 
+    a * p
 
 let run fileName = 
     let lines = readLines fileName |> List.map Seq.toList
     let garden = Array2D.fromList lines 
-    let foo = fillArea garden (0, 0)
-    foo |> printfn "%A"
-    let bar = fillArea garden (0, 1)
-    bar |> printfn "%A"
+    let foo = fillPlot garden (0, 0)
+    let plots = findPlots garden 
+    // plots |> List.length |> printfn "%d"
+    // plots |> List.map (fenceCost garden) |> printfn "%A"
+    plots |> List.sumBy (fenceCost garden) |> printfn "%A"
 
-run "sample-larger"
+run "input"
