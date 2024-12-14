d69b3b70bdc07e1e397a25089a38761fe49c346a
Einar W. Høst <einarwh@gmail.com>
2024-12-14 09:58:28 +0100

    viz

--- 2024/14/aoc14.fsx
+++ 2024/14/aoc14.fsx
@@ -36,24 +36,44 @@ let move width height seconds robot =
 let simulate width height seconds (robots : Robot list) = 
     robots |> List.map (move width height seconds)
 
+let createRow robots width yRow = 
+    let countedRobots = 
+        robots 
+        |> List.map (fun r -> r.p) 
+        |> List.choose (fun (x, y) -> if y = yRow then Some x else None) 
+        |> List.countBy id 
+    let robotMap : Map<int, int> = 
+        countedRobots
+        |> Map.ofList
+    [ 0 .. (width - 1) ] 
+    |> List.map (fun x -> Map.tryFind x robotMap |> Option.map string |> Option.defaultValue ".")
+    |> String.concat ""
+    // []
+
+let visualize width height robots = 
+    [ 0 .. (height - 1) ] |> List.map (createRow robots width) |> String.concat "\n" |> printfn "%s"
+
 let readLines = 
     File.ReadAllLines
     >> Array.filter (fun line -> line <> String.Empty)
     >> Array.toList
 
 let run fileName = 
+    // width: 11
+    // height: 7
     let lines = readLines fileName
     let robots = lines |> List.map parseRobot
-    robots |> printfn "%A"
-    let moved = robots |> simulate 11 7 100
-    moved |> List.iter (printfn "%A")
-    let robot = { p=(2,4); v=(2,-3) }
-    robot |> (fun r -> r.p) |> printfn "0s : %A" 
-    robot |> move 11 7 1 |> (fun r -> r.p) |> printfn "1s : %A"
-    robot |> move 11 7 2 |> (fun r -> r.p) |> printfn "2s : %A"
-    robot |> move 11 7 3 |> (fun r -> r.p) |> printfn "3s : %A"
-    robot |> move 11 7 4 |> (fun r -> r.p) |> printfn "4s : %A"
-    robot |> move 11 7 5 |> (fun r -> r.p) |> printfn "5s : %A"
+    visualize 11 7 robots 
+    // robots |> printfn "%A"
+    // let moved = robots |> simulate 11 7 100
+    // moved |> List.iter (printfn "%A")
+    // let robot = { p=(2,4); v=(2,-3) }
+    // robot |> (fun r -> r.p) |> printfn "0s : %A" 
+    // robot |> move 11 7 1 |> (fun r -> r.p) |> printfn "1s : %A"
+    // robot |> move 11 7 2 |> (fun r -> r.p) |> printfn "2s : %A"
+    // robot |> move 11 7 3 |> (fun r -> r.p) |> printfn "3s : %A"
+    // robot |> move 11 7 4 |> (fun r -> r.p) |> printfn "4s : %A"
+    // robot |> move 11 7 5 |> (fun r -> r.p) |> printfn "5s : %A"
     0 
 
 run "sample"
