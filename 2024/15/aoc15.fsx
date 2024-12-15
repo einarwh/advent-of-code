23a0c100d3aa759b95c7925c60b675e8337195fc
Einar W. Høst <einarwh@gmail.com>
2024-12-15 10:07:56 +0100

    ok

--- 2024/15/aoc15.fsx
+++ 2024/15/aoc15.fsx
@@ -13,6 +13,8 @@ module Warehouse =
         if inBounds a (x, y) then Some (Array2D.get a y x) else None
     let get (a : 'a[,]) (x, y) = 
         Array2D.get a y x
+    let set (a : 'a[,]) (x, y) (v : 'a) = 
+        Array2D.set a y x v
     let positions (a : 'a[,]) = 
         let rowCount = a.GetLength(0)
         let colCount = a.GetLength(1)
@@ -28,6 +30,8 @@ module Warehouse =
         let xRange = [ 0 .. a.GetLength(1) - 1 ]
         yRange 
         |> List.map (fun y -> xRange |> List.map (fun x -> get a (x, y)))
+
+type Pos = int*int
 
 type Move = N | W | S | E 
 
@@ -53,12 +57,14 @@ let cellToChar cell =
     | Box -> 'O' 
     | Space -> '.' 
 
-let visualize warehouse = 
-    let lines = warehouse |> Warehouse.map cellToChar |> Warehouse.toList 
+let visualize warehouse robotPos =
+    let charWarehouse = warehouse |> Warehouse.map cellToChar 
+    Warehouse.set charWarehouse robotPos '@'
+    let lines = charWarehouse |> Warehouse.toList 
     lines |> List.map (fun chars -> new String(List.toArray chars)) |> join "\n" |> printfn "%s"
     // printfn "%A" lines
 
-let charToCommand ch = 
+let charToMove ch = 
     match ch with 
     | '^' -> N 
     | '<' -> W 
@@ -66,8 +72,74 @@ let charToCommand ch =
     | '>' -> E
     | _ -> failwith (sprintf "%c ?" ch)
 
-let parseCommands text = 
-    text |> Seq.toList |> List.map charToCommand 
+let parseMoves text = 
+    text |> Seq.toList |> List.map charToMove 
+
+let findRobot (warehouse : char[,]) : Pos = 
+    let rec find positions = 
+        match positions with 
+        | [] -> failwith "no robot?"
+        | pos :: rest -> 
+            if (Warehouse.get warehouse pos) = '@' then pos
+            else find rest 
+    find <| Warehouse.positions warehouse
+
+let moveToOffset move = 
+    match move with 
+    | N -> (0, -1)
+    | W -> (-1, 0)
+    | S -> (0, 1)
+    | E -> (1, 0)
+
+let moveStep move (x, y) = 
+    let (dx, dy) = moveToOffset move 
+    (x + dx, y + dy)
+
+let tryFindSpace (warehouse : Cell[,]) (robotPos : Pos) (move : Move) : (Pos*Pos) list = 
+    // printfn "tryFindSpace with robot in pos %A: %A" robotPos move
+    let rec loop (x, y) (swaps : (Pos*Pos) list) = 
+        let (dx, dy) = moveToOffset move 
+        let pos = (x + dx, y + dy)
+        let nextSwaps = (pos, (x, y)) :: swaps 
+        match Warehouse.get warehouse pos with 
+        | Wall -> 
+            // printfn "Hit wall."
+            [] 
+        | Space ->
+            // printfn "Found space!"
+            nextSwaps
+        | Box ->
+            // printfn "Found box... keep looking." 
+            loop pos nextSwaps
+    loop robotPos []
+
+let rec moveStuff warehouse swaps = 
+    match swaps with 
+    | [] -> ()
+    | (pos1, pos2) :: rest -> 
+        // printfn "Swapping."
+        let cell1 = Warehouse.get warehouse pos1 
+        let cell2 = Warehouse.get warehouse pos2
+        Warehouse.set warehouse pos1 cell2 
+        Warehouse.set warehouse pos2 cell1 
+        moveStuff warehouse rest 
+
+let tryMoveRobot (warehouse : Cell[,], robotPos : Pos) (move : Move) = 
+    // printfn "tryMoveRobot in pos %A: %A" robotPos move
+    match tryFindSpace warehouse robotPos move with 
+    | [] -> (warehouse, robotPos)
+    | swaps -> 
+        moveStuff warehouse swaps 
+        (warehouse, moveStep move robotPos)
+
+let rec makeMoves (warehouse : Cell[,], robotPos : Pos) (moves : Move list) = 
+    printfn "\n"
+    visualize warehouse robotPos
+    match moves with 
+    | [] -> (warehouse, robotPos)
+    | m :: restMoves -> 
+        let result = tryMoveRobot (warehouse, robotPos) m 
+        makeMoves result restMoves
 
 let run fileName = 
     let text = File.ReadAllText fileName |> trim |> split "\n\n"
@@ -76,9 +148,18 @@ let run fileName =
     let text0 = text.[0]
     let text1 = text.[1]
     let lines = text0 |> toLines |> List.map Seq.toList 
-    let warehouse = lines |> Warehouse.fromList |> Warehouse.map (charToCell)
-    visualize warehouse
-    let commands = text1 |> joinUp |> parseCommands 
-    commands |> printfn "%A"
+    let charWarehouse = lines |> Warehouse.fromList
+    let robotPos = findRobot charWarehouse
+    // printfn "Robot at %A" robotPos
+    let warehouse = charWarehouse |> Warehouse.map (charToCell)
+    // visualize warehouse robotPos 
+    // let (wh, rp) = tryMoveRobot (warehouse, robotPos) W
+    // visualize wh rp  
+    // let (wh2, rp2) = tryMoveRobot (wh, robotPos) N
+    // visualize wh2 rp2 
+    let moves = text1 |> joinUp |> parseMoves 
+    moves |> printfn "%A"
+    makeMoves (warehouse, robotPos) moves 
+    0
 
-run "sample-small"
+run "sample"
