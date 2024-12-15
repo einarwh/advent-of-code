c9cdf7bb13959c2d68ee907abc5118e85d23655d
Einar W. Høst <einarwh@gmail.com>
2024-12-15 08:30:29 +0100

    init

---
+++ 2024/15/aoc15.fsx
@@ -0,0 +1,16 @@
+// Advent of Code 2024. Day 15: Warehouse Woes.
+// dotnet fsi aoc15.fsx
+
+open System
+open System.IO
+
+let readLines = 
+    File.ReadAllLines
+    >> Array.filter (fun line -> line <> String.Empty)
+    >> Array.toList
+
+let run fileName = 
+    let lines = readLines fileName
+    lines |> printfn "%A"
+
+run "input"
