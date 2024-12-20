412532dc6bf512a55bd00e6fff02607a221747a1
Einar W. Høst <einarwh@gmail.com>
2024-12-20 22:56:03 +0100

    cleanup

--- 2024/20/aoc20.fsx
+++ 2024/20/aoc20.fsx
@@ -4,6 +4,7 @@
 open System
 open System.IO
 open System.Collections.Generic
+open System.Diagnostics 
 
 module Array2D =
     let inBounds (a : 'a[,]) (x, y) =
@@ -17,39 +18,18 @@ module Array2D =
         let colCount = a.GetLength(1)
         [for x in [0..colCount-1] do for y in [0..rowCount-1] -> (x, y)]
 
-type Dir = N | W | S | E
-
 type Pos = (int * int)
 
-type Racetrack = char[,]
-
-type Path = { pos : Pos; dir : Dir }
-
-type Distance = int
-
-type PQ = PriorityQueue<Pos * Distance * (Pos * Distance) list, Distance>
+type PQ = PriorityQueue<Pos * int * (Pos * int) list, int>
 
 let getNeighbours (x, y) =
-    [ (x, y - 1)
-      (x - 1, y)
-      (x, y + 1)
-      (x + 1, y) ]
-
-let move dir (x, y) =
-    match dir with
-    | N -> (x, y - 1)
-    | W -> (x - 1, y)
-    | S -> (x, y + 1)
-    | E -> (x + 1, y)
-
-let isPosFree racetrack pos  =
-    '#' <> Array2D.getPos racetrack pos
+    [ (x, y - 1); (x - 1, y); (x, y + 1); (x + 1, y) ]
 
 let isWall racetrack pos  =
     '#' = Array2D.getPos racetrack pos
 
-let solve (startPos : Pos) (racetrack : Racetrack)  =
-    let rec loop (visited : Set<Pos>, q : PQ) =
+let race startPos racetrack =
+    let rec loop (visited, q : PQ) =
         if q.Count = 0 then []
         else
             let (pos, distance, posDistList) = q.Dequeue()
@@ -73,64 +53,44 @@ let solve (startPos : Pos) (racetrack : Racetrack)  =
                 failwith (sprintf "%c" ch)
     let queue = PQ()
     queue.Enqueue((startPos, 0, []), 0)
-    let results = loop (Set.empty, queue)
-    results |> List.rev
-    // results |> printfn "%A"
+    loop (Set.empty, queue) |> List.rev 
 
-let findStartPos (racetrack : Racetrack) : Pos =
-    let rec find positions =
-        match positions with
-        | [] -> failwith "?"
-        | pos :: rest ->
-            if (Array2D.getPos racetrack pos) = 'S' then pos
-            else find rest
-    find <| Array2D.positions racetrack
+let findStartPos racetrack =
+    racetrack |> Array2D.positions |> List.find (fun pos -> (Array2D.getPos racetrack pos) = 'S')
 
-let getReachables (x, y) : Pos list=
-    [ (x, y-2)
-      (x-1, y-1); (x+1, y-1)
-      (x-2, y); (x+2, y)
-      (x-1, y+1); (x+1, y+1)
-      (x, y+2) ]
+let getReachablesInDuration cheatDuration (x, y) =
+    let chooser (x, y) (dx, dy) = 
+        let len = abs dx + abs dy 
+        if len > 0 && len <= cheatDuration then Some (x + dx, y + dy) else None 
+    let range = [ -cheatDuration .. cheatDuration ]
+    range |> List.collect (fun dy -> range |> List.choose (fun dx -> chooser (x, y) (dx, dy)))
 
-let findCheatsAt (posDistMap : Map<Pos, Distance>) (pos, dist) =
-    // printfn "\nfindCheatsAt position %A (distance %d)" pos dist
-    let reachables = getReachables pos
-    // printfn "Reachable positions: %A" reachables
-    let onRoute = reachables |> List.choose (fun (p : Pos) -> Map.tryFind p posDistMap |> Option.map (fun d -> (p, d)))
-    // printfn "Reachables on route %A" onRoute
-    // let cheats = reachables |> List.choose (fun (p : Pos) -> Map.tryFind p posDistMap) |> List.map (fun d -> (d - dist)) |> List.filter (fun diff -> diff > 2)
-    let cheats1 =
-        reachables
-        |> List.choose (fun (p : Pos) -> Map.tryFind p posDistMap |> Option.map (fun d -> (p, d - dist)))
-        |> List.choose (fun (p, diff) -> if diff - 2 > 0 then Some (pos, p, diff - 2) else None)
-    // printfn "Cheats on route %A" cheats1
-    cheats1
+let posDiff (x1, y1) (x2, y2) = 
+    abs (x2 - x1) + abs (y2 - y1) 
+
+let findCheats cheatDuration posDistMap (pos, dist) =
+    let diffChooser (p, cheatDiff) = 
+        let diff = cheatDiff - posDiff pos p 
+        if diff > 0 then Some diff else None
+    getReachablesInDuration cheatDuration pos
+    |> List.choose (fun p -> Map.tryFind p posDistMap |> Option.map (fun d -> (p, d - dist)))
+    |> List.choose (diffChooser)
 
 let readLines =
     File.ReadAllLines >> Array.filter ((<>) String.Empty)
 
+let solve cheatDuration posDistList = 
+    let stopwatch = Stopwatch.StartNew()
+    let savings = posDistList |> List.collect (findCheats cheatDuration (Map.ofList posDistList)) 
+    let count = savings |> List.filter ((<=) 100) |> List.length 
+    printfn "%d (%d ms)" count (int stopwatch.Elapsed.TotalMilliseconds)
+
 let run fileName =
     let lines = readLines fileName |> Array.map (Seq.toArray)
-    // printfn "%A" lines
     let racetrack = lines |> array2D
     let startPos = findStartPos racetrack
-    printfn "%A" startPos
-    let posDistList = solve startPos racetrack
-    // printfn "LIST %A" posDistList
-    // printfn "%A" (findReachable (3, 3) |> List.sort)
-    let posDistMap = Map.ofList posDistList
-    // printfn "MAP %A" posDistMap
-
-    let result = posDistList |> List.collect (findCheatsAt posDistMap) |> List.sortBy (fun (p1, p2, saved) -> saved) |> List.rev
-    let savings = result |> List.map (fun (_, _, saved) -> saved)
-    let bigSavings = savings |> List.filter (fun saved -> saved >= 100) |> List.length
-
-    printfn "%d" bigSavings
-
-
-
-    // lines |> printfn "%A"
-    0
+    let posDistList = race startPos racetrack
+    posDistList |> solve 2 
+    posDistList |> solve 20
 
 run "input"
