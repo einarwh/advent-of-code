c79fd4147cce635ee54f64f7a7d919f53c181466
Einar W. Høst <einarwh@gmail.com>
2024-12-23 21:12:14 +0100

    Update aoc13.fsx

--- 2021/13/aoc13.fsx
+++ 2021/13/aoc13.fsx
@@ -17,21 +17,29 @@ let parseTransformation s =
     match s |> split " " |> Array.item 2 |> split "=" with 
     | [|"x";str|] -> 
         let n = int str  
-        fun (x, y) -> (2 * n - x, y)
+        fun (x, y) -> ((if x > n then 2 * n - x else x), y)
     | [|"y";str|] -> 
         let n = int str  
-        fun (x, y) -> (x, 2 * n - y) 
+        fun (x, y) -> (x, (if y > n then 2 * n - y else y))
     | _ -> failwith "?"
+
+let rec applyTransformations transformations positions =
+    match transformations with 
+    | [] -> positions
+    | t :: rest -> 
+        positions 
+        |> List.map t
+        |> List.distinct
+        |> applyTransformations rest 
 
 let run fileName = 
     let text = File.ReadAllText fileName |> trim |> split "\n\n"
     let positions = 
         text.[0] |> split "\n" |> Array.toList |> List.map parsePosition
-    positions |> printfn "%A"
     let transformations = 
         text.[1] |> split "\n" |> Array.toList |> List.map parseTransformation
-    let x = 655
-    2 * x - 1305 |> printfn "%A"
+    let t = List.head transformations
+    positions |> List.map t |> List.distinct |> List.length |> printfn "%d"
     0
 
 run "input"
