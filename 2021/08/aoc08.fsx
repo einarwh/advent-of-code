40112e10ac1d3db525a045c751059955f4472de2
Einar W. Høst <einarwh@gmail.com>
2024-12-23 13:40:04 +0100

    Update aoc08.fsx

--- 2021/08/aoc08.fsx
+++ 2021/08/aoc08.fsx
@@ -1,4 +1,4 @@
-// Advent of Code 2021. Day 08
+// Advent of Code 2021. Day 08: Seven Segment Search.
 // dotnet fsi aoc08.fsx
 
 open System
@@ -21,6 +21,9 @@ let readLines =
     >> Array.filter (fun line -> line <> String.Empty)
     >> Array.toList
 
+let rec pow a b = 
+    if b < 1 then 1 else a * pow a (b - 1)
+
 let solveLine (signals : Set<char> list, outputs : Set<char> list) = 
     let sizeMap = signals |> List.groupBy (Set.count) |> Map.ofList 
     let getBySize n = sizeMap |> Map.find n |> List.head 
@@ -28,42 +31,36 @@ let solveLine (signals : Set<char> list, outputs : Set<char> list) =
     let four = getBySize 4
     let seven = getBySize 3
     let eight = getBySize 7
-
     let sizeSixDigits = sizeMap |> Map.find 6 
     let six = sizeSixDigits |> List.find (fun digit -> 1 = (Set.intersect digit one |> Set.count))
     let nine = sizeSixDigits |> List.find (fun digit -> 4 = (Set.intersect digit four |> Set.count))
     let zero = sizeSixDigits |> List.find (fun digit -> digit <> six && digit <> nine)
-
     let sizeFiveDigits = sizeMap |> Map.find 5 
     let three = sizeFiveDigits |> List.find (fun digit -> 2 = (Set.intersect digit one |> Set.count))
-    let five = sizeFiveDigits |> List.find (fun digit -> 3 = (Set.intersect digit four |> Set.count))
-    let two = sizeFiveDigits |> List.find (fun digit -> digit <> three && digit <> five)
-    
-    printfn "6: %A" six
-    printfn "9: %A" nine
-    printfn "0: %A" zero
+    let two = sizeFiveDigits |> List.find (fun digit -> 2 = (Set.intersect digit four |> Set.count))
+    let five = sizeFiveDigits |> List.find (fun digit -> digit <> three && digit <> two)
     let lookup = 
         [ (zero, 0); (one, 1); (two, 2); (three, 3); (four, 4); (five, 5); (six, 6); (seven, 7); (eight, 8); (nine, 9) ]
         |> Map.ofList 
-
-    let digits = outputs |> List.map (fun digit -> Map.find digit lookup) 
-    let red = digits |> List.reduce (fun a b -> 10 * a + b)
-    printfn "%A" digits
-    printfn "%d" red
-    lookup
+    outputs 
+    |> List.map (fun digit -> Map.find digit lookup) 
+    |> List.rev
+    |> List.mapi (fun i x -> (pow 10 i) * x)
+    |> List.sum 
 
 let run fileName = 
     let lines = readLines fileName |> List.map parseLine 
+    // Part 1 
     lines 
     |> List.collect snd
     |> List.map Set.count
     |> List.filter (fun len -> len = 2 || len = 3 || len = 4 || len = 7)
     |> List.length 
     |> printfn "%d"
-
-    let foo = parseLine "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe"
-    solveLine foo
-
-    0
+    // Part 2 
+    lines 
+    |> List.map solveLine 
+    |> List.sum 
+    |> printfn "%d"
 
 run "input"
