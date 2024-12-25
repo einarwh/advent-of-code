fb8a17ca9f3e8a8c1fba97f442ab63d542c251c6
Einar W. Høst <einarwh@gmail.com>
2024-12-25 16:15:06 +0100

    Update aoc14.fsx

--- 2021/14/aoc14.fsx
+++ 2021/14/aoc14.fsx
@@ -4,7 +4,9 @@
 open System
 open System.IO
 
-type Rule = char*char -> char list
+type Pair = char * char
+
+type Rule = Pair -> Pair list
 
 type ComposableRule = Rule -> Rule 
 
@@ -17,15 +19,15 @@ let parseRule (s : string) : ComposableRule =
     | [|pair; element|] ->
         let (x, y) = (pair.[0], pair.[1])
         let e = element.[0]
-        fun fn (a, b) -> if (a, b) = (x, y) then [a; e; b] else fn (a, b)
+        fun fn (a, b) -> if (a, b) = (x, y) then [(a, e); (e, b)] else fn (a, b)
     | _ -> failwith "?"
 
-let createInsertionFunction (s : string) = 
+let createInsertionFunction (s : string) : Rule = 
     let rules : ComposableRule list = s |> split "\n" |> Array.toList |> List.map parseRule
     // 'T -> 'State -> 'State
     let folder (rule : ComposableRule) (fn : Rule) : Rule = 
         rule fn 
-    let initFunction (a, b) = [a; b]
+    let initFunction (a, b) = [(a, b)]
     List.foldBack folder rules initFunction
 
 let rec insert insertionFunction pairs = 
@@ -34,43 +36,49 @@ let rec insert insertionFunction pairs =
     | p :: rest -> 
         (insertionFunction p) :: (insert insertionFunction rest)
 
-let join lst = 
-    match lst with 
-    | [] -> failwith "?"
-    | h :: rest -> 
-        h @ (rest |> List.collect (List.tail))
-    
-let rec loop inserter times elements = 
-    // printfn "%A" elements 
-    if times < 1 then elements 
-    else 
-        elements 
-        |> List.pairwise 
-        |> insert inserter 
-        |> join 
-        |> loop inserter (times - 1)
+let polymerization (inserter : Rule) (countedPairs : (Pair * int64) list) : (Pair * int64) list = 
+    let rec loop (countedPairs : (Pair * int64) list) : (Pair * int64) list = 
+        match countedPairs with 
+        | [] -> []
+        | (pair, count) :: rest -> 
+            let lst = pair |> inserter |> List.map (fun p -> (p, count))
+            lst @ loop rest
+    let cps = loop countedPairs
+    cps |> List.groupBy (fst) |> List.map (fun (p, lst) -> (p, lst |> List.map snd |> List.sum))
+
+let rec solve (times : int) (inserter : Rule) (countedPairs : (Pair * int64) list) = 
+    let rec loop times countedPairs = 
+        if times > 0 then 
+            loop (times - 1) (polymerization inserter countedPairs)
+        else 
+            countedPairs
+    let result = loop times countedPairs
+    let fstList = result |> List.groupBy (fst >> fst)
+    let sndList = result |> List.groupBy (fst >> snd)
+    let sum lst = lst |> List.map (fun (ch, lst) -> (ch, lst |> List.map snd |> List.sum))
+    let lst = 
+        List.zip (fstList |> sum |> List.sort) (sndList |> sum |> List.sort) 
+        |> List.map (fun ((ch1, n1), (ch2, n2)) -> max n1 n2)
+    let minVal = lst |> List.min
+    let maxVal = lst |> List.max 
+    maxVal - minVal 
 
 let run fileName = 
     let text = File.ReadAllText fileName |> trim |> split "\n\n"
-    let template = text.[0]
-    let insertionFunction = createInsertionFunction text.[1]
-    let pairs = template |> Seq.toList |> List.pairwise 
-    printfn "%A" pairs
-    pairs |> insert insertionFunction |> join |> printfn "%A"
-
-    let polymer10 = template |> Seq.toList |> loop insertionFunction 10 
-    let counted = polymer10 |> List.countBy id 
-    let counts = counted |> List.map snd 
-    let minCount = counts |> List.min 
-    let maxCount = counts |> List.max 
-    let answer = (List.max counts - List.min counts)
-    printfn "%d" answer
-
-    let polymer20 = template |> Seq.toList |> loop insertionFunction 20 
-
+    let template : string = text.[0]
+    let inserter = createInsertionFunction text.[1]
+    let countedPairs = 
+        template 
+        |> Seq.toList 
+        |> List.pairwise 
+        |> List.countBy id 
+        |> List.map (fun (p, c) -> (p, int64 c))
+    countedPairs
+    |> solve 10 inserter 
+    |> printfn "%d"
+    countedPairs
+    |> solve 40 inserter 
+    |> printfn "%d"
     0
 
 run "input"
-
-// ['N'; 'C'; 'N'; 'B'; 'C'; 'H'; 'B']
-// NCNBCHB
