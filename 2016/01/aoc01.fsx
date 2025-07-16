2ab9d83922daf667c1082c8cbdeb5dd6c9c5c51c
Einar W. Høst <einarwh@gmail.com>
2025-07-17 01:31:10 +0200

    2016/01 solved

--- 2016/01/aoc01.fsx
+++ 2016/01/aoc01.fsx
@@ -1,21 +1,21 @@
-// Advent of Code 2016. Day 01: No Time for a Taxicab
+// Advent of Code 2016. Day 01: No Time for a Taxicab.
 // dotnet fsi aoc01.fsx
 
 open System
 open System.IO
 
-type Direction = N | W | S | E 
+type Direction = N | W | S | E
 
-type Turn = R | L 
+type Turn = R | L
 
-let parse (s : string) : (Turn * int) = 
+let parse (s : string) : (Turn * int) =
     let ch = s[0]
     let n = Int32.Parse(s.Substring(1))
     if ch = 'R' then (R, n)
     else (L, n)
 
-let nextDir dir turn = 
-    match (dir, turn) with 
+let nextDir dir turn =
+    match (dir, turn) with
     | (N, R) -> E
     | (N, L) -> W
     | (W, R) -> N
@@ -25,28 +25,57 @@ let nextDir dir turn =
     | (E, R) -> S
     | (E, L) -> N
 
-let nextPos (x, y) steps dir = 
-    match dir with 
-    | N -> (x, y + steps)
-    | W -> (x - steps, y)
-    | S -> (x, y - steps)
-    | E -> (x + steps, y)
+let nextPosStep (x, y) dir =
+    match dir with
+    | N -> (x, y + 1)
+    | W -> (x - 1, y)
+    | S -> (x, y - 1)
+    | E -> (x + 1, y)
 
-let rec move (pos : int * int) (dir : Direction) (moves : (Turn * int) list) = 
-    match moves with 
-    | [] -> pos 
+let expand (dir : Direction, steps : int) : Direction list = 
+    let rec fn acc stepsLeft =
+        if stepsLeft > 0 then fn (dir :: acc) (stepsLeft - 1)
+        else acc 
+    fn [] steps
+
+let rec toDirections (directions : Direction list) (dir : Direction) (relativeMoves : (Turn * int) list) = 
+    match relativeMoves with 
+    | [] -> directions |> List.rev 
     | (turn, steps) :: rest -> 
         let dir' = nextDir dir turn 
-        let pos' = nextPos pos steps dir' 
-        move pos' dir' rest
+        toDirections (expand (dir', steps) @ directions) dir' rest 
 
-let readText fileName = 
+let move (directions : Direction list) = 
+    let rec fn (pos : int * int) (dirs : Direction list) = 
+        match dirs with 
+        | [] -> pos 
+        | dir :: rest -> 
+            let pos' = nextPosStep pos dir
+            fn pos' rest 
+    fn (0, 0) directions
+
+let twice (directions : Direction list) = 
+    let rec fn (seen : Set<int * int>) (pos : int * int) (dirs : Direction list) = 
+        if seen |> Set.contains pos then 
+            pos 
+        else 
+            match dirs with 
+            | [] -> failwith "no hq" 
+            | dir :: rest -> 
+                let seen' = seen |> Set.add pos
+                let pos' = nextPosStep pos dir
+                fn seen' pos' rest 
+    fn Set.empty (0, 0) directions
+
+let readText fileName =
     File.ReadAllText(fileName).Trim()
 
-let run fileName = 
+let run fileName =
     let text = readText fileName
     let moves = text.Split(", ") |> List.ofArray |> List.map parse
-    let (x, y) = move (0, 0) N moves 
-    printfn "%d" (x + y)
+    let directions = toDirections [] N moves
+    let toDistance (x, y) = printfn "%d" (abs x + abs y)
+    directions |> move |> toDistance 
+    directions |> twice |> toDistance
 
 run "input"
