9ae8cdc0ba8cb8b6a0da9dd1da517ee22671b55f
Einar W. Høst <einarwh@gmail.com>
2025-07-17 11:15:33 +0200

    2016/06 part 1

--- 2016/06/aoc06.fsx
+++ 2016/06/aoc06.fsx
@@ -4,18 +4,27 @@
 open System
 open System.IO
 
-let readText fileName = 
-    File.ReadAllText(fileName).Trim()
-
 let readLines = 
     File.ReadAllLines
     >> Array.filter (fun line -> line <> String.Empty)
     >> Array.toList
 
+let getMostCommon (chars : char list) = 
+    chars 
+    |> List.countBy id 
+    |> List.sortByDescending (fun (ch, count) -> count)
+    |> List.head 
+    |> fst 
+
+let getErrorCorrectedMessage (messageLength : int) (messages : string list) = 
+    [| 0 .. messageLength - 1 |]
+    |> Array.map (fun index -> messages |> List.map (fun m -> m[index]) |> getMostCommon)
+    |> fun chars -> new string(chars)
+
 let run fileName = 
     let lines = readLines fileName
-    lines |> printfn "%A"
-    let text = readText fileName
-    text |> printfn "%s"
+    let length = lines |> List.head |> Seq.length
+    let message = getErrorCorrectedMessage length lines 
+    message |> printfn "%s"
 
-run "sample"
+run "input"
