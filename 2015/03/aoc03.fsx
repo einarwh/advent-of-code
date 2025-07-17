a6520935daba7c2c0764a332de192a7a4af3cbcf
Einar W. Høst <einarwh@gmail.com>
2025-07-17 23:02:01 +0200

    2015/03 solved

---
+++ 2015/03/aoc03.fsx
@@ -0,0 +1,47 @@
+// Advent of Code 2015. Day 03: Perfectly Spherical Houses in a Vacuum.
+// dotnet fsi aoc03.fsx
+
+open System
+open System.IO
+
+let move (ch : char) (x, y) = 
+    match ch with 
+    | '^' -> (x, y - 1)
+    | '<' -> (x - 1, y)
+    | 'v' -> (x, y + 1)
+    | '>' -> (x + 1, y)
+    | _ -> failwith "?"
+
+let walk (s : string) = 
+    let rec fn seen pos chars = 
+        match chars with 
+        | [] -> Set.count seen 
+        | ch :: rest -> 
+            let pos' = move ch pos
+            let seen' = seen |> Set.add pos' 
+            fn seen' pos' rest 
+    let start = (0, 0)
+    fn (Set.empty |> Set.add start) start (s |> Seq.toList)
+
+let doubleWalk (s : string) = 
+    let rec fn seen santaPos roboPos chars = 
+        match chars with 
+        | [] -> Set.count seen 
+        | santaChar :: roboChar :: rest -> 
+            let santaPos' = move santaChar santaPos
+            let roboPos' = move roboChar roboPos
+            let seen' = seen |> Set.add santaPos' |> Set.add roboPos'
+            fn seen' santaPos' roboPos' rest 
+        | _ -> failwith "?"
+    let start = (0, 0)
+    fn (Set.empty |> Set.add start) start start (s |> Seq.toList)
+
+let readText fileName = 
+    File.ReadAllText(fileName).Trim()
+
+let run fileName = 
+    let text = readText fileName
+    text |> walk |> printfn "%d"
+    text |> doubleWalk |> printfn "%d"
+
+run "input"
