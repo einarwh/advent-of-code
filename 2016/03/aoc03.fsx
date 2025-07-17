79b1f04be2bc5606a237027ef192f172b4ed7d78
Einar W. Høst <einarwh@gmail.com>
2025-07-17 03:11:57 +0200

    2016/03 solved

--- 2016/03/aoc03.fsx
+++ 2016/03/aoc03.fsx
@@ -13,19 +13,35 @@ let parse (s : string) : int*int*int =
     | [a; b; c] -> (a, b, c)
     | _ -> failwith "?"
 
+let substring (startIndex : int) (length : int) (s : string) : string = 
+    s.Substring(startIndex, length)
+
 let readLines = 
     File.ReadAllLines
-    >> Array.map (fun line -> line.Trim())
     >> Array.filter (fun line -> line <> String.Empty)
     >> Array.toList
 
 let isPossible (a, b, c) = 
     a + b > c && a + c > b && b + c > a
 
+let rec toTriplets numbers = 
+    match numbers with 
+    | [] -> []
+    | a :: b :: c :: rest -> 
+        (a, b, c) :: toTriplets rest 
+    | _ -> failwith "?"
+
 let run fileName = 
     let lines = readLines fileName
-    let triplets = lines |> List.map parse
-    let possible = triplets |> List.filter isPossible |> List.length
-    printfn "%d" possible
+    let rowTriplets = lines |> List.map parse
+    let rowPossible = rowTriplets |> List.filter isPossible |> List.length
+    printfn "%d" rowPossible
+    let column1 = lines |> List.map (substring 0 5)
+    let column2 = lines |> List.map (substring 5 5)
+    let column3 = lines |> List.map (substring 10 5)
+    let colNumbers = (column1 @ column2 @ column3) |> List.map int
+    let colTriplets = colNumbers |> toTriplets
+    let colPossible = colTriplets |> List.filter isPossible |> List.length
+    printfn "%d" colPossible
 
 run "input"
