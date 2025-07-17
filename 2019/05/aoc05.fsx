0d9aa2919f04b25d154cc383c551cbb3180f84b9
Einar W. Høst <einarwh@gmail.com>
2025-07-17 22:13:54 +0200

    2019/05 init

---
+++ 2019/05/aoc05.fsx
@@ -0,0 +1,21 @@
+// Advent of Code 2019. Day 05
+// dotnet fsi aoc05.fsx
+
+open System
+open System.IO
+
+let readText fileName = 
+    File.ReadAllText(fileName).Trim()
+
+let readLines = 
+    File.ReadAllLines
+    >> Array.filter (fun line -> line <> String.Empty)
+    >> Array.toList
+
+let run fileName = 
+    let lines = readLines fileName
+    lines |> printfn "%A"
+    let text = readText fileName
+    text |> printfn "%s"
+
+run "input"
