cbe2248471baf0f79c3e9c16d611a2cc4bc77bf0
Einar W. Høst <einarwh@gmail.com>
2025-07-17 02:06:59 +0200

    parameterize

--- 2016/02/aoc02.fsx
+++ 2016/02/aoc02.fsx
@@ -16,28 +16,30 @@ let toMoves (s : string) =
         | _ -> failwith <| sprintf "%c ?" ch
     s |> Seq.toList |> List.map toMove
 
-let move (x, y) (move : Move) = 
+let moveSimpleKeypad (x, y) (move : Move) = 
     match move with 
     | U -> (x, max 0 (y - 1))
     | D -> (x, min 2 (y + 1))
     | L -> (max 0 (x - 1), y)
-    | R -> (min 2 (x + 1), y)
+    | R -> (min 2 (x + 1), y) 
 
-let rec makeMoves (pos : int * int) (moves : Move list) = 
-    match moves with 
-    | [] -> pos 
-    | h :: rest -> 
-        makeMoves (move pos h) rest  
+let makeMoves (move : int*int -> Move -> int*int) (pos : int * int) (moves : Move list) = 
+    let rec fn pos moves = 
+        match moves with 
+        | [] -> pos 
+        | h :: rest -> 
+            fn (move pos h) rest  
+    fn pos moves
 
 let toDigit (x, y) = 
     y * 3 + x + 1
 
-let findCode (movesList : Move list list) = 
+let findCode (move : int*int -> Move -> int*int) (movesList : Move list list) = 
     let rec fn (pos : int * int) (acc : (int * int) list) (lst : Move list list) = 
         match lst with 
         | [] -> acc |> List.rev |> List.map toDigit |> List.map string |> String.concat ""
         | h :: rest -> 
-            let pos' = makeMoves pos h 
+            let pos' = makeMoves move pos h 
             fn pos' (pos' :: acc) rest
     fn (1, 1) [] movesList
 
@@ -49,6 +51,6 @@ let readLines =
 let run fileName = 
     let lines = readLines fileName
     let movesList = lines |> List.map toMoves
-    movesList |> findCode |> printfn "%A"
+    movesList |> findCode moveSimpleKeypad |> printfn "%A"
 
-run "input"
+run "sample"
