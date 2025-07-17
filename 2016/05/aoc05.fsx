7bfa3549653b03512356f1645a5b9596a0e67022
Einar W. Høst <einarwh@gmail.com>
2025-07-17 10:36:28 +0200

    2016/05 part1

--- 2016/05/aoc05.fsx
+++ 2016/05/aoc05.fsx
@@ -3,6 +3,8 @@
 
 open System
 open System.IO
+open System.Text
+open System.Security.Cryptography
 
 let readText fileName = 
     File.ReadAllText(fileName).Trim()
@@ -12,8 +14,27 @@ let readLines =
     >> Array.filter (fun line -> line <> String.Empty)
     >> Array.toList
 
+let toHexHash (input : string) = 
+    use md5 = MD5.Create()
+    input |> Encoding.ASCII.GetBytes |> md5.ComputeHash |> Convert.ToHexString
+
+let rec hack (charsFound : char list) (charsLeft : int) (index : int) (doorId : string) = 
+    if charsLeft > 0 then 
+        let input = doorId + index.ToString()
+        let hash = toHexHash input
+        if hash.StartsWith "00000" then 
+            // printfn "%d" index
+            let ch = Char.ToLower(hash[5])
+            printf "%c" ch
+            hack (ch :: charsFound) (charsLeft - 1) (index + 1) doorId 
+        else 
+            hack charsFound charsLeft (index + 1) doorId 
+    else 
+        charsFound |> List.rev |> fun chars -> new string(chars |> List.toArray) |> fun s -> s.ToLower()
+
 let run fileName = 
     let text = readText fileName
-    text |> printfn "%s"
+    hack [] 8 0 text 
+    |> printfn "%s"
 
-run "input"
+run "sample"
