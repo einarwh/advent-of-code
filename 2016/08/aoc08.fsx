7e514d3b946f773d623bde14fa4feecaea902e66
Einar W. Høst <einarwh@gmail.com>
2025-07-17 14:02:58 +0200

    aoc 08 parse

--- 2016/08/aoc08.fsx
+++ 2016/08/aoc08.fsx
@@ -3,11 +3,12 @@
 
 open System
 open System.IO
+open System.Text.RegularExpressions
 
 type Operation = 
-    | Rectangle of int*int 
-    | RotateRow of int*int
-    | RotateColumn of int*int
+    | Rect of int*int 
+    | Row of int*int
+    | Column of int*int
 
 module Screen = 
     let create width height = 
@@ -19,6 +20,40 @@ module Screen =
     let get (screen : bool[,]) (x, y) =
         Array2D.get screen y x
 
+// let parse (s : string) : Operation = 
+
+let tryParseRect (s : string) : Operation option = 
+    let m = Regex.Match(s, "^rect (\d+)x(\d+)$")
+    if m.Success then
+        let w = m.Groups.[1].Value |> int 
+        let h = m.Groups.[2].Value |> int 
+        Some <| Rect (w, h)
+    else
+        None
+
+let tryParseRow (s : string) : Operation option = 
+    let m = Regex.Match(s, "^rotate row y=(\d+) by (\d+)$")
+    if m.Success then
+        let y = m.Groups.[1].Value |> int 
+        let r = m.Groups.[2].Value |> int 
+        Some <| Row (y, r)
+    else
+        None
+
+let tryParseColumn (s : string) : Operation option = 
+    let m = Regex.Match(s, "^rotate column x=(\d+) by (\d+)$")
+    if m.Success then
+        let x = m.Groups.[1].Value |> int 
+        let r = m.Groups.[2].Value |> int 
+        Some <| Column (x, r)
+    else
+        None
+
+let tryParse (s : string) : Operation option = 
+    tryParseRect s
+    |> Option.orElse (tryParseRow s)
+    |> Option.orElse (tryParseColumn s)
+
 let readLines = 
     File.ReadAllLines
     >> Array.filter (fun line -> line <> String.Empty)
@@ -27,8 +62,6 @@ let readLines =
 let visualize screen = 
     let w = Screen.width screen 
     let h = Screen.height screen
-    printfn "width %d" w 
-    printfn "height %d" h 
     let createRow y = 
         printfn "row: %d" y
         [ 0 .. (w - 1) ] 
@@ -39,9 +72,9 @@ let visualize screen =
 
 let run fileName = 
     let lines = readLines fileName
-    lines |> printfn "%A"
+    let operations = lines |> List.choose tryParse
+    operations |> printfn "%A"
     let screen = Screen.create 50 6 
-    printfn "screen %A" screen
     visualize screen
     
 
