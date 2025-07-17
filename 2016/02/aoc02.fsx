19fca9a1589f249dcbaa5a91a2185df8fe055625
Einar W. Høst <einarwh@gmail.com>
2025-07-17 02:00:33 +0200

    2016/02 part 1

--- 2016/02/aoc02.fsx
+++ 2016/02/aoc02.fsx
@@ -16,6 +16,31 @@ let toMoves (s : string) =
         | _ -> failwith <| sprintf "%c ?" ch
     s |> Seq.toList |> List.map toMove
 
+let move (x, y) (move : Move) = 
+    match move with 
+    | U -> (x, max 0 (y - 1))
+    | D -> (x, min 2 (y + 1))
+    | L -> (max 0 (x - 1), y)
+    | R -> (min 2 (x + 1), y)
+
+let rec makeMoves (pos : int * int) (moves : Move list) = 
+    match moves with 
+    | [] -> pos 
+    | h :: rest -> 
+        makeMoves (move pos h) rest  
+
+let toDigit (x, y) = 
+    y * 3 + x + 1
+
+let findCode (movesList : Move list list) = 
+    let rec fn (pos : int * int) (acc : (int * int) list) (lst : Move list list) = 
+        match lst with 
+        | [] -> acc |> List.rev |> List.map toDigit |> List.map string |> String.concat ""
+        | h :: rest -> 
+            let pos' = makeMoves pos h 
+            fn pos' (pos' :: acc) rest
+    fn (1, 1) [] movesList
+
 let readLines = 
     File.ReadAllLines
     >> Array.filter (fun line -> line <> String.Empty)
@@ -24,6 +49,6 @@ let readLines =
 let run fileName = 
     let lines = readLines fileName
     let movesList = lines |> List.map toMoves
-    movesList |> printfn "%A"
+    movesList |> findCode |> printfn "%A"
 
-run "sample"
+run "input"
