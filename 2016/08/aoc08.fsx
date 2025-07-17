182a9120d4f9e327d5f6b2c1a6e4616209aad3c8
Einar W. Høst <einarwh@gmail.com>
2025-07-17 14:34:32 +0200

    rect, rotate column

--- 2016/08/aoc08.fsx
+++ 2016/08/aoc08.fsx
@@ -19,8 +19,31 @@ module Screen =
         Array2D.length1 screen
     let get (screen : bool[,]) (x, y) =
         Array2D.get screen y x
-
-// let parse (s : string) : Operation = 
+    let set (screen : bool[,]) (x, y) (value : bool) =
+        Array2D.set screen y x value
+    let positions (a : 'a[,]) = 
+        let rowCount = a.GetLength(0)
+        let colCount = a.GetLength(1)
+        [for x in [0..colCount-1] do for y in [0..rowCount-1] -> (x, y)]
+    let rect (screen : bool[,]) (w, h) = 
+        let rectPositions = 
+            [for x in [0..w-1] do for y in [0..h-1] -> (x, y)]
+        let rec fn posList = 
+            match posList with 
+            | [] -> ()
+            | pos :: rest -> 
+                set screen pos true 
+                fn rest
+        fn rectPositions
+    let rotateColumn (screen : bool[,]) (x : int) (steps : int) = 
+        let h = height screen
+        let column = [|0 .. h-1|] |> Array.map (fun y -> get screen (x, y))
+        let rot (y : int) =
+            if y < Array.length column then 
+                let v = Array.get column y 
+                let y' =  (y + steps) % h 
+                set screen (x, y') v
+        [0 .. h-1] |> List.iter rot
 
 let tryParseRect (s : string) : Operation option = 
     let m = Regex.Match(s, "^rect (\d+)x(\d+)$")
@@ -63,19 +86,25 @@ let visualize screen =
     let w = Screen.width screen 
     let h = Screen.height screen
     let createRow y = 
-        printfn "row: %d" y
         [ 0 .. (w - 1) ] 
         |> List.map (fun x -> if Screen.get screen (x, y) then "#" else ".")
         |> String.concat ""
     [ 0 .. (h - 1) ] 
     |> List.map createRow |> String.concat "\n" |> printfn "%s"
+    printfn ""
 
 let run fileName = 
     let lines = readLines fileName
     let operations = lines |> List.choose tryParse
     operations |> printfn "%A"
-    let screen = Screen.create 50 6 
+    // let screen = Screen.create 50 6 
+    let screen = Screen.create 7 3
     visualize screen
+    Screen.rect screen (3, 2)
+    visualize screen
+    Screen.rotateColumn screen 1 1
+    visualize screen
+     
     
 
 run "sample"
