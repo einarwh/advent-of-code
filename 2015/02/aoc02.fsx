0e28f427a1daff14e69d3cb4fbac2074152f5df4
Einar W. Høst <einarwh@gmail.com>
2025-07-17 22:39:33 +0200

    2015/02 solved

--- 2015/02/aoc02.fsx
+++ 2015/02/aoc02.fsx
@@ -1,8 +1,20 @@
-// Advent of Code 2015. Day 02
+// Advent of Code 2015. Day 02: I Was Told There Would Be No Math.
 // dotnet fsi aoc02.fsx
 
 open System
 open System.IO
+
+let parse (s : string) = 
+    match s.Split("x") |> Array.map int |> Array.sort with 
+    | [|a; b; c|] -> Some (a, b, c)
+    | _ -> None
+
+
+let paper (a, b, c) = 
+    3 * a * b + 2 * a * c + 2 * b * c
+
+let ribbon (a, b, c) = 
+    a + a + b + b + a * b * c
 
 let readLines = 
     File.ReadAllLines
@@ -11,6 +23,8 @@ let readLines =
 
 let run fileName = 
     let lines = readLines fileName
-    lines |> printfn "%A"
+    let boxes = lines |> List.choose parse
+    boxes |> List.map paper |> List.sum |> printfn "%d"
+    boxes |> List.map ribbon |> List.sum |> printfn "%d"
 
 run "input"
