50789dc188b2baff880e5f6ef0996744ed02e9d5
Einar W. Høst <einarwh@gmail.com>
2025-07-18 15:41:55 +0200

    2017/06 solved

--- 2017/06/aoc06.fsx
+++ 2017/06/aoc06.fsx
@@ -4,6 +4,27 @@
 open System
 open System.IO
 
+let redistribute arr =
+  let m = Array.max arr
+  let i = Array.findIndex (fun x -> x = m) arr
+  arr.[i] <- 0
+  let next i = (i + 1) % Array.length arr 
+  let mutable j = i
+  let mutable blocks = m
+  while blocks > 0 do 
+    blocks <- blocks - 1
+    j <- next j
+    arr.[j] <- arr.[j] + 1
+  arr
+
+let solve arr = 
+  let rec loop seen ary = 
+    if List.contains ary seen then 
+      ary, List.length seen
+    else 
+      ary |> Array.copy |> redistribute |> loop (ary::seen) 
+  loop [] arr
+
 let readText fileName = 
     File.ReadAllText(fileName).Trim()
 
@@ -13,9 +34,11 @@ let readLines =
     >> Array.toList
 
 let run fileName = 
-    let lines = readLines fileName
-    lines |> printfn "%A"
     let text = readText fileName
-    text |> printfn "%s"
+    let banks = text.Split "\t"  |> Array.map int
+    let loopDistribution, steps = solve banks 
+    steps |> printfn "%d"
+    let _, cycles = solve loopDistribution
+    cycles |> printfn "%d"
 
-run "input"
+run "input.txt"
