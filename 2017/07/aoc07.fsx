7050e6520959b51b91fc783c56ca464b536f43e9
Einar W. Høst <einarwh@gmail.com>
2025-07-18 16:58:00 +0200

    2017/07 solved

--- 2017/07/aoc07.fsx
+++ 2017/07/aoc07.fsx
@@ -4,7 +4,6 @@
 open System
 open System.IO
 
-
 type Name = string
 
 type Weight = int
@@ -13,73 +12,72 @@ type Disc = Name * Weight
 
 type Tree = Leaf of Disc | Branch of Disc * Tree list 
 
+exception UnbalancedTreeException of int
+
 let split (sep : string) (s : string) =  
-  s.Split([|sep|], StringSplitOptions.None) |> Seq.toList  
+    s.Split([|sep|], StringSplitOptions.None) |> Seq.toList  
 
 let readWeight (s : string) : Weight = 
-  s.Substring(1, s.Length - 2) |> int
+    s.Substring(1, s.Length - 2) |> int
 
 let readLeft (a : string) = 
-  match split " " a with 
-  | [n; w] ->
-    Some (n, readWeight w)
-  | x -> None
+    match split " " a with 
+    | [n; w] ->
+        Some (n, readWeight w)
+    | x -> None
   
 let readLine (line : string) = 
-  if line.Contains(" -> ") then 
-    match split " -> " line with 
-    | [a; b] -> 
-      readLeft a |> Option.map (fun (n, w) -> (n, (w, split ", " b))) 
-    | _-> None
-  else 
-    readLeft line |> Option.map (fun (n, w) -> (n, (w, [])))
+    if line.Contains(" -> ") then 
+        match split " -> " line with 
+        | [a; b] -> 
+            readLeft a |> Option.map (fun (n, w) -> (n, (w, split ", " b))) 
+        | _-> None
+    else 
+        readLeft line |> Option.map (fun (n, w) -> (n, (w, [])))
 
 let rec buildTree root (map : Map<Name, Weight * Name list>) : Tree = 
-  let (w, names) = Map.find root map
-  let disc = root, w
-  match names with 
-  | [] -> Leaf disc
-  | _ -> Branch (disc, names |> List.map (fun n -> buildTree n map)) 
+    let (w, names) = Map.find root map
+    let disc = root, w
+    match names with 
+    | [] -> Leaf disc
+    | _ -> Branch (disc, names |> List.map (fun n -> buildTree n map)) 
 
 let getRootDisc = function
-  | Leaf d1 -> d1
-  | Branch (d2, _) -> d2
+    | Leaf d1 -> d1
+    | Branch (d2, _) -> d2
 
-let findCorrectWeight (weights : Weight list) (branches : Tree list) = 
+let calculateCorrectWeight (weights : Weight list) (branches : Tree list) = 
     let discs = branches |> List.map getRootDisc
     let summaries = 
         List.zip discs weights 
         |> List.map (fun ((_, wx), weight) -> wx, weight)
-    summaries 
-    |> List.groupBy snd 
-    |> List.map (fun (w, lst) -> (w, lst |> List.map fst))
-    |> List.sortBy (fun (_, lst) -> List.length lst)
+        |> List.groupBy snd 
+        |> List.map (fun (w, lst) -> (w, lst |> List.map fst))
+        |> List.sortBy (fun (_, lst) -> List.length lst)
+    match summaries with 
+    | [(unbalanced, [w]); (balanced, _)] -> 
+        w + balanced - unbalanced
+    | _ -> failwith "?"
 
 let rec checkTree root : Weight = 
-  match root with
-  | Leaf (n, w) -> w
-  | Branch ((n, w), bs) ->
-    let weights = bs |> List.map checkTree
-    match weights with 
-    | [] -> w
-    | h :: t ->
-      if List.exists (fun x -> x <> h) t then 
-        let correct = findCorrectWeight weights bs 
-        printfn "correct %A" correct
-        let discs = bs |> List.map getRootDisc
-        List.zip discs weights 
-        |> List.map (fun ((n, wx), weight) -> n, wx, weight)
-        |> sprintf "??? %A" 
-        |> failwith 
-      else 
-        w + List.sum weights
+    match root with
+    | Leaf (n, w) -> w
+    | Branch ((n, w), bs) ->
+        let weights = bs |> List.map checkTree
+        match weights with 
+        | [] -> w
+        | h :: t ->
+            if List.exists (fun x -> x <> h) t then 
+                let correct = calculateCorrectWeight weights bs 
+                raise (UnbalancedTreeException correct)
+            else 
+                w + List.sum weights
  
-let solve root lines = 
-  let solidify = Seq.map readLine >> Seq.choose id >> Seq.toList
-  let map = lines |> solidify |> Map.ofList
-  let tree = buildTree root map
-  checkTree tree
-  ()
+let findCorrectWeight tree =
+    try 
+        checkTree tree 
+    with
+        UnbalancedTreeException weight -> weight
 
 let readLines = 
     File.ReadAllLines
@@ -107,13 +105,10 @@ let findRoot (map : Map<Name, Name>) =
 let run fileName = 
     let lines = readLines fileName
     let map = lines |> List.choose readLine |> Map.ofList
-    printfn "%A" map
     let revMap = reverseMap map
     let root = findRoot revMap
-    printfn "root %A" root
+    root |> printfn "%s"
     let tree = buildTree root map
-    printfn "%A" tree
-    checkTree tree
-    // lines |> printfn "%A"
+    tree |> findCorrectWeight |> printfn "%d"
 
-run "sample.txt"
+run "input.txt"
