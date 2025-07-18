869789607205712def4f1e35d0112847620e5f40
Einar W. Høst <einarwh@gmail.com>
2025-07-18 15:26:28 +0200

    2017/05 solved

--- 2017/05/aoc05.fsx
+++ 2017/05/aoc05.fsx
@@ -4,18 +4,28 @@
 open System
 open System.IO
 
-let readText fileName = 
-    File.ReadAllText(fileName).Trim()
+let solve inc offs = 
+  let offsets = Array.copy offs 
+  let rec loop ix steps = 
+    if ix < 0 || ix >= Array.length offsets then 
+      steps
+    else 
+      let off = offsets.[ix]
+      offsets.[ix] <- off + inc off
+      loop (ix + off) (steps + 1)
+  loop 0 0
 
+let solve1 = solve (fun x -> 1)
+let solve2 = solve (fun x -> if x > 2 then -1 else 1) 
+ 
 let readLines = 
     File.ReadAllLines
     >> Array.filter (fun line -> line <> String.Empty)
-    >> Array.toList
 
 let run fileName = 
     let lines = readLines fileName
-    lines |> printfn "%A"
-    let text = readText fileName
-    text |> printfn "%s"
+    let offsets = lines |> Array.map int 
+    offsets |> solve1 |> printfn "%d"
+    offsets |> solve2 |> printfn "%d"
 
-run "input"
+run "input.txt"
