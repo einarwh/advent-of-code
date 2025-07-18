81d42a3ba6293a1535d6e7b414e4680b407aead1
Einar W. Høst <einarwh@gmail.com>
2025-07-19 01:02:00 +0200

    2017/15 solved

--- 2017/15/aoc15.fsx
+++ 2017/15/aoc15.fsx
@@ -4,8 +4,33 @@
 open System
 open System.IO
 
-let readText fileName = 
-    File.ReadAllText(fileName).Trim()
+let genseq factor = 
+  let state x = Some (x, x)
+  Seq.unfold (fun prev -> state <| (prev * factor) % 2147483647L)
+
+let mask x= 
+  x &&& (int64 0xFFFF)
+
+let solve count seqA seqB =
+   Seq.zip (seqA |> Seq.map mask) (seqB |> Seq.map mask) 
+   |> Seq.take count 
+   |> Seq.choose (fun (x, y) -> if x = y then Some (x, y) else None) 
+   |> Seq.length
+
+let solve1 a b =
+   let seqA = genseq 16807L a
+   let seqB = genseq 48271L b
+   solve 40000000 seqA seqB
+
+let solve2 a b =
+   let filter multiple = 
+      Seq.choose (fun v -> if v % multiple = 0L then Some v else None)
+   let seqA = genseq 16807L a |> filter 4L
+   let seqB = genseq 48271L b |> filter 8L
+   solve 5000000 seqA seqB
+
+let parseNumber (s : string) = 
+    s.Split(" ") |> Array.toList |> List.rev |> List.head |> int64 
 
 let readLines = 
     File.ReadAllLines
@@ -14,8 +39,10 @@ let readLines =
 
 let run fileName = 
     let lines = readLines fileName
-    lines |> printfn "%A"
-    let text = readText fileName
-    text |> printfn "%s"
+    match lines |> List.map parseNumber with 
+    | [genA; genB] -> 
+        solve1 genA genB |> printfn "%d"
+        solve2 genA genB |> printfn "%d"
+    | _ -> failwith "?"
 
-run "input"
+run "input.txt"
