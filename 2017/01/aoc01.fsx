f0983305357af0b733344a875d3e57490e748c73
Einar W. Høst <einarwh@gmail.com>
2025-07-18 14:10:23 +0200

    2017/01 solved

--- 2017/01/aoc01.fsx
+++ 2017/01/aoc01.fsx
@@ -4,18 +4,29 @@
 open System
 open System.IO
 
+let rot = function
+  | [] -> []
+  | h::t -> t @ [h]
+
+let rec times n fn = 
+  if n > 0 then fn >> times (n - 1) fn else id
+
+let solve fn lst = 
+  List.zip lst (fn lst)
+  |> List.choose (fun (x, y) -> if x = y then Some x else None)
+  |> List.sum
+
+let solve1 = solve rot 
+    
+let solve2 lst = solve (times (List.length lst / 2) rot) lst
+    
 let readText fileName = 
     File.ReadAllText(fileName).Trim()
 
-let readLines = 
-    File.ReadAllLines
-    >> Array.filter (fun line -> line <> String.Empty)
-    >> Array.toList
-
 let run fileName = 
-    let lines = readLines fileName
-    lines |> printfn "%A"
     let text = readText fileName
-    text |> printfn "%s"
+    let digits = text |> Seq.map (fun c -> int c - int '0') |> Seq.toList 
+    digits |> solve1 |> printfn "%A"
+    digits |> solve2 |> printfn "%A"
 
-run "input"
+run "input.txt"
