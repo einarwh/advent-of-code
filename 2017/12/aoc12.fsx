edc0c2e85b2b698021365a17d5b45cfec2bf8ad6
Einar W. Høst <einarwh@gmail.com>
2025-07-19 00:38:58 +0200

    2017/12 solved

--- 2017/12/aoc12.fsx
+++ 2017/12/aoc12.fsx
@@ -4,8 +4,45 @@
 open System
 open System.IO
 
-let readText fileName = 
-    File.ReadAllText(fileName).Trim()
+let contains n = List.exists (fun m -> n = m)
+  
+let rec group lookup members key = 
+    if contains key members then members
+    else  
+        let members' = key :: members
+        lookup key 
+        |> List.filter (not << fun t -> contains t members')
+        |> List.fold (group lookup) members'
+  
+let makeLookup vals = 
+    let map = Map.ofList vals
+    fun it -> Map.find it map 
+
+let solve1 vals = 
+    group (makeLookup vals) [] 0 |> List.length
+
+let solve2 vals = 
+    let lookup = makeLookup vals
+    let rec solve all gs = function 
+    | [] -> gs
+    | (n, _) :: t ->
+        if contains n all then 
+            solve all gs t
+        else     
+            let g = group lookup [] n
+            solve (g @ all) (g :: gs) t
+    solve [] [] vals |> List.length
+
+let split (splitter : string) (s : string) = 
+    s.Split([|splitter|], StringSplitOptions.None)
+
+let parseLine (line : string) =
+    match line |> split " <-> " with 
+    | [|keyStr; valuesStr|] ->  
+        let key = int keyStr
+        let values = valuesStr |> split ", " |> Array.map int |> Array.toList
+        Some (key, values)
+    | _ -> None
 
 let readLines = 
     File.ReadAllLines
@@ -14,8 +51,8 @@ let readLines =
 
 let run fileName = 
     let lines = readLines fileName
-    lines |> printfn "%A"
-    let text = readText fileName
-    text |> printfn "%s"
+    let values = lines |> List.choose parseLine 
+    values |> solve1 |> printfn "%d"
+    values |> solve2 |> printfn "%A"
 
-run "input"
+run "input.txt"
