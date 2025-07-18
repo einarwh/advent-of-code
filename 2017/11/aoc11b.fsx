dfca3d928cf728c59c3015abe317945cfec21842
Einar W. Høst <einarwh@gmail.com>
2025-07-18 23:30:11 +0200

    aoc11

--- 2017/11/aoc11b.fsx
+++ 2017/11/aoc11b.fsx
@@ -87,20 +87,72 @@ let round (nw0, n0, ne0, se0, s0, sw0) =
   let (nw6, n6, ne6, se6, s6, sw6) = make_nw (nw5, n5, ne5, se5, s5, sw5)
   (nw6, n6, ne6, se6, s6, sw6)
 
-// let line = File.ReadAllText("C:/einarwh/temp/day11.txt")
-// let allmoves = line.Trim().Split(',') |> Seq.toList
-// allmoves |> List.length |> printfn "%d"
+let walk (d, (nw, n, ne, se, s, sw)) = function 
+  | "nw" ->
+    if se > 0 then // opposite 
+      (d, (nw, n, ne, se - 1, s, sw))
+    else if ne > 0 then // clockwise
+      (d, (nw, n + 1, ne - 1, se, s, sw))
+    else if s > 0 then // counterclockwise
+      (d, (nw, n, ne, se, s - 1, sw + 1))
+    else 
+      let d' = max d (nw + n + ne + se + s + sw + 1)
+      (d', (nw + 1, n, ne, se, s, sw)) 
+  | "n" ->
+    if s > 0 then // opposite 
+      (d, (nw, n, ne, se, s - 1, sw))
+    else if se > 0 then // clockwise
+      (d, (nw, n, ne + 1, se - 1, s, sw))
+    else if sw > 0 then // counterclockwise
+      (d, (nw + 1, n, ne, se, s, sw - 1))
+    else 
+      let d' = max d (nw + n + ne + se + s + sw + 1)
+      (d', (nw, n + 1, ne, se, s, sw)) 
+  | "ne" ->
+    if sw > 0 then // opposite 
+      (d, (nw, n, ne, se, s, sw - 1))
+    else if s > 0 then // clockwise
+      (d, (nw, n, ne, se + 1, s - 1, sw))
+    else if nw > 0 then // counterclockwise
+      (d, (nw - 1, n + 1, ne, se, s, sw))
+    else 
+      let d' = max d (nw + n + ne + se + s + sw + 1)
+      (d', (nw, n, ne + 1, se, s, sw)) 
+  | "se" ->
+    if nw > 0 then // opposite 
+      (d, (nw - 1, n, ne, se, s, sw))
+    else if sw > 0 then // clockwise
+      (d, (nw, n, ne, se, s + 1, sw - 1))
+    else if n > 0 then // counterclockwise
+      (d, (nw, n - 1, ne + 1, se, s, sw))
+    else 
+      let d' = max d (nw + n + ne + se + s + sw + 1)
+      (d', (nw, n, ne, se + 1, s, sw)) 
+  | "s" ->
+    if n > 0 then // opposite 
+      (d, (nw, n - 1, ne, se, s, sw))
+    else if nw > 0 then // clockwise
+      (d, (nw - 1, n, ne, se, s, sw + 1))
+    else if ne > 0 then // counterclockwise
+      (d, (nw, n, ne - 1, se + 1, s, sw))
+    else 
+      let d' = max d (nw + n + ne + se + s + sw + 1)
+      (d', (nw, n, ne, se, s + 1, sw)) 
+  | "sw" ->
+    if ne > 0 then // opposite 
+      (d, (nw, n, ne - 1, se, s, sw))
+    else if n > 0 then // clockwise
+      (d, (nw + 1, n - 1, ne, se, s, sw))
+    else if se > 0 then // counterclockwise
+      (d, (nw, n, ne, se - 1, s + 1, sw))
+    else 
+      let d' = max d (nw + n + ne + se + s + sw + 1)
+      (d', (nw, n, ne, se, s, sw + 1)) 
+  | _ -> failwith "?"
 
-// let simple = "se,sw,se,sw,sw"
-// simple.Split(',') |> Seq.toList |> List.fold collapse (0, 0, 0) |> spread |> round |> printfn "%A"
-
-// allmoves |> List.fold collapse (0, 0, 0) |> printfn "%A"
-// allmoves |> List.fold collapse (0, 0, 0) |> spread |> printfn "%A"
-// allmoves |> List.fold collapse (0, 0, 0) |> spread |> round |> printfn "%A"
-
-// allmoves |> List.fold collapse' (0, 0, 0, 0, 0, 0) |> printfn "%A"
-
-// 488 + 273 |> printfn "%d"
+let solve moves = 
+  let (maxDistance, (nw, n, ne, se, s, sw)) = moves |> List.fold walk (0, (0, 0, 0, 0, 0, 0)) 
+  (maxDistance, nw + n + ne + se + s + sw)
 
 let readText fileName = 
     File.ReadAllText(fileName).Trim()
@@ -108,6 +160,8 @@ let readText fileName =
 let run fileName = 
     let text = readText fileName
     let moves = text.Split "," |> Array.toList 
-    moves |> List.fold collapse (0, 0, 0) |> spread |> round |> printfn "%A"
+    let (maxDistance, (nw, n, ne, se, s, sw)) = moves |> List.fold walk (0, (0, 0, 0, 0, 0, 0)) 
+    nw + n + ne + se + s + sw |> printfn "%d"
+    maxDistance |> printfn "%d"
 
 run "input.txt"
