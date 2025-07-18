821263d0b19c4352a07145113821e73c556de22d
Einar W. Høst <einarwh@gmail.com>
2025-07-18 17:06:37 +0200

    Update aoc08.fsx

--- 2017/08/aoc08.fsx
+++ 2017/08/aoc08.fsx
@@ -1,11 +1,47 @@
 // Advent of Code 2017. Day 08: I Heard You Like Registers.
+// Solution for part 1 only.
 // dotnet fsi aoc08.fsx
 
 open System
 open System.IO
 
-let readText fileName = 
-    File.ReadAllText(fileName).Trim()
+let comparison = function 
+  | "==" -> (=)
+  | "!=" -> (<>)
+  | ">" -> (>)
+  | ">=" -> (>=)
+  | "<" -> (<)
+  | "<=" -> (<=)
+  | x -> failwith <| sprintf "comparison %s" x
+
+let change = function 
+  | "inc" -> (+)
+  | "dec" -> (-)
+  | x -> failwith <| sprintf "change %s" x
+
+let rec lookup name = function 
+    | [] -> 0
+    | (n, v)::t -> if n = name then v else lookup name t
+
+let rec update n change = function 
+  | [] -> [ n, change 0 ]
+  | (n', v) :: t ->
+    if n = n' then (n, change v) :: t
+    else (n', v) :: update n change t
+  
+let readLine (line : string) =
+  match line.Split() with 
+  | [|n;op;v;_;nc;c;vc|] -> 
+    let p rs = (comparison c) (lookup nc rs) (int vc) 
+    fun rs ->
+      if p rs then
+        update n (fun x -> (change op) x (int v)) rs
+      else rs
+  | x -> failwith <| sprintf "line %s" line
+
+let solve lines =
+  let solver = Seq.map readLine >> Seq.reduce (>>)
+  [] |> solver lines |> Seq.maxBy (fun (_, v) -> v)
 
 let readLines = 
     File.ReadAllLines
@@ -14,8 +50,6 @@ let readLines =
 
 let run fileName = 
     let lines = readLines fileName
-    lines |> printfn "%A"
-    let text = readText fileName
-    text |> printfn "%s"
+    solve lines |> printfn "%A"
 
-run "input"
+run "input.txt"
