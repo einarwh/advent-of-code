ff28f40348416ac22fd2e3b63b8334a08cf8ab5f
Einar W. Høst <einarwh@gmail.com>
2025-07-20 00:58:34 +0200

    fast enough

--- 2016/18/aoc18.fsx
+++ 2016/18/aoc18.fsx
@@ -4,59 +4,37 @@
 open System
 open System.IO
 
-let selectTile (tiles : char array) (i : int) : char = 
-    let readTile ix = 
-        if ix < 0 || ix >= tiles.Length then '.'
-        else tiles[ix]
-    let isTrap ch = ch = '^'
-    let left = readTile (i - 1)
-    let center = readTile i 
-    let right = readTile (i + 1)
-    let trap =  
-        (isTrap left && isTrap center && not <| isTrap right) || 
-        (not <| isTrap left && isTrap center && isTrap right) || 
-        (isTrap left && not <| isTrap center && not <| isTrap right) || 
-        (not <| isTrap left && not <| isTrap center && isTrap right)
-    if trap then '^' else '.'
+let elements = [
+    [|'.';'.';'.'|], '.'
+    [|'.';'.';'^'|], '^'
+    [|'.';'^';'.'|], '.'
+    [|'.';'^';'^'|], '^'
+    [|'^';'.';'.'|], '^'
+    [|'^';'.';'^'|], '.'
+    [|'^';'^';'.'|], '^'
+    [|'^';'^';'^'|], '.'
+]
 
-let getNextRow (tiles : char array) = 
-    [|0 .. tiles.Length - 1|] |> Array.map (selectTile tiles)
+let lookup = Map(elements)
 
-let getRows (n : int) (row : char array) = 
-    let rec fn acc row = 
-        if List.length acc < n then 
+
+let getNextRow (row : string) =
+    let exp = "." + row + "."
+    exp |> Seq.windowed 3 |> Seq.map (fun cs -> Map.find cs lookup) |> Seq.toArray |> fun cs -> new string(cs)
+
+let countSafeTiles (rowCount : int) (row : string) = 
+    let rec fn safe count row = 
+        let safe' = safe + (row |> Seq.sumBy (fun ch -> if ch = '^' then 0 else 1))
+        if count < rowCount then 
             let nxt = getNextRow row 
-            fn (nxt :: acc) nxt 
+            fn safe' (count + 1) nxt
         else 
-            acc |> List.rev
-    fn [row] row
-
-let getRowsX (n : int) (row : char array) = 
-    let rec fn acc seen row = 
-        if (List.length acc) % 10000 = 0 then printfn "acc %d" (List.length acc)
-        if List.length acc < n then 
-            let nxt = getNextRow row 
-            if seen |> Set.contains nxt then 
-                printfn "LOOP!"
-                acc |> List.rev
-            else 
-                fn (nxt :: acc) (seen |> Set.add nxt) nxt 
-        else 
-            acc |> List.rev
-    fn [row] Set.empty row
-
-let countSafeTiles (rows : char array list) = 
-    rows 
-    |> List.collect (fun cs -> cs |> Array.toList)
-    |> List.sumBy (fun ch -> if ch = '^' then 0 else 1)
+            safe'
+    fn 0 1 row 
 
 let run rowCount fileName = 
     let text = File.ReadAllText(fileName).Trim()
-    text |> Seq.toArray |> getNextRow |> fun chars -> new string(chars) |> printfn "%s"
-    let rows = text |> Seq.toArray |> getRowsX rowCount
-    rows |> countSafeTiles |> printfn "%d"
+    text |> countSafeTiles rowCount |> printfn "%d"
 
-// run 3 "sample1.txt"
-// run 10 "sample2.txt"
 run 40 "input.txt"
 run 400000 "input.txt"
