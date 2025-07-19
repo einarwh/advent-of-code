cfae05ee2e91d143779553f294fe090e0e7b311e
Einar W. Høst <einarwh@gmail.com>
2025-07-19 21:05:09 +0200

    2016/20 solved

--- 2016/20/aoc20.fsx
+++ 2016/20/aoc20.fsx
@@ -25,6 +25,25 @@ let findLowestNonBlockedIp ranges =
                 current 
     find 0 ranges 
 
+let combineRanges (ranges : (int64*int64) list) : (int64*int64) list = 
+    let rec fn acc (currentFirst, currentLast) ranges = 
+        // printfn "combining... %A %A %A" acc (currentFirst, currentLast) ranges
+        match ranges with 
+        | [] -> ((currentFirst, currentLast) :: acc) |> List.rev 
+        | (first, last) :: rest -> 
+            if first < currentFirst then 
+                failwith "not sorted?"
+            else 
+                if first > currentLast + 1L then 
+                    // printfn "GAP"
+                    // Gap! 
+                    fn ((currentFirst, currentLast) :: acc) (first, last) rest
+                else 
+                    fn acc (currentFirst, max currentLast last) rest 
+    match ranges with 
+    | [] -> failwith "?"
+    | h :: t -> fn [] h t
+
 let readLines = 
     File.ReadAllLines
     >> Array.filter (fun line -> line <> String.Empty)
@@ -33,7 +52,11 @@ let readLines =
 let run fileName = 
     let lines = readLines fileName
     let ranges = lines |> List.choose parse |> List.sort 
-    // ranges |> List.iter (fun (a, b) -> printfn "%d-%d" a b)
-    ranges |> findLowestNonBlockedIp |> printfn "%A"
+    let combined = ranges |> combineRanges
+    combined |> List.head |> snd |> (fun last -> last + 1L) |> printfn "%d"
+    let sumBlocked = combined |> List.map (fun (a, b) -> b - a + 1L) |> List.sum
+    let total = 4294967295L + 1L
+    let sumAllowed = total - sumBlocked
+    sumAllowed |> printfn "%d"
 
 run "input.txt"
