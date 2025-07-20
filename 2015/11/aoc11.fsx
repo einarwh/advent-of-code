799399d8456aabd42cd4425a149b88eb7aa4642e
Einar W. Høst <einarwh@gmail.com>
2025-07-20 21:27:23 +0200

    clean-up

--- 2015/11/aoc11.fsx
+++ 2015/11/aoc11.fsx
@@ -4,37 +4,6 @@
 open System
 open System.IO
 
-// let findNext (s : string) = 
-//     let skipTo (ch : char) (pos : int) (chars : char array) =
-//         let getChar ix = 
-//             if ix = pos then ch 
-//             else if ix < pos then chars[ix]
-//             else 'a'
-//         [|0 .. chars.Length - 1|] |> Array.map getChar
-//         chars
-//     let nextWithoutIol (chars : char array) =
-//         let rec fn pos =
-//             if pos < chars.Length then 
-//                 match chars[pos] with 
-//                 | 'i' -> skipTo 'j' pos chars 
-//                 | 'o' -> skipTo 'p' pos chars
-//                 | 'l' -> skipTo 'm' pos chars
-//                 | _ -> fn (pos + 1) 
-//             else 
-//                 chars
-//         fn 0
-//     let rec increment (pos : int) (chars : char array) = 
-//         let ch = chars[pos]
-
-//     let rec loop (chars : char array) = 
-//         let cs = nextWithoutIol chars
-//         let rec
-//         // Increment
-
-
-    
-//     s
-
 let nextChar c = if c = 'z' then 'a' else c |> int |> (+) 1 |> char 
 
 let nextIncremental (s : string) = 
@@ -52,16 +21,12 @@ let nextIncremental (s : string) =
     new string (chars)
 
 let nextWithoutLetter (c : char) (s : string) = 
-    // printfn "next without letter %c" c
     let ix = s.IndexOf c 
-    // printfn "found %c in %s at index %d" c s ix 
     if ix < 0 then s 
     else 
         let before = s.Substring(0, ix) 
-        // printfn "before '%s'" before 
         let afterLen = s.Length - (ix + 1)
         let after = new string('a', afterLen)
-        // printfn "after '%s'" after
         before + (nextChar c).ToString() + after
 
 let nextWithoutLetters (s : string) = 
@@ -96,9 +61,7 @@ let hasOverlappingPairs (s : string) =
 
 let findNextPassword (s : string) = 
     let rec fn (s : string) = 
-        // printfn "try find next password for %s" s
         let nxt = nextIncremental s |> nextWithoutLetters
-        // printfn "next candidate %s" nxt
         if hasIncreasingSequence nxt && hasOverlappingPairs nxt then 
             nxt 
         else 
@@ -107,13 +70,9 @@ let findNextPassword (s : string) =
 
 let run fileName = 
     let text = File.ReadAllText(fileName).Trim()
-    // "abcdefgh" |> findNextPassword |> printfn "%s"
-    // "ghijklmn" |> findNextPassword |> printfn "%s"
     let nxt1 = text |> findNextPassword
     let nxt2 = nxt1 |> findNextPassword
     nxt1 |> printfn "%s"
     nxt2 |> printfn "%s"
-    // "abcdefgh" |> nextIncremental |> nextWithoutLetter 'i' |> printfn "%s"
-    // "ghijklmn" |> nextIncremental |> nextWithoutLetter 'i' |> printfn "%s"
 
 run "input.txt"
