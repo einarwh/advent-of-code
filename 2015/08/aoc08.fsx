a9f3715a8e858a957657078a62c3a5a34e33ba1a
Einar W. Høst <einarwh@gmail.com>
2025-07-20 02:02:54 +0200

    rename

--- 2015/08/aoc08.fsx
+++ 2015/08/aoc08.fsx
@@ -1,8 +1,22 @@
-// Advent of Code 2015. Day 08
+// Advent of Code 2015. Day 08: Matchsticks.
 // dotnet fsi aoc08.fsx
 
 open System
 open System.IO
+
+let countCodeLength (s : string) = 
+    s.Length 
+
+let countMemoryLength (s : string) = 
+    let rec fn count chars = 
+        match chars with 
+        | [] -> count 
+        | '\\' :: '\\' :: rest  
+        | '\\' :: '"' :: rest  
+        | '\\' :: 'x' :: _ :: _ :: rest 
+        | _ :: rest ->
+            fn (count + 1) rest
+    s |> Seq.toList |> fn 0 
 
 let readLines = 
     File.ReadAllLines
@@ -12,7 +26,8 @@ let readLines =
 let run fileName = 
     let lines = readLines fileName
     lines |> printfn "%A"
-    let text = File.ReadAllText(fileName).Trim()
-    text |> printfn "%s"
+    let codeLength = lines |> List.map countCodeLength |> List.sum
+    let memoryLength = lines |> List.map countMemoryLength |> List.sum
+    codeLength - memoryLength |> printfn "%d"
 
-run "input.txt"
+run "sample.txt"
