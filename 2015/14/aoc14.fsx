8e75bf79ec6633429fafb9a9572b272b83748315
Einar W. Høst <einarwh@gmail.com>
2025-07-21 01:11:03 +0200

    2015/14 part 1 ok sample

--- 2015/14/aoc14.fsx
+++ 2015/14/aoc14.fsx
@@ -3,6 +3,38 @@
 
 open System
 open System.IO
+open System.Text.RegularExpressions
+
+type Reindeer = {
+    name : string 
+    speed : int 
+    flytime : int 
+    resttime : int 
+}
+
+let parse (s : string) : Reindeer option =
+    let m = Regex.Match(s, "^([A-Za-z]+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds\.$")
+    if m.Success then
+        let reindeer = {
+            name = m.Groups.[1].Value
+            speed = int m.Groups.[2].Value
+            flytime = int m.Groups.[3].Value
+            resttime = int m.Groups.[4].Value
+        }
+        Some reindeer
+    else
+        None
+
+let travel (timeLimit : int) (reindeer : Reindeer) = 
+    let rec loop elapsed distance = 
+        if elapsed < timeLimit then 
+            let flytime = min reindeer.flytime (timeLimit - elapsed)
+            let elapsedAfterFlying = elapsed + flytime
+            let distanceAfterFlying = distance + flytime * reindeer.speed 
+            loop (elapsedAfterFlying + reindeer.resttime) distanceAfterFlying
+        else 
+            distance 
+    loop 0 0 
 
 let readLines = 
     File.ReadAllLines
@@ -12,7 +44,8 @@ let readLines =
 let run fileName = 
     let lines = readLines fileName
     lines |> printfn "%A"
-    let text = File.ReadAllText(fileName).Trim()
-    text |> printfn "%s"
+    let reindeerList = lines |> List.choose parse
+    printfn "%A" reindeerList
+    reindeerList |> List.map (travel 1000) |> printfn "%A"
 
-run "input.txt"
+run "sample.txt"
