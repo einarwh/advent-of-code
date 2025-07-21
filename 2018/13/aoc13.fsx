5281180fd84e55cbaff8dde3393d285b17a1fed8
Einar W. Høst <einarwh@gmail.com>
2025-07-21 21:40:56 +0200

    init

---
+++ 2018/13/aoc13.fsx
@@ -0,0 +1,18 @@
+// Advent of Code 2018. Day 13
+// dotnet fsi aoc13.fsx
+
+open System
+open System.IO
+
+let readLines = 
+    File.ReadAllLines
+    >> Array.filter (fun line -> line <> String.Empty)
+    >> Array.toList
+
+let run fileName = 
+    let lines = readLines fileName
+    lines |> printfn "%A"
+    let text = File.ReadAllText(fileName).Trim()
+    text |> printfn "%s"
+
+run "input.txt"
