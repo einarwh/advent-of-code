21f8ef250ab7dd8710f6a256239c0154c777133b
Einar W. Høst <einarwh@gmail.com>
2025-07-22 01:50:58 +0200

    init

---
+++ 2018/15/aoc15.fsx
@@ -0,0 +1,16 @@
+// Advent of Code 2018. Day 15: Beverage Bandits.
+// dotnet fsi aoc15.fsx
+
+open System
+open System.IO
+
+let readLines = 
+    File.ReadAllLines
+    >> Array.filter (fun line -> line <> String.Empty)
+    >> Array.toList
+
+let run fileName = 
+    let lines = readLines fileName
+    lines |> printfn "%A"
+
+run "input.txt"
