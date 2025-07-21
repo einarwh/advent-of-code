aff285b5847a967755758c577373046e7cbd102b
Einar W. Høst <einarwh@gmail.com>
2025-07-21 19:24:32 +0200

    2018/07 part 1

--- 2018/07/aoc07.fsx
+++ 2018/07/aoc07.fsx
@@ -4,15 +4,38 @@
 open System
 open System.IO
 
+let parse (s : string) = 
+    let a = s.Split(" ")
+    a[7], a[1]
+
 let readLines = 
     File.ReadAllLines
     >> Array.filter (fun line -> line <> String.Empty)
     >> Array.toList
 
+let determineOrder (lst : List<string*Set<string>>) = 
+    let rec loop (steps : string list) (lst : List<string*Set<string>>) = 
+        let candidates = lst |> List.filter (fun (_, reqs) -> Set.isEmpty reqs) |> List.sort
+        match candidates with 
+        | [] -> steps |> List.rev 
+        | (chosenStep, _) :: _ -> 
+            let lst' = 
+                lst |> List.map (fun (s, reqs) -> (s, reqs |> Set.remove chosenStep))
+                    |> List.filter (fun (s, _) -> s <> chosenStep)
+            loop (chosenStep :: steps) lst'
+    loop [] lst
+
 let run fileName = 
     let lines = readLines fileName
-    lines |> printfn "%A"
-    let text = File.ReadAllText(fileName).Trim()
-    text |> printfn "%s"
+    let lst = 
+        lines 
+        |> List.map parse 
+        |> List.groupBy fst 
+        |> List.map (fun (step, rs) -> (step, rs |> List.map snd |> Set.ofList))
+    let allStepsInReqs = lst |> List.fold (fun set (_, rs) -> Set.union rs set) Set.empty |> Set.toList
+    let allStepsWithReqs = lst |> List.map fst 
+    let stepsWithoutReqs = Set.difference (Set.ofList allStepsInReqs) (Set.ofList allStepsWithReqs) |> Set.toList
+    let lst' = (stepsWithoutReqs |> List.map (fun s -> (s, Set.empty))) @ lst
+    lst' |> determineOrder |> String.concat "" |> printfn "%s"
 
 run "input.txt"
