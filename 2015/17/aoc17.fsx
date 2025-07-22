560eb22345488eb6d415b26d2923f6acf133cf8a
Einar W. Høst <einarwh@gmail.com>
2025-07-23 00:14:18 +0200

    2015/17 solved

--- 2015/17/aoc17.fsx
+++ 2015/17/aoc17.fsx
@@ -6,20 +6,6 @@ open System.IO
 
 let parse (s : string) = int s
 
-let findCombinations (amount : int) (nums : int list) = 
-    let rec loop (numbers : int list) = 
-        match numbers with 
-        | [] -> [[]]
-        | n :: rest -> 
-            let subPermutations = loop rest 
-            let withNumber = 
-                subPermutations 
-                |> List.map (fun perm -> n :: perm)
-                |> List.filter (fun perm -> List.sum perm <= amount) 
-            let combined = withNumber @ subPermutations
-            combined
-    loop nums |> List.filter (fun perm -> List.sum perm = amount) |> List.length
-
 let findPermutations (amount : int) (nums : int list) = 
     let rec loop (numbers : int list) = 
         match numbers with 
@@ -43,6 +29,14 @@ let run amount fileName =
     let lines = readLines fileName
     let permutations = lines |> List.map parse |> findPermutations amount 
     permutations |> List.length |> printfn "%d"
+    let ways = 
+        permutations 
+        |> List.groupBy (fun p -> List.length p) 
+        |> List.map (fun (count, lst) -> (count, List.length lst)) 
+        |> List.sort
+        |> List.head
+        |> snd 
+    ways |> printfn "%d"
 
-run 25 "sample.txt"
+// run 25 "sample.txt"
 run 150 "input.txt"
