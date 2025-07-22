ab605e4465a2912b1aea48dee707b6c65b688042
Einar W. Høst <einarwh@gmail.com>
2025-07-22 18:47:20 +0200

    clean-up

--- 2018/08/aoc08.fsx
+++ 2018/08/aoc08.fsx
@@ -19,34 +19,23 @@ let parse (s : string) =
     s.Split(" ") |> Array.map int
 
 let rec buildNode (index : int) (data : int array) : int * Node =
-    // printfn "build node @ index %d" index
     let childNodeCount = data[index]
     let metadataEntryCount = data[index + 1]
-    // printfn "reading child node count @ index %d: %d" index childNodeCount
-    // printfn "reading meta entry count @ index %d: %d" (index + 1) metadataEntryCount
-
     let header = {
         childNodeCount = childNodeCount
         metadataEntryCount = metadataEntryCount
     }
     let rec buildChildNodes (acc : Node list) (childNumber : int) (index : int) =
-        // printfn "buildChildNodes child node #%d @ index %d" childNumber index
         if childNumber < childNodeCount then
-            // printfn "try read child node #%d @ index %d" childNumber index
             let ix, child = buildNode index data
-            // printfn "after reading child node #%d, index %d" childNumber index
             buildChildNodes (child :: acc) (childNumber + 1) ix
         else
-            // printfn "done reading child node #%d @ index %d" childNumber index
             (index, acc |> List.rev)
     let readMetadataEntries (index : int) =
-        // printfn "try read metadata @ index %d" index
         let entries = [ index .. index + metadataEntryCount - 1 ] |> List.map (fun i -> data[i])
         (index + metadataEntryCount - 1, entries)
     let ixAfterChildren, childNodes = buildChildNodes [] 0 (index + 2)
-    // printfn "after reading children @ index %d" ixAfterChildren
     let ixAfterMetadata, metadataEntries = readMetadataEntries ixAfterChildren
-    // printfn "after reading metadata @ index %d" ixAfterMetadata
     let node = { header = header; childNodes = childNodes; metadataEntries = metadataEntries }
     (ixAfterMetadata + 1, node)
 
@@ -56,37 +45,17 @@ let rec sumMetadata (node : Node) =
     sum1 + sum2
 
 let rec nodeValue (node : Node) : int = 
-    // printfn "========================="
-    // printfn "nodeValue %A" node.header
     if node.childNodes |> List.isEmpty then 
-        let metadataSum = node.metadataEntries |> List.sum
-        // printfn "metadataSum: %d" metadataSum
-        metadataSum
+        node.metadataEntries |> List.sum
     else 
-        // printfn "has children!"
-        // printfn "children:" 
-        // node.childNodes |> List.iter (printfn "%A")
-        // printfn ""
-        // printfn "metadata: %A" node.metadataEntries 
-        // printfn ""
-        let selected = 
-            node.metadataEntries
-            |> List.choose (fun i -> List.tryItem (i - 1) node.childNodes)
-        // printfn "selected:" 
-        // selected |> List.iter (printfn "%A")
-        // printfn ""
-        let childValues = selected |> List.map nodeValue 
-        // printfn "childValues: %A" childValues 
-        // printfn ""
-        // let childSum = childValues |> List.sum 
-        // printfn "childSum: %d" childSum 
-        // printfn ""
-        // printfn "========================="
-        childValues |> List.sum 
+        node.metadataEntries
+        |> List.choose (fun i -> List.tryItem (i - 1) node.childNodes)
+        |> List.map nodeValue 
+        |> List.sum
 
 let run fileName =
     let text = File.ReadAllText(fileName).Trim()
-    let (_, node) = text |> parse |> buildNode 0
+    let node = text |> parse |> buildNode 0 |> snd
     node |> sumMetadata |> printfn "%d"
     node |> nodeValue |> printfn "%d"
 
