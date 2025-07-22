5fefa793f25c8e1108580076c9831f6fa4cc10ee
Einar W. Høst <einarwh@gmail.com>
2025-07-22 23:10:09 +0200

    init

--- 2015/16/aoc16.fsx
+++ 2015/16/aoc16.fsx
@@ -1,8 +1,21 @@
-// Advent of Code 2015. Day 16
+// Advent of Code 2015. Day 16: Aunt Sue.
 // dotnet fsi aoc16.fsx
 
 open System
 open System.IO
+
+let message = [
+    ("children", 3)
+    ("cats", 7)
+    ("samoyeds", 2)
+    ("pomeranians", 3)
+    ("akitas", 0)
+    ("vizslas", 0)
+    ("goldfish", 5)
+    ("trees", 3)
+    ("cars", 2)
+    ("perfumes", 1)
+]
 
 let readLines = 
     File.ReadAllLines
