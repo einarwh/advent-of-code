bbd6ac3a28984f63a8ab1adae01a74cb130a6acb
Einar W. Høst <einarwh@gmail.com>
2025-07-29 08:26:05 +0200

    moving

--- 2022/17/src/Aoc17.elm
+++ 2022/17/src/Aoc17.elm
@@ -36,7 +36,7 @@ type alias Pos = (Int, Int)
 
 type Dir = N | W | S | E
 
-type Move = Turn | Forward
+type Move = Blow | Fall
 
 type Visit = Vertical | Horizontal | Both 
 
@@ -44,16 +44,18 @@ type Cell = Highlight Char | Plain Char
 
 type Shape = Dash | Cross | Angle | Bar | Box 
 
-type alias Rock = (Pos, Shape)
+-- type alias Rock = (Pos, Shape)
 
-type MoveResult = Moving | Stopped 
+type Rock = Moving (Pos, Shape) | Stopped Shape
 
 type alias Model = 
   { chamber : Array2D Char
-  , rock : Maybe Rock
+  , rock : Rock
   , highestRock : Int
   , dataSource : DataSource
-  , moves : List Char 
+  , move : Move
+  , jetPattern : Array Char 
+  , jetIndex : Int 
   , paused : Bool 
   , finished : Bool 
   , tickInterval : Float 
@@ -80,9 +82,11 @@ initModel dataSource =
   in 
     { chamber = chamber 
     , highestRock = 0
-    , rock = Just rock
+    , rock = Moving rock
     , dataSource = dataSource
-    , moves = []
+    , move = Blow
+    , jetPattern = data |> String.toList |> Array.fromList  
+    , jetIndex = 0
     , paused = True
     , finished = False  
     , tickInterval = defaultTickInterval 
@@ -113,212 +117,149 @@ getAllPositions board =
   in 
     ys |> List.concatMap (\y -> xs |> List.map (\x -> (x, y)))
 
-findRobotLoop : Array2D Char -> List Pos -> Pos 
-findRobotLoop warehouse positions = 
-  case positions of 
-    [] -> (0, 0)
-    (x, y) :: rest -> 
-      case Array2D.get y x warehouse of 
-        Just '@' -> (x, y)
-        _ -> findRobotLoop warehouse rest 
-
-findRobot : Array2D Char -> Pos 
-findRobot warehouse = 
-  findRobotLoop warehouse (getAllPositions warehouse)
-
-moveToOffset : Char -> Pos 
-moveToOffset move = 
-  case move of 
-    '^' -> (0, -1)
-    '<' -> (-1, 0)
-    'v' -> (0, 1)
-    '>' -> (1, 0)
-    _ -> (0, 0)
-
-moveStep move (x, y) = 
-  let 
-    (dx, dy) = moveToOffset move 
-  in 
-    (x + dx, y + dy)
-
-zip : List a -> List b -> List (a, b) 
-zip lst1 lst2 = 
-  case (lst1, lst2) of 
-    ((h1 :: r1), (h2 :: r2)) -> (h1, h2) :: zip r1 r2 
-    _ -> []
-
-tryFindSpaceDoubleLoop : Array2D Char -> Char -> List Pos -> List (Pos, Pos) -> List (Pos, Pos)
-tryFindSpaceDoubleLoop warehouse move positionsToMove swaps = 
-  let 
-    nextPositions = positionsToMove |> List.map (moveStep move)
-    things = nextPositions |> List.map (\(x, y) -> Array2D.get y x warehouse |> Maybe.withDefault '?')
-    proposedSwaps = zip positionsToMove nextPositions
-    nextSwaps = proposedSwaps ++ swaps 
-  in 
-    if things |> List.any (\ch -> ch == '#') then 
-      -- Met a wall! 
-      []
-    else if things |> List.all (\ch -> ch == '.') then 
-      -- Free space for all!
-      nextSwaps 
-    else 
-      -- Boxes...
-      let 
-        positionsAndThings = nextPositions |> List.map (\(x, y) -> ((x, y), Array2D.get y x warehouse |> Maybe.withDefault '?'))
-        openPositions = positionsAndThings |> List.filterMap (\(p, ch) -> if ch == '[' then Just p else Nothing)
-        closePositions = positionsAndThings |> List.filterMap (\(p, ch) -> if ch == ']' then Just p else Nothing)
-        toTheRight (x, y) = (x + 1, y)
-        toTheLeft (x, y) = (x - 1, y)
-        rightPositions = openPositions |> List.map toTheRight 
-        leftPositions = closePositions |> List.map toTheLeft 
-        nextPositionsToMove = (openPositions ++ rightPositions ++ closePositions ++ leftPositions) |> Set.fromList |> Set.toList 
-      in 
-        tryFindSpaceDoubleLoop warehouse move nextPositionsToMove nextSwaps
-
-tryFindSpaceDouble : Array2D Char -> Pos -> Char -> List (Pos, Pos)
-tryFindSpaceDouble warehouse robot move = 
-  tryFindSpaceDoubleLoop warehouse move [ robot ] []
-
-tryFindSpaceSimpleLoop : Array2D Char -> Char -> Pos -> List (Pos, Pos) -> List (Pos, Pos)
-tryFindSpaceSimpleLoop warehouse move pos swaps = 
-  let 
-    (dx, dy) = moveToOffset move 
-    (x, y) = moveStep move pos
-    nextSwaps = (pos, (x, y)) :: swaps 
-  in 
-    case Array2D.get y x warehouse of 
-      Just '#' -> []
-      Just '.' -> nextSwaps 
-      Just '[' -> 
-        tryFindSpaceSimpleLoop warehouse move (x, y) nextSwaps 
-      Just ']' -> 
-        tryFindSpaceSimpleLoop warehouse move (x, y) nextSwaps 
-      Just 'O' -> 
-        tryFindSpaceSimpleLoop warehouse move (x, y) nextSwaps 
-      _ -> [] 
-
-tryFindSpaceSimple : Array2D Char -> Pos -> Char -> List (Pos, Pos) 
-tryFindSpaceSimple warehouse robot move = 
-  tryFindSpaceSimpleLoop warehouse move robot []
-
-tryFindSpace : Bool -> Array2D Char -> Pos -> Char -> List (Pos, Pos) 
-tryFindSpace wide warehouse robot move = 
-  if wide then 
-    case move of 
-      '^' -> tryFindSpaceDouble warehouse robot move 
-      'v' -> tryFindSpaceDouble warehouse robot move 
-      '<' -> tryFindSpaceSimple warehouse robot move 
-      '>' -> tryFindSpaceSimple warehouse robot move 
-      _ -> tryFindSpaceSimple warehouse robot move 
-  else 
-    tryFindSpaceSimple warehouse robot move
-
-moveStuff : Array2D Char -> List (Pos, Pos) -> Array2D Char
-moveStuff warehouse swaps = 
-  case swaps of
-    [] -> warehouse 
-    ((x1, y1), (x2, y2)) :: rest -> 
-      let 
-        maybeCell1 = Array2D.get y1 x1 warehouse
-        maybeCell2 = Array2D.get y2 x2 warehouse
-      in 
-        case (maybeCell1, maybeCell2) of 
-          (Just cell1, Just cell2) -> 
-            let 
-              wh = warehouse |> Array2D.set y1 x1 cell2 |> Array2D.set y2 x2 cell1 
-            in 
-              moveStuff wh rest  
-          _ -> 
-            let 
-              wh = Array2D.set 0 0 '?' warehouse  
-              wh2 = Array2D.set 3 3 '?' wh  
-            in 
-              moveStuff wh2 rest
-
-tryMoveRobot : Bool -> Array2D Char -> Pos -> Char -> (Array2D Char, Pos) 
-tryMoveRobot wide warehouse robot move = 
-  let 
-    swaps = tryFindSpace wide warehouse robot move
-  in 
-    if List.length swaps == 0 then 
-      (warehouse, robot) 
-    else 
-      let 
-        wh = moveStuff warehouse swaps
-        rb = moveStep move robot
-      in 
-        (wh, rb)
-
-swapsToText swaps = 
-  case swaps of 
-    [] -> ""
-    ((x1, y1), (x2, y2)) :: rest -> 
-      let 
-        x1s = String.fromInt x1
-        y1s = String.fromInt y1
-        x2s = String.fromInt x2
-        y2s = String.fromInt y2
-        s = "(" ++ x1s ++ "," ++ y1s ++ ") -> (" ++ x2s ++ "," ++ y2s ++ ")"
-      in 
-        s ++ (swapsToText rest)
-
 tryMoveRight : Rock -> Rock  
-tryMoveRight ((x, y), shape) =
-  case shape of 
-    Dash -> if x + 4 < chamberWidth then ((x + 1, y), Dash) else ((x, y), shape)
-    Cross -> if x + 3 < chamberWidth then ((x + 1, y), Cross) else ((x, y), shape)
-    Angle -> if x + 3 < chamberWidth then ((x + 1, y), Angle) else ((x, y), shape)
-    Bar -> if x + 1 < chamberWidth then ((x + 1, y), Bar) else ((x, y), shape)
-    Box -> if x + 2 < chamberWidth then ((x + 1, y), Box) else ((x, y), shape)
+tryMoveRight rock =
+  case rock of 
+    Stopped _ -> rock 
+    Moving ((x, y), shape) -> 
+      case shape of 
+        Dash -> if x + 4 < chamberWidth then Moving ((x + 1, y), Dash) else Moving ((x, y), shape)
+        Cross -> if x + 3 < chamberWidth then Moving ((x + 1, y), Cross) else Moving ((x, y), shape)
+        Angle -> if x + 3 < chamberWidth then Moving ((x + 1, y), Angle) else Moving ((x, y), shape)
+        Bar -> if x + 1 < chamberWidth then Moving ((x + 1, y), Bar) else Moving ((x, y), shape)
+        Box -> if x + 2 < chamberWidth then Moving ((x + 1, y), Box) else Moving ((x, y), shape)
 
 tryMoveLeft : Rock -> Rock  
-tryMoveLeft ((x, y), shape) =
-  case shape of 
-    Dash -> if x > 0 then ((x - 1, y), Dash) else ((x, y), shape)
-    Cross -> if x > 0 then ((x - 1, y), Cross) else ((x, y), shape)
-    Angle -> if x > 0 then ((x - 1, y), Angle) else ((x, y), shape)
-    Bar -> if x > 0 then ((x - 1, y), Bar) else ((x, y), shape)
-    Box -> if x > 0 then ((x - 1, y), Box) else ((x, y), shape)
+tryMoveLeft rock =
+  case rock of 
+    Stopped _ -> rock 
+    Moving ((x, y), shape) -> 
+      case shape of 
+        Dash -> if x > 0 then Moving ((x - 1, y), Dash) else Moving ((x, y), shape)
+        Cross -> if x > 0 then Moving ((x - 1, y), Cross) else Moving ((x, y), shape)
+        Angle -> if x > 0 then Moving ((x - 1, y), Angle) else Moving ((x, y), shape)
+        Bar -> if x > 0 then Moving ((x - 1, y), Bar) else Moving ((x, y), shape)
+        Box -> if x > 0 then Moving ((x - 1, y), Box) else Moving ((x, y), shape)
 
 tryMoveDown : Rock -> Rock  
-tryMoveDown ((x, y), shape) = 
-  case shape of 
-    Dash -> if y > 0 then ((x, y - 1), Dash) else ((x, y), shape)
-    Cross -> if y > 0 then ((x, y - 1), Cross) else ((x, y), shape)
-    Angle -> if y > 0 then ((x, y - 1), Angle) else ((x, y), shape)
-    Bar -> if y > 0 then ((x, y - 1), Bar) else ((x, y), shape)
-    Box -> if y > 0 then ((x, y - 1), Box) else ((x, y), shape)
+tryMoveDown rock = 
+  case rock of 
+    Stopped _ -> rock 
+    Moving ((x, y), shape) -> 
+      case shape of 
+        Dash -> if y > 0 then Moving ((x, y - 1), Dash) else Moving ((x, y), shape)
+        Cross -> if y > 0 then Moving ((x, y - 1), Cross) else Moving ((x, y), shape)
+        Angle -> if y > 0 then Moving ((x, y - 1), Angle) else Moving ((x, y), shape)
+        Bar -> if y > 0 then Moving ((x, y - 1), Bar) else Moving ((x, y), shape)
+        Box -> if y > 0 then Moving ((x, y - 1), Box) else Moving ((x, y), shape)
 
-moveRock : Char -> Rock -> (MoveResult, Rock) 
+moveRock : Char -> Rock -> Rock 
 moveRock move rock =
   case move of 
-    '>' -> (Moving, tryMoveRight rock)
-    '<' -> (Moving, tryMoveLeft rock) 
-    'v' -> 
-      let 
-        r = tryMoveDown rock 
-      in 
-        if r == rock then (Stopped, r) else (Moving, r)
-    _ -> (Moving, rock)
+    '>' -> tryMoveRight rock
+    '<' -> tryMoveLeft rock 
+    'v' -> tryMoveDown rock
+    _ -> rock
+
+moveRockJet : Char -> Rock -> Rock 
+moveRockJet move rock =
+  case move of 
+    '>' -> tryMoveRight rock
+    '<' -> tryMoveLeft rock
+    _ -> rock
+
+addRockPositions : Array2D Char -> List Pos -> Array2D Char 
+addRockPositions chamber positions = 
+  case positions of 
+    [] -> chamber 
+    (x, y) :: rest -> 
+      addRockPositions (Array2D.set y x '#' chamber) rest 
+
+nextMove : Move -> Move 
+nextMove move = 
+  case move of 
+    Blow -> Fall 
+    Fall -> Blow
+
+nextShape : Shape -> Shape
+nextShape shape = 
+  case shape of 
+    Dash -> Cross 
+    Cross -> Angle 
+    Angle -> Bar 
+    Bar -> Box 
+    Box -> Dash
 
 updateClear : Model -> Model
 updateClear model = 
   initModel model.dataSource
 
+updateBlow : Model -> Model
+updateBlow model = 
+  case model.rock of 
+    Moving _ -> 
+      let 
+        jetMove = model.jetPattern |> Array.get model.jetIndex |> Maybe.withDefault ' '
+        jetIndex = (model.jetIndex + 1) |> modBy (Array.length model.jetPattern)
+        r = moveRockJet jetMove model.rock 
+      in 
+        { model | message = jetMove |> String.fromChar, jetIndex = jetIndex, rock = r, move = Fall }
+    Stopped _ -> 
+      model
+
+findHighestRock : Array2D Char -> Int 
+findHighestRock chamber = 
+  let 
+    positions = getAllPositions chamber 
+    index = positions |> List.filterMap (\(x, y) -> if Array2D.get y x chamber == Just '#' then Just y else Nothing) |> List.maximum |> Maybe.withDefault 0 
+  in 
+    index + 1
+
+updateFall : Model -> Model
+updateFall model = 
+  case model.rock of 
+    Moving (pos, shape) -> 
+      let 
+        r = tryMoveDown model.rock 
+      in 
+        if r == model.rock then 
+          let 
+            chamber = addRockPositions model.chamber (r |> toRockPositions) 
+            highestRock = findHighestRock chamber 
+          in 
+            { model | rock = Stopped shape, chamber = chamber, move = Blow, highestRock = highestRock }
+        else 
+          { model | message = "v", rock = r, move = Blow }
+    Stopped _ -> 
+      model
+
 updateStep : Model -> Model
-updateStep model = model 
+updateStep model = 
+  case model.rock of 
+    Moving _ -> 
+      case model.move of 
+        Blow -> updateBlow model 
+        Fall -> updateFall model 
+    Stopped shape -> 
+      let 
+        y = model.highestRock + 3
+      in 
+        { model | rock = Moving ((2, y), nextShape shape)}
+
+updateSideways : Char -> Model -> Model
+updateSideways move model = 
+  let 
+    rock = moveRock move model.rock
+  in 
+    { model | rock = rock }
 
 updateKey : Char -> Model -> Model
 updateKey move model = 
-  case model.rock of 
-    Just rock -> 
-      let 
-        (result, r) = moveRock move rock 
-      in 
-        case result of 
-          Stopped -> { model | rock = Just r }
-          Moving -> { model | rock = Just r }
-    Nothing -> model 
+  case move of 
+    '>' -> updateSideways move model
+    '<' -> updateSideways move model
+    'v' -> updateFall model
+    _ -> model
 
 updateTogglePlay : Model -> Model
 updateTogglePlay model = 
@@ -376,27 +317,17 @@ subscriptions model =
 
 -- VIEW
 
-toWarehouseRows : Array2D Char -> List String 
-toWarehouseRows warehouse = 
-  let
-    ys = List.range 0 (Array2D.rows warehouse - 1)
-    xs = List.range 0 (Array2D.columns warehouse - 1)
-    rows = ys |> List.map (\y -> xs |> List.filterMap (\x -> Array2D.get y x warehouse) |> String.fromList)
-  in 
-    rows 
-
-
 toChamberRow : Set Pos -> Array2D Char -> List Int -> Int -> String 
 toChamberRow rocks chamber xs y = 
   let 
-    s = xs |> List.filterMap (\x -> if Set.member (x, y) rocks then Just '#' else (Array2D.get y x chamber)) |> String.fromList
+    s = xs |> List.filterMap (\x -> if Set.member (x, y) rocks then Just '@' else (Array2D.get y x chamber)) |> String.fromList
   in 
     "|" ++ s ++ "|"
 
 toChamberRows : Set Pos -> Int -> Array2D Char -> List String 
 toChamberRows rocks highest chamber = 
   let
-    ys = List.range 0 (highest  + 3)
+    ys = List.range 0 (highest  + 7)
     xs = List.range 0 (Array2D.columns chamber - 1)
     rows = ys |> List.map (toChamberRow rocks chamber xs)
   in 
@@ -413,14 +344,17 @@ movePoint : Pos -> Pos -> Pos
 movePoint (x0, y0) (x1, y1) = 
   (x0 + x1, y0 + y1)
 
-toRockPositions : Pos -> Shape -> List Pos
-toRockPositions pos shape = 
-  case shape of 
-    Dash -> [(0, 0), (1, 0), (2, 0), (3, 0)] |> List.map (movePoint pos)
-    Cross -> [(1, 0), (0, 1), (1, 1), (2, 1), (1, 2)] |> List.map (movePoint pos)
-    Angle -> [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)] |> List.map (movePoint pos)
-    Bar -> [(0, 0), (0, 1), (0, 2), (0, 3)] |> List.map (movePoint pos)
-    Box -> [(0, 0), (1, 0), (0, 1), (1, 1)] |> List.map (movePoint pos)
+toRockPositions : Rock -> List Pos
+toRockPositions rock = 
+  case rock of 
+    Stopped _ -> []
+    Moving (pos, shape) -> 
+      case shape of 
+        Dash -> [(0, 0), (1, 0), (2, 0), (3, 0)] |> List.map (movePoint pos)
+        Cross -> [(1, 0), (0, 1), (1, 1), (2, 1), (1, 2)] |> List.map (movePoint pos)
+        Angle -> [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)] |> List.map (movePoint pos)
+        Bar -> [(0, 0), (0, 1), (0, 2), (0, 3)] |> List.map (movePoint pos)
+        Box -> [(0, 0), (1, 0), (0, 1), (1, 1)] |> List.map (movePoint pos)
 
 view : Model -> Document Msg
 view model = 
@@ -431,16 +365,13 @@ viewBody : Model -> Html Msg
 viewBody model =
   let
     textFontSize = "20px"
-    rockPositions = 
-      case model.rock of 
-        Just (pos, shape) -> toRockPositions pos shape |> Set.fromList
-        Nothing -> Set.empty
+    rockPositions = model.rock |> toRockPositions |> Set.fromList 
     rows = toChamberRows rockPositions model.highestRock model.chamber
     elements = rows |> List.concatMap (toRowElements)
     rockPosStr = 
       case model.rock of 
-        Just ((x, y), shape) -> "(" ++ String.fromInt x ++ ", " ++ String.fromInt y ++  ")"
-        Nothing -> "_"
+        Moving ((x, y), shape) -> "(" ++ String.fromInt x ++ ", " ++ String.fromInt y ++  ")"
+        Stopped _ -> "_"
   in 
     Html.table 
       [ Html.Attributes.style "width" "1080px"
@@ -532,6 +463,7 @@ viewBody model =
               [ 
                 Html.div [] [ Html.text (String.fromInt model.highestRock) ]
               , Html.div [] [ Html.text rockPosStr ]
+              , Html.div [] [ Html.text model.message ]
               ] ]
       , Html.tr 
           []
