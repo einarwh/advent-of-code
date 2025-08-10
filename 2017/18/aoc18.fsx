9a4e9d161b107d8febe8486cfabfdc8845014f6d
Einar W. Høst <einarwh@gmail.com>
2025-08-10 14:06:16 +0200

    sample works

--- 2017/18/aoc18.fsx
+++ 2017/18/aoc18.fsx
@@ -13,17 +13,21 @@ type Value =
     | Num of int
 
 type Instruction = 
-    | Snd of Value 
+    | Snd of RegName 
     | Set of RegName * Value 
     | Add of RegName * Value 
     | Mul of RegName * Value 
     | Mod of RegName * Value 
-    | Rcv of Value 
+    | Rcv of RegName 
     | Jgz of Value * Value 
+
+type State = Terminated | Waiting | Running 
 
 type Program = {
     ptr : int 
-    instructions : Instruction list 
+    instructions : Instruction array 
+    state : State
+    duet : bool
     sound : int 
     registers : Map<RegName, int>
     inbox : Queue<int> 
@@ -39,7 +43,7 @@ let tryParseSnd (line : string) : Instruction option =
     let m = Regex.Match(line, "^snd ([a-z]|\d+)$")
     if m.Success then
         let s = m.Groups.[1].Value
-        let inst = Snd (parseValue s)
+        let inst = Snd s
         Some inst
     else
         None
@@ -88,7 +92,7 @@ let tryParseRcv (line : string) : Instruction option =
     let m = Regex.Match(line, "^rcv ([a-z]|-?\d+)$")
     if m.Success then
         let s = m.Groups.[1].Value
-        let inst = Rcv (parseValue s)
+        let inst = Rcv s
         Some inst
     else
         None
@@ -116,18 +120,125 @@ let tryParse (s : String) : Instruction option =
    |> Option.orElseWith (fun () -> tryParseJgz s)
    |> Option.orElseWith (fun () -> failwith <| sprintf "Failed to parse '%s'!" s)
 
-let readText fileName = 
-    File.ReadAllText(fileName).Trim()
-
 let readLines = 
     File.ReadAllLines
     >> Array.filter (fun line -> line <> String.Empty)
-    >> Array.toList
+
+let readValue (r : string) (registers : Map<RegName, int>) : int =
+    match Map.tryFind r registers with 
+    | Some n -> n 
+    | None -> 0
+
+let writeValue (r : string) (n : int) (registers : Map<RegName, int>) : Map<RegName, int> = 
+    Map.add r n registers
+
+let resolveValue (v : Value) (registers : Map<RegName, int>) : int = 
+    match v with 
+    | Num n -> n 
+    | Reg r -> 
+        readValue r registers
+
+let executeInstruction (inst : Instruction) (program : Program) : Program = 
+    // printfn "Execute %A" inst
+    match inst with 
+    | Snd r -> 
+        let n = readValue r program.registers
+        if program.duet then 
+            let outbox = program.outbox
+            outbox.Enqueue n 
+            { program with outbox = outbox; ptr = program.ptr + 1 }
+        else 
+            printfn "snd %s | sound <- %d" r n
+            { program with sound = n; ptr = program.ptr + 1 }
+    | Rcv r -> 
+        if program.duet then 
+            let inbox = program.inbox 
+            if inbox.Count = 0 then 
+                { program with state = Waiting }
+            else 
+                let received = inbox.Dequeue()
+                let registers = writeValue r received program.registers
+                { program with registers = registers; ptr = program.ptr + 1 }
+        else 
+            let n = readValue r program.registers
+            if n = 0 then 
+                printfn "rcv skipped"
+                { program with ptr = program.ptr + 1 }
+            else 
+                printfn "rcv %d" program.sound
+                { program with state = Terminated }
+    | Set (r, v) -> 
+        let n = resolveValue v program.registers
+        let registers = writeValue r n program.registers
+        printfn "set %s <- %d" r n
+        { program with registers = registers; ptr = program.ptr + 1 }
+    | Add (r, v) -> 
+        let n = readValue r program.registers
+        let m = resolveValue v program.registers
+        let result = n + m
+        printfn "add %s <- %d+%d=%d" r n m result
+        let registers = writeValue r result program.registers
+        { program with registers = registers; ptr = program.ptr + 1 }
+    | Mul (r, v) -> 
+        let n = readValue r program.registers
+        let m = resolveValue v program.registers
+        let result = n * m
+        printfn "mul %s <- %d+%d=%d" r n m result
+        let registers = writeValue r result program.registers
+        { program with registers = registers; ptr = program.ptr + 1 }
+    | Mod (r, v) -> 
+        let n = readValue r program.registers
+        let m = resolveValue v program.registers
+        let result = n % m
+        printfn "mul %s <- %d+%d=%d" r n m result
+        let registers = writeValue r result program.registers
+        { program with registers = registers; ptr = program.ptr + 1 }
+    | Jgz (v1, v2) -> 
+        let x = resolveValue v1 program.registers
+        let y = resolveValue v2 program.registers
+        if x > 0 then 
+            let ptr = program.ptr + y
+            printfn "jgz %d %d to %d" x y ptr 
+            { program with ptr = ptr}
+        else 
+            printfn "jgz %d %d skipped" x y 
+            { program with ptr = program.ptr + 1 }
+
+let executeProgramStep (program : Program) = 
+    match program.state with 
+    | Terminated -> program 
+    | Waiting -> program 
+    | Running -> 
+        let ptr = program.ptr 
+        if ptr < 0 || ptr >= Array.length program.instructions then 
+            { program with state = Terminated }
+        else 
+            let inst = program.instructions[ptr]
+            executeInstruction inst program 
+
+let rec executeLoop (program : Program) = 
+    match program.state with 
+    | Terminated -> program 
+    | Waiting -> program 
+    | Running -> 
+        program |> executeProgramStep |> executeLoop
+
+let initProgram duet instructions = 
+    { ptr = 0  
+      instructions = instructions
+      state = Running
+      duet = duet
+      sound = 0
+      registers = Map.empty
+      inbox = new Queue<int>()
+      outbox = new Queue<int>()
+    }
 
 let run fileName = 
     let lines = readLines fileName
-    let instructions = lines |> List.choose tryParse
+    let instructions = lines |> Array.choose tryParse
     instructions |> printfn "%A"
-    "Not solved." |> printfn "%s"
+    let program = initProgram false instructions
+    executeLoop program |> printfn "%A"
 
-run "input.txt"
+run "sample.txt"
