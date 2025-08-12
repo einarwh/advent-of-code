a28945be0ada2d253b93ce33dc58274bf74ebe26
Einar W. Høst <einarwh@gmail.com>
2025-08-12 20:00:39 +0200

    2020/23

--- 2020/23/aoc23.fsx
+++ 2020/23/aoc23.fsx
@@ -4,80 +4,66 @@
 open System
 open System.IO
 
-type Circle = {
-    current : int 
-    cups : int array
-}
+let findDestination (cups : int list) (maxCup : int) (curr : int) = 
+    let rec loop i = 
+        if i < 1 then loop maxCup 
+        else if List.contains i cups then loop (i - 1)
+        else i 
+    loop (curr - 1)
 
-module Circle = 
+let move (maxCup : int) (links : Map<int, int>, curr : int) = 
+    let cup1 = links[curr]
+    let cup2 = links[cup1]
+    let cup3 = links[cup2]
+    let next = links[cup3]
+    let cups = [cup1; cup2; cup3]
+    let dest = findDestination cups maxCup curr 
+    let links' = 
+        links 
+        |> Map.add curr next 
+        |> Map.add dest cup1 
+        |> Map.add cup3 links[dest]
+    (links', next)
 
-    let getIndex offset index cupCount = 
-        (index + offset) % cupCount
+let toCups (links : Map<int,int>) = 
+    let rec loop acc curr = 
+        match links[curr] with 
+        | 1 -> List.rev acc 
+        | c -> loop (c :: acc) c 
+    loop [] 1 |> List.toArray
 
-    let getDestinationCup current remaining removed = 
-        let rec loop candidate = 
-            if candidate < 1 then loop (Array.max remaining)
-            else
-                if Array.contains candidate removed then 
-                    loop (candidate - 1)
-                else 
-                    candidate 
-        loop (current - 1)
-
-    let move (circle : Circle) = 
-        let index = circle.cups |> Array.findIndex (fun c -> c = circle.current)
-        let cupCount = circle.cups |> Array.length 
-        let i1 = (index + 1) % cupCount
-        let i2 = (index + 2) % cupCount
-        let i3 = (index + 3) % cupCount
-        let cup1 = circle.cups[i1]
-        let cup2 = circle.cups[i2]
-        let cup3 = circle.cups[i3]
-        let removed = [|cup1;cup2;cup3|]
-        let cupsStr = circle.cups |> Array.toList |> List.map (fun c -> if c = circle.current then "(" + string c + ")" else " " + string c + " ") |> String.concat " "
-        // printfn "cups: %s" cupsStr
-        // printfn "current: %d" circle.current
-        let removedStr = removed |> Array.toList |> List.map string |> String.concat ", "
-        // printfn "pick up: %s" removedStr
-        let remaining = circle.cups |> Array.filter (fun c -> not <| Array.contains c removed)
-        let destination = getDestinationCup circle.current remaining removed 
-        // printfn "destination: %d" destination
-        let destinationIndex = Array.findIndex (fun c -> c = destination) remaining
-        // printfn "destination index: %d" destinationIndex
-        let splitIndex = destinationIndex + 1
-        let movedCups = 
-            if splitIndex < cupCount then 
-                let left, right = Array.splitAt splitIndex remaining
-                Array.concat [| left; removed; right |]
-            else 
-                Array.concat [| remaining; removed |]
-        let movedIndex = movedCups |> Array.findIndex (fun c -> c = circle.current)
-        let nextCup = movedCups[(movedIndex + 1) % cupCount]
-        { current = nextCup; cups = movedCups }
-
-    let findOrder circle = 
-        let cups = circle.cups 
-        let ix = Array.findIndex (fun c -> c = 1) cups
-        if ix = 0 then cups[1..]
-        else if ix = Array.length cups - 1 then cups[..(Array.length cups - 1)]
+let play moves (cups : int array) = 
+    // printfn "cups: %A" cups
+    let maxCup = cups |> Array.max 
+    let rec loop i (links, curr) = 
+        if i <= moves then 
+            loop (i + 1) (move maxCup (links, curr))
         else 
-            let before = cups[0..ix-1]
-            let after = cups[ix+1..Array.length cups - 1]
-            Array.concat [|after; before|]
+            (links, curr)
+    let links = 
+        cups 
+        |> Array.toList 
+        |> List.indexed 
+        |> List.fold (fun ls (ix, c) -> ls |> Map.add c (cups[(ix + 1) % cups.Length])) Map.empty
+    // printfn "%A" links
+    loop 1 (links, cups[0]) |> fst |> toCups
 
-    let moveLoop moves circle = 
-        let rec loop i c = 
-            // printfn "\n-- move %d --" i 
-            if i <= moves then 
-                loop (i + 1) (move c) 
-            else 
-                c |> findOrder |> Array.toList |> List.map string |> String.concat ""
-        loop 1 circle
+let part1 cups = 
+    cups |> play 100 |> Array.map string |> String.concat "" |> printfn "%s"
+
+let part2 cups = 
+    let moreCups = Array.concat [|cups; [|10 .. 1000000|]|]
+    printfn "%A" (Array.max moreCups)
+    printfn "%A" (Array.take 20 moreCups)
+    let order = play 10000000 moreCups 
+    printfn "%A" (Array.take 20 order)
+    int64 order[0] * int64 order[1] |> printfn "%d"
 
 let run fileName = 
     let text = File.ReadAllText(fileName).Trim()
-    let cups : int array = text |> Seq.toArray |> Array.map (fun ch -> Int32.Parse(ch.ToString()))
-    let circle = { current = cups[0]; cups = cups }
-    Circle.moveLoop 100 circle |> printfn "%s"
+    let cups = text |> Seq.toArray |> Array.map (fun ch -> Int32.Parse(ch.ToString()))
+    cups |> part1 
+    cups |> part2
+    0
 
 run "input.txt"
