a0c01bcf95a901c06307e7fd9a9a4554978858cb
Einar W. Høst <einarwh@gmail.com>
2025-08-12 11:31:33 +0200

    sample works

--- 2020/22/aoc22.fsx
+++ 2020/22/aoc22.fsx
@@ -15,7 +15,7 @@ let parsePlayer (line : string) : int list =
 let endGame winningCards : int = 
     winningCards |> List.rev |> List.mapi (fun i c -> (i + 1) * c) |> List.sum
 
-let rec play (round : int) (cards1 : int list) (cards2 : int list) : int = 
+let rec playGame (round : int) (cards1 : int list) (cards2 : int list) : int = 
     match (cards1, cards2) with 
     | [], _ -> 
         endGame cards2 
@@ -24,12 +24,66 @@ let rec play (round : int) (cards1 : int list) (cards2 : int list) : int =
     | h1 :: t1, h2 :: t2 -> 
         let round' = round + 1
         if h1 > h2 then 
-            play round' (t1 @ [h1; h2]) t2
+            playGame round' (t1 @ [h1; h2]) t2
         else 
-            play round' t1 (t2 @ [h2; h1])
+            playGame round' t1 (t2 @ [h2; h1])
+
+let serializeCards (cards : int list) = 
+    cards |> List.map string |> String.concat ", "
+
+let rec playRecursiveGame (game : int) (round : int) (seen : Set<int list * int list>) (cards1 : int list) (cards2 : int list) : (int * int) = 
+    if Set.contains (cards1, cards2) seen then 
+        (endGame cards1, 0)
+    else 
+        match (cards1, cards2) with 
+        | [], _ -> 
+            (0, endGame cards2)
+        | _, [] -> 
+            (endGame cards1, 0)
+        | c1 :: t1, c2 :: t2 -> 
+            printfn "-- Round %d (Game %d) --" round game  
+            printfn "Player 1's deck: %s" (serializeCards cards1) 
+            printfn "Player 2's deck: %s" (serializeCards cards2) 
+            let round' = round + 1
+            let seen' = Set.add (cards1, cards2) seen 
+            if c1 <= List.length t1 && c2 <= List.length t2 then 
+                printfn "Playing a sub-game to determine the winner..."
+                printfn ""
+                // Recur.
+                let nextCards1 = t1 |> List.take c1 
+                let nextCards2 = t2 |> List.take c2 
+                let (score1, score2) = playRecursiveGame (game + 1) 1 Set.empty nextCards1 nextCards2 
+                if score1 > score2 then 
+                    printfn "The winner of %d is player 1!" (game + 1)
+                    printfn ""
+                    printfn "...anyway, back to game %d" game
+                    printfn "Player 1 wins round %d of game %d!" round game 
+                    playRecursiveGame game round' seen' (t1 @ [c1; c2]) t2
+                else 
+                    printfn "The winner of %d is player 2!" (game + 1)
+                    printfn ""
+                    printfn "...anyway, back to game %d" game
+                    printfn "Player 2 wins round %d of game %d!" round game 
+                    playRecursiveGame game round' seen' t1 (t2 @ [c2; c1])
+            else 
+                printfn "Player 1 plays: %d" c1
+                printfn "Player 2 plays: %d" c2
+                if c1 > c2 then 
+                    printfn "Player 1 wins round %d of game %d!" round game 
+                    printfn ""
+                    playRecursiveGame game round' seen' (t1 @ [c1; c2]) t2
+                else 
+                    printfn "Player 2 wins round %d of game %d!" round game 
+                    printfn ""
+                    playRecursiveGame game round' seen' t1 (t2 @ [c2; c1])
 
 let run (cardArray : int list array) = 
-    play 0 cardArray.[0] cardArray.[1] |> printfn "%d"
+    playGame 0 cardArray.[0] cardArray.[1] |> printfn "%d"
+    // Player 1's deck: 9, 2, 6, 3, 1
+    // Player 2's deck: 5, 8, 4, 7, 10
+    let cards1 = [9;2;6;3;1]
+    let cards2 = [5;8;4;7;10]
+    playRecursiveGame 1 1 Set.empty cards1 cards2 |> printfn "%A"
 
 "input.txt"
 |> File.ReadAllText
