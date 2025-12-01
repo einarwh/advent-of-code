38c0063ca7bf73c3ffca78278658085784d4618c
Einar W. Høst <einarwh@gmail.com>
2025-12-01 06:54:10 +0100

    Update aoc01.fsx

--- 2025/01/aoc01.fsx
+++ 2025/01/aoc01.fsx
@@ -4,7 +4,7 @@
 open System
 open System.IO
 
-let parse (s : string) : int = 
+let parse (s : string) = 
     let n = int <| s.Substring 1
     match s[0] with 
     | 'L' -> -n 
@@ -16,15 +16,14 @@ let readLines =
     >> Array.toList
 
 let run fileName = 
-    let lines = readLines fileName
-    let numbers = lines |> List.map parse
-    let folder (current, count) rot = 
+    let numbers = readLines fileName |> List.map parse
+    let zeroes (current, count) rot = 
         let next = (current + rot) % 100
         (next + 100) % 100, count + if next = 0 then 1 else 0
     let toSteps num = 
         if num < 0 then Seq.replicate (abs num) -1 else Seq.replicate num 1 
         |> Seq.toList
-    numbers |> List.fold folder (50, 0) |> snd |> printfn "%d"
-    numbers |> List.collect toSteps |> List.fold folder (50, 0) |> snd |> printfn "%d"
+    numbers |> List.fold zeroes (50, 0) |> snd |> printfn "%d"
+    numbers |> List.collect toSteps |> List.fold zeroes (50, 0) |> snd |> printfn "%d"
 
 run "input.txt"
