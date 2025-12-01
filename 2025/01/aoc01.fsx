461f48fdab54124e777a4a9433968b4c469ad797
Einar W. Høst <einarwh@gmail.com>
2025-12-01 06:54:53 +0100

    Update aoc01.fsx

--- 2025/01/aoc01.fsx
+++ 2025/01/aoc01.fsx
@@ -20,10 +20,10 @@ let run fileName =
     let zeroes (current, count) rot = 
         let next = (current + rot) % 100
         (next + 100) % 100, count + if next = 0 then 1 else 0
-    let toSteps num = 
+    let steps num = 
         if num < 0 then Seq.replicate (abs num) -1 else Seq.replicate num 1 
         |> Seq.toList
     numbers |> List.fold zeroes (50, 0) |> snd |> printfn "%d"
-    numbers |> List.collect toSteps |> List.fold zeroes (50, 0) |> snd |> printfn "%d"
+    numbers |> List.collect steps |> List.fold zeroes (50, 0) |> snd |> printfn "%d"
 
 run "input.txt"
