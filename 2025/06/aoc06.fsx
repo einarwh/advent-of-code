90d00317be9dcbe30b011715903cab4eec77ed13
Einar W. Høst <einarwh@gmail.com>
2025-12-06 11:06:04 +0100

    Update aoc06.fsx

--- 2025/06/aoc06.fsx
+++ 2025/06/aoc06.fsx
@@ -24,22 +24,22 @@ let operation (cs : char array) =
     | _ -> failwith "?"
 
 let human (grid : char array array) = 
-    [0 .. grid.Length - 1]
-    |> List.map (fun c -> grid |> Array.map (fun r -> r[c]) |> fun cs -> new string(cs) |> int64)
+    [|0 .. grid.Length - 1|]
+    |> Array.map (fun c -> grid |> Array.map (fun r -> r[c]) |> fun cs -> new string(cs) |> int64)
 
 let cephalopod (grid : char array array) = 
-    grid |> Array.toList |> List.map (fun cs -> new string(cs) |> int64)
+    grid |> Array.map (fun cs -> new string(cs) |> int64)
 
-let eval (toNumbers : char array array -> int64 list) (group : char array array) = 
+let eval (toNumbers : char array array -> int64 array) (group : char array array) = 
     let op = group |> Array.map Array.last |> operation
     let grid = group |> Array.map (fun cs -> cs[0 .. cs.Length - 2])
-    grid |> toNumbers |> List.reduce op
+    grid |> toNumbers |> Array.reduce op
 
 let run fileName = 
     let rows = fileName |> File.ReadAllLines |> Array.filter (fun line -> line <> String.Empty)
     let cols = [|0 .. rows[0].Length - 1|] |> Array.map (fun x -> rows |> Array.map (fun r -> r[x]))
     let grouped = group cols
-    grouped |> List.map (eval human) |> List.sum |> printfn "%d"
-    grouped |> List.map (eval cephalopod) |> List.sum |> printfn "%d"
+    grouped |> List.sumBy (eval human) |> printfn "%d"
+    grouped |> List.sumBy (eval cephalopod) |> printfn "%d"
 
 run "input.txt"
