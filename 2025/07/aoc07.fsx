930154686233276b2e3cf2ba8b5537cc9054fbf8
Einar W. Høst <einarwh@gmail.com>
2025-12-07 11:18:26 +0100

    Update aoc07.fsx

--- 2025/07/aoc07.fsx
+++ 2025/07/aoc07.fsx
@@ -9,8 +9,8 @@ let readLines =
     >> Array.filter (fun line -> line <> String.Empty)
     >> Array.toList
 
-let split (lines : string list) = 
-    let rec loop (count : int) (beams : Set<int>) (lines : string list) = 
+let splitCount (beam, lines) = 
+    let rec loop count beams lines = 
         match lines with 
         | [] -> count 
         | h :: t -> 
@@ -20,43 +20,31 @@ let split (lines : string list) =
             let beams' = Set.difference beams collisions |> Set.union splitBeams
             let count' = count + Set.count collisions 
             loop count' beams' t
-    match lines with 
-    | [] -> failwith "?"
-    | h :: t -> 
-        let ix = h |> Seq.findIndex ((=) 'S')
-        let beams = Set.empty |> Set.add ix
-        loop 0 beams t 
+    loop 0 (Set.empty |> Set.add beam) lines 
 
-let rec timelineCount fn (beam : int, lines : string list) = 
+let timelineCount fn (beam, lines) = 
     match lines with 
     | [] -> 1L
     | h :: t -> 
         let indexes = h |> Seq.toList |> List.indexed |> List.choose (fun (i, c) -> if c = '^' then Some i else None)
         if indexes |> List.contains beam then 
-            let countL = fn (beam - 1, t) 
-            let countR = fn (beam + 1, t) 
-            let count = countL + countR 
-            count 
+            fn (beam - 1, t) + fn (beam + 1, t) 
         else 
             fn (beam, t) 
 
-let memoizeRec f =
+let memoize f =
     let cache = System.Collections.Concurrent.ConcurrentDictionary()
     let rec recF x =
         cache.GetOrAdd(x, lazy f recF x).Value
     recF
 
-let timelines (lines : string list) = 
-    match lines with 
+let solve counter = function 
     | [] -> failwith "?"
-    | h :: t -> 
-        let ix = h |> Seq.findIndex ((=) 'S')
-        let fn = memoizeRec <| timelineCount 
-        fn (ix, t) 
+    | h :: t -> counter (h |> Seq.findIndex ((=) 'S'), t)
 
 let run fileName = 
     let lines = readLines fileName
-    lines |> split |> printfn "%d"
-    lines |> timelines |> printfn "%d"
+    lines |> solve splitCount |> printfn "%d"
+    lines |> solve (memoize timelineCount) |> printfn "%d"
 
 run "input.txt"
