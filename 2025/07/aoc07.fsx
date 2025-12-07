51f252160b6e6d665b258f30bfd8d3dc6ece305c
Einar W. Høst <einarwh@gmail.com>
2025-12-07 10:48:13 +0100

    07

--- 2025/07/aoc07.fsx
+++ 2025/07/aoc07.fsx
@@ -9,7 +9,7 @@ let readLines =
     >> Array.filter (fun line -> line <> String.Empty)
     >> Array.toList
 
-let solve (lines : string list) = 
+let split (lines : string list) = 
     let rec loop (count : int) (beams : Set<int>) (lines : string list) = 
         match lines with 
         | [] -> count 
@@ -27,8 +27,33 @@ let solve (lines : string list) =
         let beams = Set.empty |> Set.add ix
         loop 0 beams t 
 
+let rec timelineCount (mem : Map<int * int, int64>) (beam : int, depth : int) (lines : string list) = 
+    if Map.containsKey (beam, depth) mem then 
+        mem, Map.find (beam, depth) mem
+    else 
+        match lines with 
+        | [] -> mem, 1L
+        | h :: t -> 
+            let indexes = h |> Seq.toList |> List.indexed |> List.choose (fun (i, c) -> if c = '^' then Some i else None)
+            if indexes |> List.contains beam then 
+                let memL, countL = timelineCount mem (beam - 1, depth + 1) t 
+                let memR, countR = timelineCount memL (beam + 1, depth + 1) t 
+                let count = countL + countR 
+                let mem' = memR |> Map.add (beam, depth) count  
+                mem', count 
+            else 
+                timelineCount mem (beam, depth + 1) t 
+
+let timelines (lines : string list) = 
+    match lines with 
+    | [] -> failwith "?"
+    | h :: t -> 
+        let ix = h |> Seq.findIndex ((=) 'S')
+        timelineCount Map.empty (ix, 1) t |> snd 
+
 let run fileName = 
     let lines = readLines fileName
-    lines |> solve |> printfn "%d"
+    lines |> split |> printfn "%d"
+    lines |> timelines |> printfn "%d"
 
 run "input.txt"
