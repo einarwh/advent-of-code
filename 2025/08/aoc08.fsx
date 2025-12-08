8573ede143b217a32c2f08e5ecab7911d33cbe67
Einar W. Høst <einhos@norkart.no>
2025-12-08 13:48:01 +0100

    Update aoc08.fsx

--- 2025/08/aoc08.fsx
+++ 2025/08/aoc08.fsx
@@ -28,11 +28,46 @@ let getDistances boxes =
         | _ -> 
             acc |> List.concat |> List.sortBy fst
     loop [] boxes
+    
+let connect conn circuits = 
+    let rec loop acc circuits =
+        printfn "\nloop..."
+        circuits |> List.iter (printfn "%A")
+        printfn "."
+        match circuits with 
+        | [] -> 
+            printfn "found no match for %A, form new circuit" conn 
+            (conn :: acc) |> List.rev 
+        | circ :: rest -> 
+            let shared = circ |> Set.intersect conn |> Set.count
+            if shared > 0 then 
+                let union = circ |> Set.union conn 
+                printfn "overlap between %A and %A -> connect %A" conn circ union 
+                List.rev (union :: acc) @ rest 
+            else 
+                printfn "no overlap between %A and %A" conn circ
+                loop (circ :: acc) rest 
+    loop [] circuits
+    
+let solve count boxes = 
+    let rec loop circuits connections = 
+        match connections with 
+        | [] -> circuits 
+        | conn :: rest ->
+            loop (connect conn circuits) rest 
+    let distances = boxes |> getDistances
+    let toSet (a, b) = Set.empty |> Set.add a |> Set.add b 
+    let connections = distances |> List.take count |> List.map (snd >> toSet)
+    connections |> List.iter (printfn "%A")
+    let result = loop [] connections
+    printfn "%A" result
+    0
 
 let run fileName = 
     let lines = readLines fileName
     let boxes = lines |> List.map parse
-    boxes |> printfn "%A"
-    boxes |> getDistances |> printfn "%A"
+    // boxes |> printfn "%A"
+    // boxes |> getDistances |> printfn "%A"
+    solve 10 boxes
 
 run "sample.txt"
