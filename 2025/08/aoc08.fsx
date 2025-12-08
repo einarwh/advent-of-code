cac5a183a2e91787c8c3337d3785c3036a14b824
Einar W. Høst <einhos@norkart.no>
2025-12-08 13:11:40 +0100

    Update aoc08.fsx

--- 2025/08/aoc08.fsx
+++ 2025/08/aoc08.fsx
@@ -1,4 +1,4 @@
-// Advent of Code 2025. Day 08.
+// Advent of Code 2025. Day 08: Playground.
 // dotnet fsi aoc08.fsx
 
 open System
@@ -18,10 +18,21 @@ let distance (x1, y1, z1) (x2, y2, z2) =
     let dx, dy, dz = x2-x1, y2-y1, z2-z1 
     let sq n = n * n |> float
     sqrt <| sq dx + sq dy + sq dz 
+    
+let getDistances boxes = 
+    let rec loop acc boxes = 
+        match boxes with 
+        | a :: rest -> 
+            let these = rest |> List.map (fun b -> (distance a b, (a, b))) 
+            loop (these :: acc) rest 
+        | _ -> 
+            acc |> List.concat |> List.sortBy fst
+    loop [] boxes
 
 let run fileName = 
     let lines = readLines fileName
     let boxes = lines |> List.map parse
     boxes |> printfn "%A"
+    boxes |> getDistances |> printfn "%A"
 
 run "sample.txt"
