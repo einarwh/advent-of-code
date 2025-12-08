16cdac669487d75d94eb1a4cc5c9f2b9d76b6cb9
Einar W. Høst <einarwh@gmail.com>
2025-12-08 22:20:16 +0100

    Update aoc07step.fsx

--- 2025/07/aoc07step.fsx
+++ 2025/07/aoc07step.fsx
@@ -9,30 +9,55 @@ let readLines =
     >> Array.filter (fun line -> line <> String.Empty)
     >> Array.toList
 
+type SplitState = 
+    { beams : Set<int>
+      depth : int 
+      lines : string list
+      seen : Set<int*int>
+      count : int }
+
+let splitCount (state : SplitState) = 
+    match state.lines with 
+    | [] -> state 
+    | h :: t -> 
+        let beams = state.beams 
+        let depth = state.depth 
+        let count = state.count
+        let indexes = h |> Seq.toList |> List.indexed |> List.choose (fun (i, c) -> if c = '^' then Some i else None)
+        let collisions = beams |> Set.filter (fun b -> indexes |> List.contains b)
+        let splitBeams = collisions |> Set.fold (fun s b -> s |> Set.add (b - 1) |> Set.add (b + 1)) Set.empty
+        let beams' = Set.difference beams collisions |> Set.union splitBeams
+        let count' = count + Set.count collisions 
+        let seen = beams |> Set.map (fun b -> b, depth) |> Set.union state.seen
+        { beams = beams'; depth = depth + 1; lines = t; seen = seen; count = count' }
+
+let rec splitLoop (state : SplitState) = 
+    match state.lines with 
+    | [] -> state 
+    | _ -> 
+        state |> splitCount |> splitLoop 
+
 let split (lines : string list) = 
-    let rec loop (count : int) (beams : Set<int>) (lines : string list) = 
-        match lines with 
-        | [] -> count 
-        | h :: t -> 
-            let indexes = h |> Seq.toList |> List.indexed |> List.choose (fun (i, c) -> if c = '^' then Some i else None)
-            let collisions = beams |> Set.filter (fun b -> indexes |> List.contains b)
-            let splitBeams = collisions |> Set.fold (fun s b -> s |> Set.add (b - 1) |> Set.add (b + 1)) Set.empty
-            let beams' = Set.difference beams collisions |> Set.union splitBeams
-            let count' = count + Set.count collisions 
-            loop count' beams' t
     match lines with 
     | [] -> failwith "?"
     | h :: t -> 
         let ix = h |> Seq.findIndex ((=) 'S')
         let beams = Set.empty |> Set.add ix
-        loop 0 beams t 
+        let state = 
+            { beams = beams 
+              depth = 1 
+              lines = t
+              seen = Set.empty 
+              count = 0 }
+        let state' = splitLoop state 
+        state'.count
 
 type CallInfo = 
     { pos : int*int 
       lines : string list }
 
 type StackItem = 
-    Call of CallInfo | Combine 
+    Left of CallInfo | Right of CallInfo | Combine | Down of CallInfo | Bubble
 
 type TimelineState = 
     { mem : Map<int * int, int64> 
@@ -43,9 +68,6 @@ type TimelineState =
       stack : StackItem list }
 
 let timelineStep (state : TimelineState) : TimelineState =
-    printfn "timelineStep"
-    printfn " - pos: %A" state.pos
-    printfn " - count: %A" state.count
     let mem = state.mem 
     let beam, depth = state.pos 
     if Map.containsKey (beam, depth) mem then 
@@ -54,54 +76,57 @@ let timelineStep (state : TimelineState) : TimelineState =
     else 
         let lines = state.lines 
         match lines with 
-        | [] -> { state with count = state.count + 1L }
+        | [] ->
+            let mem' = state.mem |> Map.add (beam, depth) 1L
+            { state with count = state.count + 1L; mem = mem' }
         | h :: t -> 
-            printfn "line %A" h
             let indexes = h |> Seq.toList |> List.indexed |> List.choose (fun (i, c) -> if c = '^' then Some i else None)
             if indexes |> List.contains beam then 
                 let seen = state.seen |> Set.add (beam, depth) |> Set.add (beam - 1, depth)
-                let itemL = Call { pos = beam - 1, depth + 1; lines = t }
-                let itemR = Call { pos = beam + 1, depth + 1; lines = t }
+                let itemL = Left { pos = beam - 1, depth + 1; lines = t }
+                let itemR = Right { pos = beam + 1, depth + 1; lines = t }
                 let stack = itemL :: itemR :: Combine :: state.stack 
                 { state with seen = seen; stack = stack }
             else 
                 let seen = state.seen |> Set.add (beam, depth)
-                let item = Call { pos = beam, depth + 1; lines = t }
-                let stack = item :: state.stack 
+                let item = Down { pos = beam, depth + 1; lines = t }
+                let stack = item :: Bubble :: state.stack 
                 { state with seen = seen; pos = beam, depth + 1; stack = stack }
 
 let rec timelineLoop (state : TimelineState) : TimelineState =
-    printfn "\ntimelineLoop %A" state.pos
     let state' = 
         match state.stack with 
         | [] -> 
-            printfn "call stack empty"
-            let state' = timelineStep state 
-            printfn "after step %A" state' 
-            state' 
+            timelineStep state 
         | item :: rest -> 
             match item with 
-            | Call info -> 
-                printfn "CALL"
+            | Left info -> 
+                let state' = { state with pos = info.pos; lines = info.lines; stack = rest }
+                timelineStep state' 
+            | Right info -> 
+                let state' = { state with pos = info.pos; lines = info.lines; stack = rest }
+                timelineStep state' 
+            | Down info -> 
                 let state' = { state with pos = info.pos; lines = info.lines; stack = rest }
                 timelineStep state' 
             | Combine -> 
-                printfn "COMBINE"
-                let mem' = state.mem |> Map.add state.pos state.count
-                { state with mem = mem'; stack = rest } 
+                let (beam, depth) = state.pos
+                let b, d = beam - 1, depth - 1
+                let pos = (b, d)
+                let countL = Map.find (b - 1, d + 1) state.mem
+                let countR = Map.find (b + 1, d + 1) state.mem 
+                let mem' = state.mem |> Map.add pos (countL + countR)
+                { state with mem = mem'; stack = rest; pos = pos } 
+            | Bubble -> 
+                let (beam, depth) = state.pos 
+                let pos = beam, depth - 1
+                let countD = Map.find (beam, depth) state.mem
+                let mem' = state.mem |> Map.add pos countD
+                { state with mem = mem'; stack = rest; pos = pos } 
+    // printfn "%d" state'.count
     if state'.stack |> List.isEmpty then state' 
         else timelineLoop state'
     
-
-// type TimelineState = 
-//     { mem : Map<int * int, int64> 
-//       pos : int*int
-//       lines : string list
-//       seen : Set<int*int>
-//       count : int64
-//       stack : StackItem list }
-
-
 let timelines (lines : string list) = 
     match lines with 
     | [] -> failwith "?"
@@ -114,11 +139,12 @@ let timelines (lines : string list) =
               seen = Set.empty 
               count = 0L 
               stack = [] } 
-        timelineLoop state
+        let result = timelineLoop state
+        result.count
 
 let run fileName = 
     let lines = readLines fileName
-    // lines |> split |> printfn "%d"
-    lines |> timelines |> printfn "%A"
+    lines |> split |> printfn "%d"
+    lines |> timelines |> printfn "%d"
 
-run "sample.txt"
+run "input.txt"
