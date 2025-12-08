1cf1c0749e8f91c3cd74c042dc90bc8501cc4c42
Einar W. Høst <einhos@norkart.no>
2025-12-08 13:02:38 +0100

    basics

--- 2025/08/aoc08.fsx
+++ 2025/08/aoc08.fsx
@@ -8,9 +8,20 @@ let readLines =
     File.ReadAllLines
     >> Array.filter (fun line -> line <> String.Empty)
     >> Array.toList
+    
+let parse (s : string) = 
+    match s.Split "," |> Array.map int with 
+    | [|a; b; c|] -> (a, b, c)
+    | _ -> failwith "?"
+
+let distance (x1, y1, z1) (x2, y2, z2) = 
+    let dx, dy, dz = x2-x1, y2-y1, z2-z1 
+    let sq n = n * n |> float
+    sqrt <| sq dx + sq dy + sq dz 
 
 let run fileName = 
     let lines = readLines fileName
-    lines |> printfn "%A"
+    let boxes = lines |> List.map parse
+    boxes |> printfn "%A"
 
-run "input.txt"
+run "sample.txt"
