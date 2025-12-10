91370afff5d3c2a9bee4ff8cc3572362f75f0927
Einar W. Høst <einarwh@gmail.com>
2025-12-10 22:13:13 +0100

    Update aoc10bit.fsx

--- 2025/10/aoc10bit.fsx
+++ 2025/10/aoc10bit.fsx
@@ -4,17 +4,13 @@
 open System
 open System.IO
 
-type Machine =
-    { lights : int
-      buttons : int array }
-
 let middleString (s : string) = s[1 .. s.Length - 2]
 
 let middleArray (arr : 'a array) = arr[1 .. arr.Length - 2]
 
 let bit pos = 1 <<< pos
 
-let parseLights (s : string) : int = 
+let parseLights (s : string) = 
     middleString s 
     |> Seq.toArray 
     |> Array.indexed
@@ -27,39 +23,30 @@ let parseNumbers (s : string) =
 let parseButton (s : string) = 
     middleString s |> parseNumbers |> Array.map bit |> Array.reduce (|||)
 
-let parseMachine (s : string) = 
+let parse (s : string) = 
     let parts = s.Split " "
     let lights = parts |> Array.head |> parseLights
     let buttons = middleArray parts |> Array.map parseButton
-    { lights = lights; buttons = buttons }
+    lights, buttons
     
 let readLines = 
     File.ReadAllLines
     >> Array.filter (fun line -> line <> String.Empty)
     >> Array.toList
 
-let pushAll (buttons : int array) (pattern : int) : int array = 
+let pushAll (buttons : int array) (pattern : int) = 
     buttons |> Array.map ((^^^) pattern)
 
-let solve (m : Machine) = 
-    let lights = m.lights 
-    let buttons = m.buttons
-    let rec loop (pushCount : int) (seen : Set<int>) (patterns : Set<int>) = 
+let solve (lights, buttons) = 
+    let rec loop pushCount patterns = 
         if Set.contains lights patterns then pushCount
         else
-            let patterns' = patterns |> Set.toArray |> Array.collect (pushAll buttons) |> Set.ofArray
-            printfn "patterns' %d" patterns'.Count
-            let shrunk = Set.difference patterns' seen
-            printfn "shrunk %d" shrunk.Count
-            let seen' = Set.union seen patterns 
-            printfn "seen' %d" seen'.Count
-            loop (pushCount + 1) seen' patterns'
-    let patterns = Set.empty |> Set.add 0
-    loop 0 Set.empty patterns 
+            patterns 
+            |> Set.toArray |> Array.collect (pushAll buttons) |> Set.ofArray 
+            |> loop (pushCount + 1) 
+    Set.empty |> Set.add 0 |> loop 0  
 
 let run fileName = 
-    let lines = readLines fileName
-    let machines = lines |> List.map parseMachine
-    machines |> List.sumBy solve |> printfn "%d"
+    fileName |> readLines |> List.map parse |> List.sumBy solve |> printfn "%d"
 
 run "input.txt"
